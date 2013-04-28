34c08124b9feb007375b52346a93ccc772d60232
Jonathan Neal <jonathantneal@hotmail.com>
2013-04-28 02:10:39 -0700

    IE6-7 Placeholder

--- source/oldie.after.js
+++ source/oldie.after.js
@@ -351,6 +351,50 @@
 			element[property] = ElementPrototype[property];
 		}
 
+		if (/^(input|textarea)$/i.test(element.nodeName) && /^(email|number|password|search|tel|text|url|)$/i.test(element.getAttribute('type'))) {
+			var
+			xInput = document.createElement('-ms-input'),
+			xPlaceholder = xInput.appendChild(document.createElement('-ms-placeholder')),
+			xPlaceholderText = xPlaceholder.appendChild(document.createTextNode(element.placeholder)),
+			xInputRuntimeStyle = xInput.runtimeStyle,
+			xPlaceholderRuntimeStyle = xPlaceholder.runtimeStyle,
+			elementCurrentStyle = element.currentStyle,
+			style;
+
+			xInputRuntimeStyle.display = 'inline-block';
+			xInputRuntimeStyle.fontSize = '100%';
+			xInputRuntimeStyle.margin = elementCurrentStyle.margin;
+
+			element.parentNode.insertBefore(xInput, element).appendChild(element);
+
+			xPlaceholderRuntimeStyle.backgroundColor = 'transparent';
+			xPlaceholderRuntimeStyle.fontFamily = elementCurrentStyle.fontFamily;
+			xPlaceholderRuntimeStyle.fontSize = elementCurrentStyle.fontSize;
+			xPlaceholderRuntimeStyle.margin = '2px 0 0 2px';
+			xPlaceholderRuntimeStyle.padding = elementCurrentStyle.padding;
+			xPlaceholderRuntimeStyle.position = 'absolute';
+			xPlaceholderRuntimeStyle.display = element.value ? 'none' : 'inline-block';
+			element.runtimeStyle.margin = '0';
+
+			xPlaceholder.attachEvent('onclick', function (event) {
+				element.focus();
+			});
+
+			element.attachEvent('onkeypress', function (event) {
+				xPlaceholderRuntimeStyle.display = 'none';
+			});
+
+			element.attachEvent('onkeyup', function (event) {
+				xPlaceholderRuntimeStyle.display = element.value ? 'none' : 'inline-block';
+			});
+
+			element.attachEvent('onpropertychange', function (event) {
+				if (event.propertyName == 'placeholder') {
+					xPlaceholder.innerHTML = element.placeholder;
+				}
+			});
+		}
+
 		return element;
 	}
 
