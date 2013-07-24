64bb5819e6dc7d3aee74420c4b1ded7c083ccff2
Jonathan Neal <jonathantneal@hotmail.com>
2013-07-24 14:34:56 -0700

    Rename object.defineProperties.js to Object.defineProperties.js

---
+++ minified/Object.defineProperties.js
@@ -0,0 +1,1 @@
+Object.defineProperties=function defineProperties(object,descriptors){for(var property in descriptors){Object.defineProperty(object,property,descriptors[property])}return object};
--- minified/object.defineProperties.js
+++
@@ -1,1 +0,0 @@
-Object.defineProperties=function defineProperties(object,descriptors){for(var property in descriptors){Object.defineProperty(object,property,descriptors[property])}return object};
