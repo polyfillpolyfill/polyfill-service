3da493fa0ec9dce4d211addb558df5e71fb81845
Jonathan Neal <jonathantneal@hotmail.com>
2013-07-24 14:35:07 -0700

    Rename object.defineProperty.js to Object.defineProperty.js

---
+++ minified/Object.defineProperty.js
@@ -0,0 +1,1 @@
+Object.defineProperty=function(object,property,descriptor){if(descriptor.get){object.__defineGetter__(property,descriptor.get)}if(descriptor.set){object.__defineSetter__(property,descriptor.set)}return object};
--- minified/object.defineProperty.js
+++
@@ -1,1 +0,0 @@
-Object.defineProperty=function(object,property,descriptor){if(descriptor.get){object.__defineGetter__(property,descriptor.get)}if(descriptor.set){object.__defineSetter__(property,descriptor.set)}return object};
