1e9014d98884caf3ad67d1503e8d43464ad2d327
Jonathan Neal <jonathantneal@hotmail.com>
2013-07-24 14:35:53 -0700

    Rename element.prototype.matchesSelector.js to Element.prototype.matchesSelector.js

--- minified/element.prototype.matchesSelector.js
+++
@@ -1,1 +0,0 @@
-Element.prototype.matchesSelector=function matchesSelector(selector){var element=this,elements=(element.document||element.ownerDocument).querySelectorAll(selector),index=0;while(elements[index]&&elements[index]!==element){++index}return elements[index]?true:false};
