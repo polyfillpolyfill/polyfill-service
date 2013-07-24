eedfd6781eac628b2648f760931801a577685f14
Jonathan Neal <jonathantneal@hotmail.com>
2013-07-24 14:35:17 -0700

    Rename object.keys.js to Object.keys.js

---
+++ minified/Object.keys.js
@@ -0,0 +1,1 @@
+Object.keys=function keys(object){var buffer=[],key;for(key in object){if(Object.prototype.hasOwnProperty.call(object,key)){buffer.push(key)}}return buffer};
--- minified/object.keys.js
+++
@@ -1,1 +0,0 @@
-Object.keys=function keys(object){var buffer=[],key;for(key in object){if(Object.prototype.hasOwnProperty.call(object,key)){buffer.push(key)}}return buffer};
