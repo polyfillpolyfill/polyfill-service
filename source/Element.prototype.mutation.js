1f4f48e34fd1f6a96a6b6d150b650937c7ccab20
Jonathan Neal <jonathantneal@hotmail.com>
2013-08-01 17:43:27 -0700

    DOM4 Mutation Methods
    prepend, append, before, after, replace, and remove

---
+++ source/Element.prototype.mutation.js
@@ -0,0 +1,58 @@
+(function () {
+	// http://dom.spec.whatwg.org/#mutation-method-macro
+	function mutation(nodes) {
+		if (!nodes.length) {
+			throw new Error('DOM Exception 8');
+		} else if (nodes.length === 1) {
+			return nodes[0];
+		} else {
+			var fragment = document.createDocumentFragment(), index, node;
+
+			for (index in arguments) {
+				node = arguments[index];
+
+				fragment.appendChild(typeof node === 'string' ? document.createTextNode(node) : node);
+			}
+
+			return fragment;
+		}
+	}
+
+	// http://dom.spec.whatwg.org/#dom-parentnode-prepend
+	Element.prototype.prepend = function prepend() {
+		this.insertBefore(mutation(arguments), this.firstChild);
+	};
+
+	// http://dom.spec.whatwg.org/#dom-parentnode-append
+	Element.prototype.append = function append() {
+		this.appendChild(mutation(arguments));
+	};
+
+	// http://dom.spec.whatwg.org/#dom-childnode-before
+	Element.prototype.before = function before() {
+		if (this.parentNode) {
+			this.parentNode.insertBefore(mutation(arguments), this);
+		}
+	};
+
+	// http://dom.spec.whatwg.org/#dom-childnode-after
+	Element.prototype.after = function append() {
+		if (this.parentNode) {
+			this.parentNode.insertBefore(mutation(arguments), this.nextSibling);
+		}
+	};
+
+	// http://dom.spec.whatwg.org/#dom-childnode-replace
+	Element.prototype.replace = function append() {
+		if (this.parentNode) {
+			this.parentNode.replaceChild(mutation(arguments), this.nextSibling);
+		}
+	};
+
+	// http://dom.spec.whatwg.org/#dom-childnode-remove
+	Element.prototype.remove = function remove() {
+		if (this.parentNode) {
+			this.parentNode.removeChild(this.nextSibling);
+		}
+	};
+})();
