3cf431df0b4644b0a971eb11629ac9a8e75a5e01
Jonathan Neal <jonathantneal@hotmail.com>
2013-08-28 22:16:01 -0700

    Date.prototype.toISOString
    typo

--- source/Date.prototype.toISOString.js
+++ source/Date.prototype.toISOString.js
@@ -1,5 +1,5 @@
 // Date.prototype.toISOString
-Date.prototype.toISOStrings = function toISOString() {
+Date.prototype.toISOString = function toISOString() {
 	var date = this;
 
 	return ((date.getUTCMonth() + 1) / 100 + date.toUTCString() + date / 1e3).replace(/..(..).+?(\d+)\D+(\d+).(\S+).*(...)/,'$3-$1-$2T$4.$5Z');
