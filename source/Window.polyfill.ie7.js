44f2fe656f208ccdad5626a1b9217f6e6a7267fb
Jonathan Neal <jonathantneal@hotmail.com>
2013-12-13 15:51:28 -0800

    IE6/7 Polyfill
    - Document/HTMLDocument Element/HTMLElement (#27)
    - Cleaner, faster query selector
    - Normalized commenting

--- source/Window.polyfill.ie7.js
+++ source/Window.polyfill.ie7.js
@@ -1,336 +1,265 @@
 // Window.polyfill
 (function () {
-	function getAllElements(element) {
-		for (var elements = [], all = element.all, index = 0, length = all.length; index < length; ++index) {
-			elements.push(all[index]);
-		}
-
-		return elements;
-	}
-
-	function getElementsByQuery(element, query) {
-		var
-		// get all descendant elements
-		allElements = getAllElements(element),
-		allMatchedElements = new NodeList(),
-		valid = '-?[_a-zA-Z]+[_a-zA-Z0-9-]*', // valid tag, id, or classname
-		matches = new RegExp([
-			'(' + valid + '|\\*)', // element [1]
-			'#(' + valid + ')', // id [2]
-			'\\.(' + valid + ')', // class [3]
-			'\\[(' + valid + ')]', // attribute [4]
-			'\\[(' + valid + ')([*$|~^]?=)?([\"\'])(.*?)\\7\\]', // attribute with value [5, 6, 7, 8]
-			':(' + valid + ')', // :pseudo [9]
-			'\\s*([>~+ ])\\s*' // modifier [10]
-		].join('|'), 'g'),
-		elements, match;
-
-		// loop through the queries
-		for (var queries = query.replace(/^\s+|\s+$/g, '').split(/\s*,\s*/), queryIndex = 0; query = queries[queryIndex]; ++queryIndex) {
-			elements = [].concat(allElements);
-
-			while (match = elements && matches.exec(query)) {
-				var
-				_element = match[1],
-				_id = match[2],
-				_class = match[3],
-				_attribute = match[4], _attributeName = match[5], _attributeType = match[6], _attributeValue = match[8],
-				_pseudo = match[9],
-				_modifier = match[10],
-				child;
-
-				if (_element === '*') {
-					continue;
-				}
-
-				for (var matchedElements = [], elementIndex = 0; element = elements[elementIndex]; ++elementIndex) {
-					if (_element) {
-						if (element.nodeName.toLowerCase() === _element.toLowerCase()) {
-							push.call(matchedElements, element);
-						}
-					}
-					else if (_id) {
-						if (element.id.toLowerCase() === _id.toLowerCase()) {
-							push.call(matchedElements, element);
-						}
-					}
-					else if (_class) {
-						if ((' ' + element.className.split(/\s+/).join(' ') + ' ').indexOf(' ' +_class + ' ') > -1) {
-							push.call(matchedElements, element);
-						}
-					}
-					else if (_pseudo === 'first-child') {
-						child = element.parentNode && element.parentNode.nodeType === 1 && element.parentNode.firstChild;
-
-						while (child && child.nodeType !== 1) {
-							child = child.nextSibling;
-						}
-
-						if (element === child) {
-							push.call(matchedElements, element);
-						}
-					}
-					else if (_pseudo === 'last-child') {
-						child = element.parentNode && element.parentNode.nodeType === 1 && element.parentNode.lastChild;
-
-						while (child && child.nodeType !== 1) {
-							child = child.previousSibling;
-						}
-
-						if (element === child) {
-							push.call(matchedElements, element);
-						}
-					}
-					else if (_attribute) {
-						if (element.attributes[_attribute] && element.attributes[_attribute].specified) {
-							push.call(matchedElements, element);
-						}
-					}
-					else if (_attributeName) {
-						if (element.attributes[_attributeName] && element.attributes[_attributeName].specified) {
-							var nodeValue =  element.attributes[_attributeName].nodeValue;
-
-							if (_attributeType === '=' && nodeValue === _attributeValue) {
-								push.call(matchedElements, element);
-							}
-							if (_attributeType === '*=' && nodeValue.indexOf(_attributeValue) > -1) {
-								push.call(matchedElements, element);
-							}
-							if (_attributeType === '^=' && nodeValue.indexOf(_attributeValue) === 0) {
-								push.call(matchedElements, element);
-							}
-							if (_attributeType === '$=' && nodeValue.indexOf(_attributeValue) === nodeValue.length - _attributeValue.length) {
-								push.call(matchedElements, element);
-							}
-							if (_attributeType === '~=' && (' ' + nodeValue.split(/\s+/).join(' ') + ' ').indexOf(' ' + _attributeValue + ' ') > -1) {
-								push.call(matchedElements, element);
-							}
-							if (_attributeType === '|=' && (' ' + nodeValue.split(/-+/).join(' ') + ' ').indexOf(' ' + _attributeValue + ' ') > -1) {
-								push.call(matchedElements, element);
-							}
-						}
-					}
-					else if (_pseudo === 'last-child') {
-						child = element.parentNode.lastChild;
-
-						while (child && child.nodeType !== 1) {
-							child = child.previousSibling;
-						}
-
-						if (element === child) {
-							push.call(matchedElements, element);
-						}
-					}
-					else if (_modifier === '>') {
-						for (var children = element.children, childIndex = 0; child = children[childIndex]; ++childIndex) {
-							if (child.nodeType === 1) {
-								push.call(matchedElements, child);
-							}
-						}
-					}
-					else if (_modifier === '~') {
-						do {
-							element = element.nextSibling;
-						} while (element && element.nodeType !== 1);
-
-						if (element) {
-							push.call(matchedElements, element);
-						}
-					}
-					else if (_modifier === '+') {
-						do {
-							element = element.nextSibling;
-						} while (element && element.nodeType !== 1);
-
-						if (element) {
-							push.call(matchedElements, element);
-						}
-					}
-					else if (_modifier === ' ') {
-						push.apply(matchedElements, getAllElements(element));
-					}
-					else {
-						throw new Error('SyntaxError: DOM Exception 12');
-					}
-				}
-
-				elements = matchedElements;
-			}
-
-			push.apply(allMatchedElements, elements);
-		}
-
-		return sort.call(allMatchedElements, function (a, b) {
-			return a.sourceIndex - b.sourceIndex;
-		});
-	}
+	/* jshint validthis:true */
+	'use strict';
 
 	var
+	// Window
 	Window = window.Window = function Window() {},
-	HTMLDocument = window.HTMLDocument = function HTMLDocument() {},
-	Element = window.Element = function Element() {},
+	// Document / HTMLDocument
+	Document = window.Document = window.HTMLDocument = function Document() {},
+	// Element / HTMLElement
+	Element = window.Element = window.HTMLElement = function Element() {},
+	// NodeList
 	NodeList = window.NodeList = function NodeList() {},
+
+	// Polyfill
 	polyfill = window.polyfill = function polyfill(instance, constructor) {
-		var each, index;
+		// if constructor does not match
+		if (instance.constructor !== constructor) {
+			var property, index;
 
-		if (!instance || instance.constructor === constructor) {
-			return instance;
-		} else {
+			// match constructor
 			instance.constructor = constructor;
-		}
 
-		for (each in constructor.prototype) {
-			instance[each] = constructor.prototype[each];
-		}
+			// copy constructor prototypes
+			for (property in constructor.prototype) {
+				instance[property] = constructor.prototype[property];
+			}
 
-		for (index = 0; constructor.polyfill[index]; ++index) {
-			constructor.polyfill[index].call(instance);
+			// run constructor polyfills
+			for (index = 0; constructor.polyfill[index]; ++index) {
+				constructor.polyfill[index].call(instance);
+			}
 		}
 
 		return instance;
 	},
-	push = Array.prototype.push,
-	sort = Array.prototype.sort;
 
-	NodeList.prototype.length = Array.prototype.length;
+	// CSS queries
+	MATCH = '(-?[_a-zA-Z]+[_a-zA-Z0-9-]*)',
+	REGEX = '^(?:' + MATCH + ')|^#' + MATCH + '|^\\.' + MATCH + '|^\\[' + MATCH + '(?:([*$|~^]?=)(["\'])((?:(?=(\\\\?))\\8.)*?)\\6)?\\]|^\\s*([>~+\\s])\\s*|^:(first|last)-child';
 
+	// Polyfill for Document / Element
+	function sharedPolyfill() {
+		var
+		// instance is either document or element
+		instance = this,
+		// cache native getters
+		getElementById = instance.getElementById,
+		getElementsByTagName = instance.getElementsByTagName;
+
+		// Document|Element.prototype.getElementById
+		instance.getElementById = function (id) {
+			return polyfill(getElementById.call(instance, id), Element);
+		};
+
+		// Document|Element.prototype.getElementsByClassname
+		instance.getElementsByClassname = function (className) {
+			return instance.querySelectorAll('.' + className);
+		};
+
+		// Document|Element.prototype.getElementsByTagName
+		instance.getElementsByTagName = function (tagName) {
+			for (var nodeList = new NodeList(), elements = getElementsByTagName.call(instance, tagName), index = 0, node; (node = elements[index]); ++index) {
+				Array.prototype.push.call(nodeList, polyfill(node, Element));
+			}
+
+			return nodeList;
+		};
+
+		// Document|Element.prototype.querySelectorAll
+		instance.querySelectorAll = function () {
+			var
+			selectors = String(arguments[0]).split(/\s*,\s*/),
+			elementsFiltered = [],
+			match;
+
+			for (var selectorIndex = 0, selector; (selector = selectors[selectorIndex]); ++selectorIndex) {
+				var elements = getElementsByTagName.call(instance, '*');
+
+				// match selector
+				while ((match = selector.match(REGEX))) {
+					// loop elements
+					for (var elementIndex = 0, element, elementsMatched = [], index = 0, childNodes; (element = elements[elementIndex]); ++elementIndex) {
+						// match if property validates
+						if (
+							// element
+							(match[1] && element.nodeName.toLowerCase() === match[1].toLowerCase()) ||
+							// id
+							(match[2] && element.id === match[2]) ||
+							// className
+							(match[3] && (' ' + element.className + ' ').indexOf(' ' + match[3] + ' ') !== -1)
+						) {
+							elementsMatched.push(element);
+						}
+
+						if (match[4]) {
+							var
+							attribute = element.attributes && element.attributes[match[4]] && element.attributes[match[4]].specified && element.attributes[match[4]],
+							nodeValue = attribute.nodeValue;
+
+							if (
+								(match[5] === '=' && nodeValue === match[7]) ||
+								(match[5] === '*=' && nodeValue.indexOf(match[7]) > -1) ||
+								(match[5] === '^=' && nodeValue.indexOf(match[7]) === 0) ||
+								(match[5] === '$=' && nodeValue.indexOf(match[7]) === nodeValue.length - match[7].length) ||
+								(match[5] === '~=' && (' ' + nodeValue.split(/\s+/).join(' ') + ' ').indexOf(' ' + match[7] + ' ') > -1) ||
+								(match[5] === '|=' && (' ' + nodeValue.split(/-+/).join(' ') + ' ').indexOf(' ' + match[7] + ' ') > -1)
+							) {
+								elementsMatched.push(element);
+							}
+						}
+
+						// match next
+						if (match[9] === '+') {
+							// next sibling must be element
+							do element = element.nextSibling;
+							while (element && element.nodeType !== 1);
+
+							if (element) {
+								elementsMatched.push();
+							}
+						}
+
+						// match after
+						if (match[9] === '~') {
+							// after siblings must be elements
+							do {
+								element = element.nextSibling;
+
+								if (element.nodeType === 1) {
+									elementsMatched.push(element);
+								}
+							}
+							while (element);
+						}
+
+						// match children
+						if (match[9] === '>') {
+							// children must be elements
+							for (childNodes = element.childNodes; childNodes && (element = childNodes[index]); ++index) {
+								if (element.nodeType === 1) {
+									elementsMatched.push(element);
+								}
+							}
+						}
+
+						// match descendants
+						if (match[9] === ' ') {
+							elementsMatched = elementsMatched.concat(getElementsByTagName.call(element, '*'));
+						}
+
+						// match if first child
+						if (match[10] === 'first') {
+							// get all siblings
+							childNodes = element.parentNode && element.parentNode.childNodes || [];
+
+							// move to first element
+							while (childNodes[index] && childNodes[index].nodeType !== 1) {
+								++index;
+							}
+
+							if (element === childNodes[index]) {
+								elementsMatched.push(element);
+							}
+						}
+
+						// match if last child
+						if (match[10] === 'last') {
+							childNodes = element.parentNode && element.parentNode.childNodes || [];
+							index = childNodes.length - 1;
+
+							// move to last element
+							while (childNodes[index] && childNodes[index].nodeType !== 1) {
+								--index;
+							}
+
+							if (element === childNodes[index]) {
+								elementsMatched.push(element);
+							}
+						}
+					}
+
+					// trim selector
+					selector = selector.slice(match[0].length);
+
+					// copy matched elements
+					elements = elementsMatched;
+				}
+
+				elementsFiltered = elementsFiltered.concat(elements);
+			}
+
+			// sort and polyfill elements
+			var nodeList = new NodeList();
+
+			Array.prototype.push.apply(nodeList, Array.prototype.sort.call(elementsFiltered, function (a, b) {
+				return a.sourceIndex - b.sourceIndex;
+			}));
+
+			return nodeList;
+		};
+
+		// Document|Element.prototype.querySelector
+		instance.querySelector = function () {
+			return instance.querySelectorAll.apply(instance, arguments)[0] || null;
+		};
+	}
+
+	// Configure Window
 	Window.polyfill = [function () {
 		var window = this;
 
-		polyfill(window.document, HTMLDocument);
+		polyfill(window.document, Document);
 	}];
 
-	HTMLDocument.polyfill = [function () {
+	// Configure Document / HTMLDocument
+	Document.polyfill = [sharedPolyfill, function () {
 		var
 		document = this,
 		createDocumentFragment = document.createDocumentFragment,
-		createElement = document.createElement,
-		getElementById = document.getElementById,
-		getElementsByTagName = document.getElementsByTagName;
+		createElement = document.createElement;
 
-		// HTMLDocument.prototype.head
-		document.head = document.getElementsByTagName('head')[0] || document.createElement('head');
+		// Document.prototype.head
+		document.head = document.getElementsByTagName('head')[0] || null;
 
-		// HTMLDocument.prototype.createDocumentFragment
+		// Document.prototype.createDocumentFragment
 		document.createDocumentFragment = function () {
-			return polyfill(createDocumentFragment(), HTMLDocument);
+			return polyfill(createDocumentFragment.call(document), Document);
 		};
 
-		// HTMLDocument.prototype.createElement
+		// Document.prototype.createElement
 		document.createElement = function (tagName) {
-			return polyfill(createElement(tagName), Element);
+			return polyfill(createElement.call(document, tagName), Element);
 		};
 
-		// HTMLDocument.prototype.getElementById
-		document.getElementById = function (id) {
-			return polyfill(getElementById(id), Element);
-		};
-
-		// HTMLDocument.prototype.getElementsByClassName
-		document.getElementsByClassName = function (className) {
-			for (var nodeList = new NodeList(), elements = getElementsByTagName('*'), index = 0, element; element = elements[index]; ++index) {
-				if ((' ' + element.className.split(/\s+/).join(' ') + ' ').indexOf(' '+className+' ') > -1) {
-					push.call(nodeList, polyfill(element, Element));
-				}
-			}
-
-			return nodeList;
-		};
-
-		// HTMLDocument.prototype.getElementsByTagName
-		document.getElementsByTagName = function (tagName) {
-			for (var nodeList = new NodeList(), elements = getElementsByTagName(tagName), index = 0, element; element = elements[index]; ++index) {
-				push.call(nodeList, polyfill(element, Element));
-			}
-
-			return nodeList;
-		};
-
-		// HTMLDocument.prototype.querySelector
-		document.querySelector = function (query) {
-			var elements = getElementsByQuery(document, query);
-
-			return elements[0] && polyfill(elements[0], Element) || null;
-		};
-
-		// HTMLDocument.prototype.querySelectorAll
-		document.querySelectorAll = function (query) {
-			for (var elements = getElementsByQuery(document, query), index = 0, element; element = elements[index]; ++index) {
-				polyfill(element, Element);
-			}
-
-			return elements;
-		};
-
-		// HTML Shiving
-		'abbr article aside audio bdi canvas data datalist details figcaption figure footer header hgroup main mark meter nav output progress section subline summary time video'.replace(/\w+/g, function (nodeName) {
-			document.createElement(nodeName);
+		// Shiv elements
+		'abbr article aside audio bdi canvas data datalist details figcaption figure footer header hgroup main mark meter nav output progress section subhead summary time video'.replace(/\w+/g, function (nodeName) {
+			createElement.call(document, nodeName);
 		});
 
-		// Element
+		// Polyfill Element (now)
 		for (var all = document.all, index = 0; all[index]; ++index) {
 			polyfill(all[index], Element);
 		}
 
+		// Polyfill Element (on document ready)
 		document.addEventListener('DOMContentLoaded', function () {
-			Array.prototype.forEach.call(document.all, function (element) {
+			for (var elements = document.getElementsByTagName('*'), index = 0, element; (element = elements[index]); ++index) {
 				polyfill(element, Element);
-			});
+			}
 		});
 	}];
 
-	Element.polyfill = [function () {
+	// Configure Element, HTMLElement
+	Element.polyfill = [sharedPolyfill, function () {
 		var
 		element = this,
-		getElementById = element.getElementById,
-		getElementsByTagName = element.getElementsByTagName,
 		setAttribute = element.setAttribute;
 
-		// <element>.getElementById
-		element.getElementById = function (id) {
-			return polyfill(getElementById(id), Element);
-		};
-
-		// <element>.getElementsByClassName
-		element.getElementsByClassName = function (className) {
-			for (var nodeList = new NodeList(), elements = getElementsByTagName('*'), index = 0; element = elements[index]; ++index) {
-				if ((' ' + element.className.split(/\s+/).join(' ') + ' ').indexOf(' ' + className + ' ') > -1) {
-					push.call(nodeList, polyfill(element, Element));
-				}
-			}
-
-			return nodeList;
-		};
-
-		// <element>.getElementsByTagName
-		element.getElementsByTagName = function (tagName) {
-			for (var nodeList = new NodeList(), elements = getElementsByTagName(tagName), index = 0; element = elements[index]; ++index) {
-				push.call(nodeList, polyfill(element, Element));
-			}
-
-			return nodeList;
-		};
-
-		// <element>.querySelector
-		element.querySelector = function (query) {
-			var elements = getElementsByQuery(element, query);
-
-			return elements[0] && polyfill(elements[0], Element) || null;
-		};
-
-		// <element>.querySelectorAll
-		element.querySelectorAll = function (query) {
-			for (var elements = getElementsByQuery(element, query), index = 0; element = elements[index]; ++index) {
-				polyfill(element, Element);
-			}
-
-			return elements;
-		};
-
-		// <element>.setAttribute
+		// Element.prototype.setAttribute
 		element.setAttribute = function (name, value) {
-			element = element || this;
-
 			if (name === 'class') {
 				element.className = value !== undefined && value !== null ? String(value) : '';
 			}
@@ -338,11 +267,12 @@
 			return setAttribute(name, value);
 		};
 
-		// <element>.hasAttribute
+		// Element.prototype.hasAttribute
 		element.hasAttribute = function (name) {
-			element = element || this;
-
 			return element.getAttribute(name) !== null;
 		};
 	}];
+
+	// Configure NodeList
+	NodeList.prototype.length = Array.prototype.length;
 })();
