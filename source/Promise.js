d20ca356b744a8224a15abfacac9a445fa9089b4
Jonathan Neal <jonathantneal@hotmail.com>
2013-12-31 09:20:11 -0800

    ES6 Promise

---
+++ source/Promise.js
@@ -0,0 +1,73 @@
+(function () {
+	'use strict';
+
+	function Promise(resolver) {
+		var
+		self = this,
+		then = self.then = function () {
+			return Promise.prototype.then.apply(self, arguments);
+		};
+
+		then.fulfilled = [];
+		then.rejected = [];
+
+		function timeout(state, object) {
+			then.state = 'pending';
+
+			if (then[state].length) setTimeout(function () {
+				timeout(state, then.value = then[state].shift().call(self, object));
+			}, 0);
+			else then.state = state;
+		}
+
+		then.fulfill = function (object) {
+			timeout('fulfilled', object);
+		};
+
+		then.reject = function (object) {
+			timeout('rejected', object);
+		};
+
+		resolver.call(self, then.fulfill, then.reject);
+
+		return self;
+	}
+
+	Promise.prototype.then = function (onFulfilled, onRejected) {
+		if (onFulfilled) this.then.fulfilled.push(onFulfilled);
+		if (onRejected) this.then.rejected.push(onRejected);
+
+		if (this.then.state === 'fulfilled') this.then.fulfill(this.then.value);
+
+		return this;
+	};
+
+	Promise.prototype.catch = function (onRejected) {
+		if (onRejected) this.then.rejected.push(onRejected);
+
+		return this;
+	};
+
+	Promise.all = function () {
+		var
+		args = Array.prototype.slice.call(arguments),
+		countdown = args.length;
+
+		function process(promise, fulfill, reject) {
+			promise.then(function onfulfilled(value) {
+				if (promise.then.fulfilled.length > 1) promise.then(onfulfilled);
+				else if (!--countdown) fulfill(value);
+
+				return value;
+			}, function (value) {
+				reject(value);
+			});
+		}
+
+		return new Promise(function (fulfill, reject) {
+			while (args.length) process(args.shift(), fulfill, reject);
+		});
+	};
+
+	window.Promise = Promise;
+})();
