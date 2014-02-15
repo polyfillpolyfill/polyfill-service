7c9fb0d2083d7002bb6cafa026a1bb47144bf935
Jonathan Neal <jonathantneal@hotmail.com>
2014-02-14 23:03:13 -0800

    Remove CSS polyfills
    At least, for now…

--- index.js
+++ index.js
@@ -2,12 +2,10 @@ var fs = require('fs');
 var join = require('path').join;
 
 exports.agent = {
-  js: require('./agent.js.json'),
-  css: require('./agent.css.json'),
+	js: require('./agent.js.json')
 };
 
 exports.useragent = require('./agent.json');
-exports.normalize = require('./normalize.json');
 
 // map lookup for sources with aliases
 // source[name] = js string
@@ -15,7 +13,7 @@ var source = exports.source = {};
 var sourceFolder = join(__dirname, 'source');
 
 fs.readdirSync(sourceFolder).forEach(function (filename) {
-  if (filename[0] === '.') return;
+	if (filename[0] === '.') return;
 
-  source[filename.replace(/\.js$/, '')] = fs.readFileSync(join(sourceFolder, filename), 'utf8');
+	source[filename.replace(/\.js$/, '')] = fs.readFileSync(join(sourceFolder, filename), 'utf8');
 });
