d899df2921ce62b2c7a155f8995fb8632a71e3a2
SamGiles <samuel.giles@ft.com>
2014-06-25 18:56:38 +0100

    Moving function into its own file

--- app.js
+++ app.js
@@ -1,18 +1,10 @@
-var express   = require('express');
-	app       = express(),
-	polyfills = require('./index'),
-	useragent = require('useragent'),
-	uglify    = require('uglify-js'),
-	AliasResolver = require('./aliases'),
-	lookupAgent = require('./agent');
+var polyfillio = require('./index'),
+	express = require('express'),
+	app = express();
 
 'use strict';
 
-// Load additional useragent features: primarily to use: agent.satisfies to
-// test a browser version against a semver string
-require('useragent/features');
-
-var aliasResolver = AliasResolver.createDefault(polyfills),
+var aliasResolver = AliasResolver.createDefault(polyfillio.aliases),
 	port = 3000;
 
 app.get(/^\/polyfill(\.\w+)(\.\w+)?/, function(req, res) {
@@ -20,10 +12,10 @@ app.get(/^\/polyfill(\.\w+)(\.\w+)?/, function(req, res) {
 	var firstParameter = req.params[0].toLowerCase(),
 		minified =  firstParameter === '.min',
 		fileExtension = minified ? req.params[1].toLowerCase() : firstParameter,
-		maybePolyfills   = parseRequestedPolyfills(req.query.maybe || '', ['maybe'])
+		maybePolyfills   = parseRequestedPolyfills(req.query.maybe || '', ['maybe']),
 		defaultPolyfills = parseRequestedPolyfills(req.query["default"] || '');
 
-	var polyfill = getPolyfillString({
+	var polyfill = polyfillio.getPolyfills({
 		polyfills: maybePolyfills.concat(defaultPolyfills) ,
 		extension: fileExtension,
 		minify: minified,
@@ -41,67 +33,6 @@ app.get(/^\/polyfill(\.\w+)(\.\w+)?/, function(req, res) {
 	res.send(polyfill);
 });
 
-function getPolyfillString(options) {
-	var ua = useragent.lookup(options.uaString),
-		uaFamily = lookupAgent(ua.family.toLowerCase()),
-
-		// Holds the strings that will be built into the explainer comment that is
-		// placed before the polyfill code.
-		explainerComment = [
-			options.url,
-			'Detected ' + uaFamily + '/' + ua.toVersion()
-		],
-
-		// Holds the source code for each polyfill that should be sent to the
-		// client
-		currentPolyfills = {};
-
-	// Browsers don't really use semantic versioning but tend to at least
-	// have a major and minor version.  This normalises the patch version so that
-	// semantic version comparison is consistent.
-	if (!isNumeric(ua.patch)) {
-		ua.patch = '0';
-	}
-
-
-	// Filter out polyfills that do not match the browser version in the UA
-	// with their config
-	var includePolyfills = options.polyfills.forEach(function(polyfill) {
-		var polyfillSource = polyfills.sources[polyfill.name];
-
-		if (!polyfillSource) {
-			explainerComment.push('"' + polyfill.name + '" does not match any polyfills');
-			return;
-		}
-
-		if (polyfill.flags.indexOf('maybe') !== -1) {
-			var polyfillConfig = polyfillSource.config;
-
-			if (!(polyfillConfig && polyfillConfig.browsers)) {
-				return;
-			}
-
-
-			var browserVersion = polyfillConfig.browsers[uaFamily];
-			if (!(browserVersion && ua.satisfies(browserVersion))) {
-				return;
-			}
-		}
-
-		explainerComment.push(polyfill.name + ' - ' + polyfill.aliasOf + ' (LICENSE TODO)');
-		currentPolyfills[polyfill.name] = polyfillSource.file;
-
-	});
-
-	var builtExplainerComment = '/* ' + explainerComment.join('\n * ') + '\n */\n';
-	var builtPolyfillString = objectJoin(currentPolyfills, '\n');
-
-	if (options.minified) {
-		builtPolyfillString = uglify.minify(builtPolyfillString, {fromString: true}).code;
-	}
-
-	return builtExplainerComment + builtPolyfillString;
-}
 
 app.listen(port);
 
@@ -119,29 +50,3 @@ function parseRequestedPolyfills(polyfillList, additionalFlags) {
 	});
 }
 
-/**
- * jQuery: isNumeric
- * https://github.com/jquery/jquery/blob/bbdfbb4ee859fe9319b348d88120ddc2c9efbd63/src/core.js#L212
- */
-function isNumeric(obj) {
-	return !Array.isArray(obj) && (((obj- parseFloat(obj)) + 1) >= 0);
-}
-
-/**
- * For each key in an object/hash map, join the value associated with the
- * key.  This allows you to perform a join operation over what is effectively
- * a set.
- *
- * Example:
- *
- * objectJoin({ x: 'A', y: 'B' }, ',');
- *
- * Result:
- * 'A,B'
- *
- * In Harmony (ES6) Just use 'Set':
- * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Set
- */
-function objectJoin(obj, joinString) {
-	return Object.keys(obj).map(function(key) { return obj[key]; }).join(joinString);
-}
--- index.js
+++ index.js
@@ -1,36 +1,127 @@
 var fs = require('fs'),
 	path = require('path');
+	useragent = require('useragent'),
+	uglify    = require('uglify-js'),
+	AliasResolver = require('./aliases'),
+	lookupAgent = require('./agent');
+
+// Load additional useragent features: primarily to use: agent.satisfies to
+// test a browser version against a semver string
+require('useragent/features');
+
+"use strict";
 
 var polyfillSourceFolder = path.join(__dirname, 'source');
 
-module.exports = (function initialisePolyfillInfo() {
-	var sources = {},
-		aliases = {};
+var sources = {},
+	aliases = {};
 
-	fs.readdirSync(polyfillSourceFolder).forEach(function (polyfillName) {
+fs.readdirSync(polyfillSourceFolder).forEach(function (polyfillName) {
 
-		var config = require(path.join(polyfillSourceFolder, polyfillName, 'config.json')),
-			polyfillSourcePath = path.join(polyfillSourceFolder, polyfillName, 'polyfill.js');
+	var config = require(path.join(polyfillSourceFolder, polyfillName, 'config.json')),
+		polyfillSourcePath = path.join(polyfillSourceFolder, polyfillName, 'polyfill.js');
 
-		// Read each file and store in a map for quick lookup
-		sources[polyfillName] = {
-			file: fs.readFileSync(polyfillSourcePath, 'utf8'),
-			config: config
-		};
+	// Read each file and store in a map for quick lookup
+	sources[polyfillName] = {
+		file: fs.readFileSync(polyfillSourcePath, 'utf8'),
+		config: config
+	};
 
-		// Store alias names in a map for efficient lookup, mapping aliases to
-		// polyfillNames.  An alias can map to many polyfill names. So a group
-		// of polyfills can be aliased under the same name.  This is why an
-		// array is created and used for the value in the map.
-		config.aliases = config.aliases || [];
-		config.aliases.forEach(function(aliasName) {
-			if (aliases[aliasName]) {
-				aliases[aliasName].push(polyfillName);
-			} else {
-				aliases[aliasName] = [ polyfillName ];
+	// Store alias names in a map for efficient lookup, mapping aliases to
+	// polyfillNames.  An alias can map to many polyfill names. So a group
+	// of polyfills can be aliased under the same name.  This is why an
+	// array is created and used for the value in the map.
+	config.aliases = config.aliases || [];
+	config.aliases.forEach(function(aliasName) {
+		if (aliases[aliasName]) {
+			aliases[aliasName].push(polyfillName);
+		} else {
+			aliases[aliasName] = [ polyfillName ];
+		}
+	});
+});
+
+function getPolyfillString(options) {
+	var ua = useragent.lookup(options.uaString),
+		uaFamily = lookupAgent(ua.family.toLowerCase()),
+		explainerComment = [
+			options.url,
+			'Detected ' + uaFamily + '/' + ua.toVersion()
+		],
+		currentPolyfills = {};
+
+	// Browsers don't really use semantic versioning but tend to at least
+	// have a major and minor version.  This normalises the patch version so that
+	// semantic version comparison is consistent.
+	if (!isNumeric(ua.patch)) {
+		ua.patch = '0';
+	}
+
+	var includePolyfills = options.polyfills.forEach(function(polyfill) {
+		var polyfillSource = sources[polyfill.name];
+
+		if (!polyfillSource) {
+			explainerComment.push('"' + polyfill.name + '" does not match any polyfills');
+			return;
+		}
+
+		if (polyfill.flags.indexOf('maybe') !== -1) {
+			var polyfillConfig = polyfillSource.config;
+
+			if (!(polyfillConfig && polyfillConfig.browsers)) {
+				return;
 			}
-		});
+
+
+			var browserVersion = polyfillConfig.browsers[uaFamily];
+			if (!(browserVersion && ua.satisfies(browserVersion))) {
+				return;
+			}
+		}
+
+		explainerComment.push(polyfill.name + ' - ' + polyfill.aliasOf + ' (LICENSE TODO)');
+		currentPolyfills[polyfill.name] = polyfillSource.file;
+
 	});
 
-	return { sources: sources, aliases: aliases };
-}());
+	var builtExplainerComment = '/* ' + explainerComment.join('\n * ') + '\n */\n';
+	var builtPolyfillString = objectJoin(currentPolyfills, '\n');
+
+	if (options.minified) {
+		builtPolyfillString = uglify.minify(builtPolyfillString, {fromString: true}).code;
+	}
+
+	return builtExplainerComment + builtPolyfillString;
+}
+
+module.exports = {
+	getPolyfills: getPolyfillString,
+	aliases: aliases
+};
+
+/**
+ * jQuery: isNumeric
+ * https://github.com/jquery/jquery/blob/bbdfbb4ee859fe9319b348d88120ddc2c9efbd63/src/core.js#L212
+ */
+function isNumeric(obj) {
+	return !Array.isArray(obj) && (((obj- parseFloat(obj)) + 1) >= 0);
+}
+
+/**
+ * For each key in an object/hash map, join the value associated with the
+ * key.  This allows you to perform a join operation over what is effectively
+ * a set.
+ *
+ * Example:
+ *
+ * objectJoin({ x: 'A', y: 'B' }, ',');
+ *
+ * Result:
+ * 'A,B'
+ *
+ * In Harmony (ES6) Just use 'Set':
+ * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Set
+ */
+function objectJoin(obj, joinString) {
+	return Object.keys(obj).map(function(key) { return obj[key]; }).join(joinString);
+}
