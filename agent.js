40f8e3b8b811bc4b7cca6eeb6b7e6da8e78e197a
SamGiles <samuel.giles@ft.com>
2014-06-25 11:20:01 +0100

    Map agents to a normalised name

---
+++ agent.js
@@ -0,0 +1,23 @@
+/**
+ * Helper module used to alias browser names parsed out of the 'useragent'
+ * module to the equivalent names used in the config.json for each polyfill.
+ * Should always be lowercase values as it makes comparisons easier.
+ */
+
+var agentlist = {
+	"chromium": "chrome",
+	"mobile safari": "safari ios"
+};
+
+
+/**
+ * Return a normalized name for the user agent, if there is no normalized name
+ * the name is returned as is, without modification.
+ *
+ * @param {string} agentname The name of the user agent to normalize.
+ * @returns {string} A normalized user agent name.
+ */
+module.exports = function(agentname) {
+
+	return agentlist[agentname] || agentname;
+};
