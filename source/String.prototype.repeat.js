30990466f2a391ae951501369e7bf7a0ff744e83
SamGiles <samuel.giles@ft.com>
2014-06-27 10:16:16 +0100

    Update repeat to master, remove duplicate

--- source/String.prototype.repeat.js
+++
@@ -1,4 +0,0 @@
-// String.prototype.repeat
-String.prototype.repeat = function (count) {
-	return new Array((count || 0) + 1).join(this);
-};
--- source/String.prototype.repeat/polyfill.js
+++ source/String.prototype.repeat/polyfill.js
@@ -1,6 +1,4 @@
 // String.prototype.repeat
-String.prototype.repeat = function repeat(count) {
-	if (count < 0 || count >= Infinity) throw new Error('Out of range');
-
-	return (new Array(parseInt(count + 1, 10))).join(this);
+String.prototype.repeat = function (count) {
+	return new Array((count || 0) + 1).join(this);
 };
