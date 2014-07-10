e4081ebf9358bc210a478b413364f8d6115b0e49
Marat Dulin <mdevils@yandex.ru>
2014-07-10 16:36:56 +0400

    Fixing wrong Function.prototype.bind behavior with Array arguments

--- source/Function.prototype.bind.js
+++ source/Function.prototype.bind.js
@@ -1,7 +1,14 @@
 // Function.prototype.bind
 Function.prototype.bind = function bind(scope) {
-	var callback = this, prepend = Array.prototype.slice.call(arguments, 1), Constructor = function () {}, bound = function () {
-		return callback.apply(this instanceof Constructor && scope ? this : scope, Array.prototype.concat.apply(prepend, arguments));
+	var
+	callback = this,
+	prepend = Array.prototype.slice.call(arguments, 1),
+	Constructor = function () {},
+	bound = function () {
+		return callback.apply(
+			this instanceof Constructor && scope ? this : scope,
+			prepend.concat(Array.prototype.slice.call(arguments, 0))
+		);
 	};
 
 	Constructor.prototype = bound.prototype = callback.prototype;
