2957fa4bac644f1e38f72bbf336c3c1fb4c65aad
SamGiles <samuel.giles@ft.com>
2014-07-14 12:16:27 +0100

    Add origami __about endpoints (#14 and #15)

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 var polyfillio = require('./index'),
 	express = require('express'),
-	app = express();
+	app = express(),
+	packagejson = require('./package.json'),
+	origamijson = require('./origami.json');
 
 'use strict';
 
@@ -8,8 +10,34 @@ var aliasResolver = AliasResolver.createDefault(polyfillio.aliases),
 	port = 3000;
 
 
+/** Return information about the web service
+ *  See Origami spec:
+ *  	http://origami.ft.com/docs/syntax/web-service-index/
+ */
 app.get(/^\/__about$/, function(req, res) {
-	res.send("__about");
+	var info = {
+		"name": "polyfill-service",
+		"versions": [
+			"/v1/"
+		]
+	};
+	res.set("Content-Type", "application/json");
+	res.send(JSON.stringify(info));
+});
+
+
+app.get(/^\/v1\/__about$/, function(req, res) {
+	var info = {
+		"name": "polyfill-service",
+		"apiVersion": 1,
+		"appVersion": packagejson.version,
+		"dateCreated": '2014-07-14T10:28:45Z',
+		"support": origamijson.support,
+		"supportStatus": "active"
+	};
+
+	res.set("Content-Type", "application/json");
+	res.send(JSON.stringify(info));
 });
 
 app.get(/^\/v1\/polyfill(\.\w+)(\.\w+)?/, function(req, res) {
