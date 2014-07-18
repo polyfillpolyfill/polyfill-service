56fa70836459069997c81622ae8e4068f67f124f
SamGiles <samuel.giles@ft.com>
2014-07-18 14:51:55 +0100

    Manually merge in the 'add __healthcheck commit

--- service/index.js
+++ service/index.js
@@ -47,6 +47,31 @@ app.get(/^\/__gtg$/, function(req, res) {
 	res.send("OK");
 });
 
+app.get(/^\/__health$/, function(req, res) {
+    var info = {
+        "schemaVersion": 1,
+        // TODO: This should match a service name in CMDB
+        "name": "polyfill-service",
+        "description": "Open API endpoint for retrieving Javascript polyfill libraries based on the user's user agent.  More at http://github.com/Financial-Times/polyfill-service.",
+        "checks": [
+            {
+                "name": "Server is up",
+                "ok": "true",
+                "severity": 2,
+                "businessImpact": "Web page rendering may degrade for customers using certain browsers. Dynamic client side behaviour is likely to fail.",
+                "technicalSummary": "Tests that the Node JS process is up.",
+                "panicGuide": "This application consists of Node JS processes on any number of nodes in an environment.  The process must have read permissions on files within its deployment.",
+                "checkOutput": "None",
+                "lastUpdated": new Date().toISOString()
+            }
+        ],
+    };
+
+    res.set('Cache-Control', 'no-store');
+    res.set('Content-Type', 'application/json; charset=utf-8');
+    res.send(JSON.stringify(info));
+});
+
 app.get(/^\/v1\/polyfill(\.\w+)(\.\w+)?/, function(req, res) {
 
 	var firstParameter = req.params[0].toLowerCase(),
