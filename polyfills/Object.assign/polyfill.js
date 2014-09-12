929ce80270c2ed886b09a1b25242551b3ff2f461
Jonathan Neal <jonathantneal@hotmail.com>
2014-09-12 06:41:31 -0400

    Object.assign
    http://people.mozilla.org/~jorendorff/es6-draft.html#sec-object.assign

---
+++ polyfills/Object.assign/polyfill.js
@@ -0,0 +1,14 @@
+// Object.assign
+Object.assign = function assign(target, source) {
+	for (var index = 1, key; index in arguments; ++index) {
+		source = arguments[index];
+
+		for (key in source) {
+			if (Object.prototype.hasOwnProperty.call(source, key)) {
+				target[key] = source[key];
+			}
+		}
+	}
+
+	return target;
+};
