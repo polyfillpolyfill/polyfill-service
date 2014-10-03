aa484180e149c68a11a3f9035caa88d9670a2c33
SamGiles <samuel.giles@ft.com>
2014-10-03 15:09:33 +0100

    Make sure the sauce connect tunnel is actually set up (passing true as the final parameter) and make sure jobs are annotated with the tunnel id

--- tasks/saucelabs.js
+++ tasks/saucelabs.js
@@ -8,9 +8,12 @@ module.exports = function(grunt) {
 	var SauceTunnel = require('sauce-tunnel');
 
 	grunt.registerMultiTask('saucelabs', 'Perform tests on Sauce', function() {
+		var done = this.async();
+		var nameSuffix = process.env.TRAVIS_BUILD_NUMBER ? "-travis-build:" + process.env.TRAVIS_BUILD_NUMBER : ':' + (new Date()).toUTCString();
+		var tunnelId = 'SauceTunnel-' + nameSuffix;
 
 		var options = this.options({
-			name: 'grunt-test',
+			name: 'grunt-test' + nameSuffix,
 			build: null,
 			user: process.env.SAUCE_USER_NAME,
 			key: process.env.SAUCE_API_KEY,
@@ -22,30 +25,38 @@ module.exports = function(grunt) {
 			screenshots: false
 		});
 
-		var done = this.async();
 
 		var batch = new Batch();
 		batch.concurrency(options.concurrency);
 
+		var tunnel = new SauceTunnel(options.user, options.key, tunnelId, true);
+
 		grunt.log.writeln('Opening tunnel to Sauce Labs');
-		var tunnel = new SauceTunnel(options.user, options.key);
 		tunnel.start(function(status) {
 
-			if (status !== true) done(status);
+			console.log("Tunnel Started");
+			if (status !== true)  {
+				done(status);
+			}
 
 			options.browsers.forEach(function(conf) {
+
 				conf['name'] = options.name;
 				conf['record-video'] = options.video;
 				conf['record-screenshots'] = options.screenshots;
+				conf['tunnel-identifier'] = tunnelId;
 				if (options.tags) conf['tags'] = options.tags;
 				if (options.build) conf['build'] = options.build;
 
 				batch.push(function(done) {
 
 					// initialize remote connection to Sauce Labs
-					grunt.log.writeln('Running %s %s %s', conf.browserName, conf.version, conf.platform);
-					var browser = wd.remote("127.0.0.1", 4445, options.user, options.key);
+					var sauceConnectPort = 4445;
+					var browser = wd.remote("127.0.0.1", sauceConnectPort, options.user, options.key);
+					grunt.log.writeln('Initialising browser %s %s %s', conf.browserName, conf.version, conf.platform);
+
 					browser.init(conf, function() {
+						grunt.log.writeln('Running %s %s %s', conf.browserName, conf.version, conf.platform);
 
 						grunt.log.writeln('Open %s', options.url);
 						browser.get(options.url, function(err) {
@@ -60,8 +71,8 @@ module.exports = function(grunt) {
 
 									if (err) return done(err);
 
-									console.log("Results are in", data);
 
+									console.log("Setting job results on Sauce");
 									// Update Sauce Labs job with custom test data
 									request({
 										method: "PUT",
@@ -71,15 +82,16 @@ module.exports = function(grunt) {
 											'custom-data': { custom: data },
 											'passed': !data.failed
 										})
-									}, function (/*error, response, body*/) {
+									}, function (err, res, body) {
 
 										// TODO: write test data to disk, maybe /test/results/<browser>.json?
 										// TODO: Then another grunt task to read all the result data and output a single compatibility data file in /docs/assets/compat.json?  We then gitignore the test/results directory but commit the docs/assets dir.
+										console.log("Test data written to Sauce");
 
 										browser.quit();
 										tunnel.stop(function() {
-											done(null, res);
-										})
+											done(!!err);
+										});
 									});
 								});
 							}());
@@ -90,7 +102,6 @@ module.exports = function(grunt) {
 			});
 
 			batch.end(done);
-
 		});
 	});
 };
