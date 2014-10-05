40e7bca169013f7f3ab29526588cda7f6cecabfd
Andrew Betts <andrew.betts@ft.com>
2014-10-05 18:18:52 +0100

    Deploy the current branch, may not be master, + better docs

--- deploy.sh
+++ deploy.sh
@@ -1,20 +1,29 @@
 #!/bin/bash
 
-# Delete current deploy branch
+# Store current branch and version
+BRANCH=`git symbolic-ref -q --short HEAD`
+VERSION=`git describe`
+
+# Delete any existing deploy branch
 git branch -D deploy
 
-# Create new deploy branch based on master
+# Create new deploy branch based on current branch
 git checkout -b deploy
 
-# Perform pre-deploy build steps
-git describe > .semver
+############################################
+# Perform pre-deploy build steps - could run a grunt task here
+
+echo $VERSION > .semver
+
+#
+############################################
 
 # Commit changes
 git add -A
-git commit -m "Deploying to Heroku"
+git commit -m "Deploying $VERSION to Heroku"
 
 # Push it up to heroku, the -f ensures that heroku won't complain
 git push herokuqa -f deploy:master
 
-# Switch it back to master
-git checkout master
+# Switch it back to the branch we were working on
+git checkout $BRANCH
