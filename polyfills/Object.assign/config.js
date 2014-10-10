7b7eeb528ee2f192d98b996d8736ef39d8ca6a4a
Jonathan Neal <jonathantneal@hotmail.com>
2014-10-10 13:03:45 -0400

    Object.assign config

--- polyfills/Object.assign/config.js
+++ polyfills/Object.assign/config.js
@@ -1,1 +1,18 @@
-{}
+{
+	"aliases": [
+		"es6"
+	],
+	"browsers": {
+		"android": "*",
+		"bb": "*",
+		"chrome": "*",
+		"firefox": "1 - 33",
+		"ie": "*",
+		"ios_chr": "*",
+		"ios_saf": "*",
+		"opera": "*",
+		"op_mob": "*",
+		"op_mini": "*",
+		"safari": "*"
+	}
+}
