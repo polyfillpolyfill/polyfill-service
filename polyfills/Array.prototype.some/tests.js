93180d28d9e60869b922e25f50e52f23538caf54
SamGiles <samuel.giles@ft.com>
2014-10-20 16:35:46 +0100

    Test that the third parameter to the function is a reference to the array

--- polyfills/Array.prototype.some/tests.js
+++ polyfills/Array.prototype.some/tests.js
@@ -20,5 +20,17 @@ it("Should accept an optional 'this' argument as its second argument which becom
 	array.some(function(value) {
 		expect(this).to.be(mockThis);
 		expect(this.foo).to.be(mockThis.foo);
+		return false;
 	}, mockThis);
+});
+
+it("Should pass a reference to the array as the third parameter to the function argument", function() {
+	var array = [10];
+
+	array.some(function(value, index, object) {
+		object[0] = 100;
+		return false;
+	});
+
+	expect(array[0]).to.be(100);
 });
