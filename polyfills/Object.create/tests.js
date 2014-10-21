db5203b8340841e8812271990efb2bc796e36e9c
SamGiles <samuel.giles@ft.com>
2014-10-21 16:38:44 +0100

    Define properties

--- polyfills/Object.create/tests.js
+++ polyfills/Object.create/tests.js
@@ -47,3 +47,19 @@ it("Should set the prototype of the passed-in object and add new properties", fu
 	expect(b.x).to.be(undefined);
 	expect(b.y).to.be(undefined);
 });
+
+// http://www.ecma-international.org/ecma-262/5.1/#sec-15.2.3.5
+// https://github.com/tc39/test262/blob/master/test/suite/ch15/15.2/15.2.3/15.2.3.5/15.2.3.5-4-100.js
+it("If the second argument is present and not undefined, add own properties to result object as if by calling the standard built-in function Object.defineProperties with arguments returned object and Properties.", function() {
+	var newObj = Object.create({}, {
+		prop: {
+			value: "ownDataProperty"
+		}
+	});
+
+	var result1 = newObj.hasOwnProperty("prop");
+	delete newObj.prop;
+	var result2 = newObj.hasOwnProperty("prop");
+	expect(result1).to.be(true);
+	expect(result2).to.be(true);
+});
