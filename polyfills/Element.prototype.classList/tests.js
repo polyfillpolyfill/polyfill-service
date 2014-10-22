76e1a8d6b9afff00082a6dcfaec9bfd234f519aa
SamGiles <samuel.giles@ft.com>
2014-10-22 13:29:51 +0100

    More classList/DOMTokenList tests

--- polyfills/Element.prototype.classList/tests.js
+++ polyfills/Element.prototype.classList/tests.js
@@ -1,3 +1,35 @@
+
+it("Should be able to add a class and returns true when a class is added", function() {
+	var el = document.createElement("p");
+	var classList = el.classList;
+	classList.toggle("classA");
+	expect(classList.contains("classA")).to.be(true);
+});
+
+it("Should be able to remove a class and return false when a class is removed", function() {
+	var el = document.createElement("p");
+	var classList = el.classList;
+	el.className = "classA";
+
+	expect(classList.toggle("classA")).to.be(false);
+});
+
+it("Should allow classes to be added using #add", function() {
+	var el = document.createElement("p");
+	var classList = el.classList;
+
+	classList.add("classA");
+	expect(classList.contains("classA")).to.be(true);
+});
+
+it("Should force add a class using toggle if the second argument is true", function() {
+	var el = document.createElement("p");
+	var classList = el.classList;
+	classList.add("classA");
+	classList.toggle("classA", true);
+	expect(classList.contains("classA")).to.be(true);
+});
+
 it("Should remove duplicate instances of class", function() {
 	var el = document.createElement("p");
 	var classList = el.classList;
