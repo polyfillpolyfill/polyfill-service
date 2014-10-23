a9469448bed2683c6cf71ddfecbf1ed744ae0095
Jonathan Neal <jonathantneal@hotmail.com>
2014-10-23 04:22:50 -0400

    Update Window.prototype.viewport
    - correctly check innerHeight

--- polyfills/Window.prototype.viewport/detect.js
+++ polyfills/Window.prototype.viewport/detect.js
@@ -1,1 +1,1 @@
-'innnerHeight' in window && 'innerWidth' in window && 'pageXOffset' in window && 'pageYOffset' in window && 'scrollX' in window && 'scrollY' in window
+'innerHeight' in window && 'innerWidth' in window && 'pageXOffset' in window && 'pageYOffset' in window && 'scrollX' in window && 'scrollY' in window
