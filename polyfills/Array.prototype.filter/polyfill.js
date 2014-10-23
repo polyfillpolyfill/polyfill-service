4dcc1ed78b6610bad662bcf7dffa836566f36c2a
Jonathan Neal <jonathantneal@hotmail.com>
2014-10-23 03:54:05 -0400

    Update Array.prototype.filter
    - throw error on non-objects
    - array should be an object
    - support strings in old ie

--- polyfills/Array.prototype.filter/polyfill.js
+++ polyfills/Array.prototype.filter/polyfill.js
@@ -1,10 +1,22 @@
 Array.prototype.filter = function filter(callback) {
+	if (!(this instanceof Object)) {
+		throw new TypeError(this + 'is not an object');
+	}
+
 	if (typeof callback !== 'function') {
 		throw new TypeError(callback + ' is not a function');
 	}
 
-	for (var array = this, result = [], index = 0, length = array.length; index < length; ++index) {
-		if (index in array && callback.call(arguments[1], array[index], index, array)) {
+	var
+	array = Object(this),
+	arrayIsString = array instanceof String,
+	scope = arguments[1],
+	length = array.length,
+	index = 0,
+	result = [];
+
+	for (; index < length; ++index) {
+		if (index in array && callback.call(scope, arrayIsString ? array.charAt(index) : array[index], index, array)) {
 			result.push(array[index]);
 		}
 	}
