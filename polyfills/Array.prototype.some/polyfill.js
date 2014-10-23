8b17e8fa1ab37d644f521e33255ffa4c03ce1633
SamGiles <samuel.giles@ft.com>
2014-10-23 16:24:05 +0100

    Revert changes to Array.prototype.some.  I think the changes were destined for reduceRight @jonathantneal?

--- polyfills/Array.prototype.some/polyfill.js
+++ polyfills/Array.prototype.some/polyfill.js
@@ -1,21 +1,10 @@
-Array.prototype.reduceRight = function reduceRight(callback) {
-	if (!(this instanceof Object)) {
-		throw new TypeError(this + 'is not an object');
-	}
-
+Array.prototype.some = function some(callback) {
 	if (typeof callback !== 'function') {
 		throw new TypeError(callback + ' is not a function');
 	}
 
-	var
-	array = Object(this),
-	arrayIsString = array instanceof String,
-	scope = arguments[1],
-	length = array.length,
-	index = 0;
-
-	for (; index < length; ++index) {
-		if (index in array && callback.call(scope, arrayIsString ? array.charAt(index) : array[index], index, array)) {
+	for (var array = this, index = 0, length = array.length; index < length; ++index) {
+		if (index in array && callback.call(arguments[1], array[index], index, array)) {
 			return true;
 		}
 	}
