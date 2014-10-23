470354af477f1f650e19f4cf6674ba715de199e2
Jonathan Neal <jonathantneal@hotmail.com>
2014-10-23 03:54:18 -0400

    Update Array.prototype.every
    - throw error on non-objects
    - array should be an object
    - support strings in old ie

--- polyfills/Array.prototype.every/polyfill.js
+++ polyfills/Array.prototype.every/polyfill.js
@@ -1,11 +1,24 @@
 Array.prototype.every = function every(callback) {
+	if (!(this instanceof Object)) {
+		throw new TypeError(this + 'is not an object');
+	}
+
 	if (typeof callback !== 'function') {
 		throw new TypeError(callback + ' is not a function');
 	}
 
-	for (var array = this, index = 0, length = array.length; index < length; ++index) {
-		if (index in array && !callback.call(arguments[1], array[index], index, array)) {
-			return false;
+	var
+	array = Object(this),
+	arrayIsString = array instanceof String,
+	scope = arguments[1],
+	length = array.length,
+	index = 0;
+
+	for (; index < length; ++index) {
+		if (arrayIsString || index in array) {
+			if (!callback.call(scope, arrayIsString ? array.charAt(index) : array[index], index, array)) {
+				return false;
+			}
 		}
 	}
 
