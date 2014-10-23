8084e8232e03d49b6171419e495b980c5ed2ad76
Jonathan Neal <jonathantneal@hotmail.com>
2014-10-23 12:26:26 -0400

    Update Array.prototype.some
    - throw error on non-objects
    - array should be an object
    - support strings in old ie

--- polyfills/Array.prototype.some/polyfill.js
+++ polyfills/Array.prototype.some/polyfill.js
@@ -1,10 +1,14 @@
 Array.prototype.some = function some(callback) {
+	if (!(this instanceof Object)) {
+		throw new TypeError(this + 'is not an object');
+	}
+
 	if (typeof callback !== 'function') {
 		throw new TypeError(callback + ' is not a function');
 	}
 
-	for (var array = this, index = 0, length = array.length; index < length; ++index) {
-		if (index in array && callback.call(arguments[1], array[index], index, array)) {
+	for (var array = Object(this), arrayIsString = array instanceof String, scope = arguments[1], index = 0, length = array.length; index < length; ++index) {
+		if ((arrayIsString || index in array) && callback.call(scope, arrayIsString ? array.charAt(index) : array[index], index, array)) {
 			return true;
 		}
 	}
