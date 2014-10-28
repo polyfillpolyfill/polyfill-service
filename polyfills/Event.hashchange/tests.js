f5bf93303fb3d1a54117191ba1d02adb703e9323
Jonathan Neal <jonathantneal@hotmail.com>
2014-10-27 20:34:34 -0400

    Update hatching test
    - remove possibility of failing when hash is already test

--- polyfills/Event.hashchange/tests.js
+++ polyfills/Event.hashchange/tests.js
@@ -1,7 +1,10 @@
 it('Should dispatch the hashchange event', function(done) {
+	window.location.hash = '';
+
 	window.addEventListener('hashchange', function(e) {
 		expect(e.type).to.be('hashchange');
 		done();
 	});
+
 	window.location.hash = 'test';
 });
