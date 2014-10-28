a35104b42c3b83929f21056f38f060d3a5d3ac48
Jonathan Neal <jonathantneal@hotmail.com>
2014-10-28 13:31:42 -0400

    Update Object.create
    - support old IE

--- polyfills/Object.create/polyfill.js
+++ polyfills/Object.create/polyfill.js
@@ -1,20 +1,19 @@
-(function () {
-	Object.create = function create(prototype, properties) {
-		/* jshint evil: true */
+// Object.create
+Object.create = function create(prototype, properties) {
+	/* jshint evil: true */
 
-		if (typeof prototype !== 'object') {
-			throw TypeError('Argument must be an object');
-		}
+	if (!(prototype instanceof Object || prototype === null)) {
+		throw new TypeError('Object prototype may only be an Object or null');
+	}
 
-		var
-		object = new Function('e', 'function Object() {}Object.prototype=e;return new Object')(prototype);
+	var
+	object = new Function('e', 'function Object() {}Object.prototype=e;return new Object')(prototype);
 
-		object.constructor.prototype = prototype;
+	object.constructor.prototype = prototype;
 
-		if (1 in arguments) {
-			Object.defineProperties(object, properties);
-		}
+	if (1 in arguments) {
+		Object.defineProperties(object, properties);
+	}
 
-		return object;
-	};
-})();
+	return object;
+};
