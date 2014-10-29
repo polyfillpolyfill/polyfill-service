7969614b3c1187744516505e7f28ba4c13105a3f
Jonathan Neal <jonathantneal@hotmail.com>
2014-10-29 10:11:32 -0400

    Update Array tests
    - simplifies tests

--- polyfills/Array.isArray/tests.js
+++ polyfills/Array.isArray/tests.js
@@ -20,8 +20,6 @@ describe('returns true with', function () {
 });
 
 describe('returns false with', function () {
-	var args;
-
 	it('empty, null, or undefined', function () {
 		expect(Array.isArray()).to.be(false);
 		expect(Array.isArray(undefined)).to.be(false);
@@ -29,16 +27,25 @@ describe('returns false with', function () {
 	});
 
 	it('arguments', function () {
-		args = (function () {
+		var args = (function () {
 			return arguments;
 		})('a', 'b', 'c');
 
 		expect(Array.isArray(args)).to.be(false);
 	});
 
-	it('booleans', function () {
-		expect(Array.isArray(false)).to.be(false);
+	it('primatives', function () {
 		expect(Array.isArray(true)).to.be(false);
+		expect(Array.isArray('abc')).to.be(false);
+		expect(Array.isArray(123)).to.be(false);
+	});
+
+	it('instances', function () {
+		expect(Array.isArray(new Date)).to.be(false);
+		expect(Array.isArray(new Object)).to.be(false);
+		expect(Array.isArray(new Function)).to.be(false);
+		expect(Array.isArray(new Number)).to.be(false);
+		expect(Array.isArray(new String)).to.be(false);
 	});
 
 	it('constructors', function () {
@@ -48,25 +55,14 @@ describe('returns false with', function () {
 		expect(Array.isArray(String)).to.be(false);
 	});
 
-	it('functions', function () {
-		expect(Array.isArray(function () {})).to.be(false);
-	});
-
-	it('new instances', function () {
-		expect(Array.isArray(new Date)).to.be(false);
-		expect(Array.isArray(new Object)).to.be(false);
-		expect(Array.isArray(new Function)).to.be(false);
-		expect(Array.isArray(new Number)).to.be(false);
-		expect(Array.isArray(new String)).to.be(false);
-	});
-
-	it('numbers', function () {
-		expect(Array.isArray(3)).to.be(false);
-	});
-
 	it('objects', function () {
 		expect(Array.isArray({})).to.be(false);
-		expect(Array.isArray({ length: 3 })).to.be(false);
+		expect(Array.isArray({
+			0: 'a',
+			1: 'b',
+			2: 'c',
+			length: 3
+		})).to.be(false);
 	});
 
 	it('objects as array subclasses', function () {
@@ -77,11 +73,13 @@ describe('returns false with', function () {
 		expect(Array.isArray(new NotArray)).to.be(false);
 	});
 
-	it('regular expressions', function () {
-		expect(Array.isArray(/abc/)).to.be(false);
+	it('functions', function () {
+		expect(Array.isArray(function () {
+			return [];
+		})).to.be(false);
 	});
 
-	it('strings', function () {
-		expect(Array.isArray('')).to.be(false);
+	it('regular expressions', function () {
+		expect(Array.isArray(/abc/)).to.be(false);
 	});
 });
--- polyfills/Array.of/tests.js
+++ polyfills/Array.of/tests.js
@@ -13,6 +13,14 @@ it('has correct argument length', function () {
 describe('returns an array with', function () {
 	var array, object;
 
+	it('one string', function () {
+		array = Array.of('abc');
+
+		expect(array).to.be.an(Array);
+		expect(array.length).to.be(1);
+		expect(array[0]).to.be('abc');
+	});
+
 	it('three strings', function () {
 		array = Array.of('a', 'b', 'c');
 
@@ -23,16 +31,21 @@ describe('returns an array with', function () {
 		expect(array[2]).to.be('c');
 	});
 
-	it('an integer', function () {
-		array = Array.of(3);
+	it('one number', function () {
+		array = Array.of(123);
 
 		expect(array).to.be.an(Array);
 		expect(array.length).to.be(1);
-		expect(array[0]).to.be(3);
+		expect(array[0]).to.be(123);
 	});
 
-	it('an object', function () {
-		object = { length: 3 };
+	it('one object', function () {
+		object = {
+			0: 'a',
+			1: 'b',
+			2: 'c',
+			length: 3
+		};
 		array = Array.of(object);
 
 		expect(array).to.be.an(Array);
