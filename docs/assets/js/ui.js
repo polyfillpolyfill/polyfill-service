c82b01cd2337575a2d1cfc435dd01293c990bfa3
Andrew Betts <andrew.betts@ft.com>
2014-10-29 23:19:39 +0000

    Fixes for docs (use textContent instead of deprecated innerText, define drawCharts earlier, and add Array.from polyfill to Safari)

--- docs/assets/js/ui.js
+++ docs/assets/js/ui.js
@@ -4,14 +4,35 @@ Array.from(document.querySelectorAll('.demo')).forEach(function(el) {
 	var xhr = new XMLHttpRequest();
 	xhr.open("get", el.getAttribute('data-src'));
 	xhr.addEventListener('load', function() {
-	  el.innerText = this.responseText;
+	  el.textContent = this.responseText;
 	});
 	xhr.send();
 });
 
 // Perf charts
+var chart1, data1, chart2, data2, drawCharts;
+drawCharts = function() {
+	chart1.draw(data1, {
+		chartArea:{left:0,top:0,width:'100%',height:'90%'},
+		hAxis: {title: 'Time (by hour, last 7 days)'},
+		vAxis: {textPosition: 'in'},
+		legend: {position: 'none'},
+		bar: {groupWidth: '95%'}
+	});
+	chart2.draw(data2, {
+		pieHole: 0.6,
+		chartArea:{left:'5%',top:'5%',width:'100%',height:'90%'},
+		legend: {position: 'labeled'},
+		pieSliceText: 'none'
+	});
+	chart3.draw(data3, {
+		chartArea:{left:30,top:10,width:'100%',height:'80%'},
+		legend: {position: 'none'},
+		hAxis: {textPosition: 'none'},
+		vAxis: {minValue: 0}
+	});
+}
 if (document.getElementById('chart-requests')) {
-	var chart1, data1, chart2, data2;
 	google.load('visualization', '1.0', {'packages':['corechart']});
 	window.onresize = drawCharts;
 	google.setOnLoadCallback(function() {
@@ -19,7 +40,7 @@ if (document.getElementById('chart-requests')) {
 		data1 = google.visualization.arrayToDataTable(
 			Array.from(chartel.querySelectorAll('tr')).map(function(rowel) {
 				return Array.from(rowel.querySelectorAll('td,th')).map(function(cellel) {
-					return isNaN(cellel.innerText) ? cellel.innerText : parseFloat(cellel.innerText);
+					return isNaN(cellel.textContent) ? cellel.textContent : parseFloat(cellel.textContent);
 				});
 			})
 		);
@@ -29,7 +50,7 @@ if (document.getElementById('chart-requests')) {
 		data2 = google.visualization.arrayToDataTable(
 			Array.from(chartel.querySelectorAll('tr')).map(function(rowel) {
 				return Array.from(rowel.querySelectorAll('td,th')).map(function(cellel) {
-					return isNaN(cellel.innerText) ? cellel.innerText : parseFloat(cellel.innerText);
+					return isNaN(cellel.textContent) ? cellel.textContent : parseFloat(cellel.textContent);
 				});
 			})
 		);
@@ -39,7 +60,7 @@ if (document.getElementById('chart-requests')) {
 		data3 = google.visualization.arrayToDataTable(
 			Array.from(chartel.querySelectorAll('tr')).map(function(rowel) {
 				return Array.from(rowel.querySelectorAll('td,th')).map(function(cellel) {
-					return isNaN(cellel.innerText) ? cellel.innerText : parseFloat(cellel.innerText);
+					return isNaN(cellel.textContent) ? cellel.textContent : parseFloat(cellel.textContent);
 				});
 			})
 		);
@@ -47,25 +68,4 @@ if (document.getElementById('chart-requests')) {
 
 		drawCharts();
 	});
-	function drawCharts() {
-		chart1.draw(data1, {
-			chartArea:{left:0,top:0,width:'100%',height:'90%'},
-			hAxis: {title: 'Time (by hour, last 7 days)'},
-			vAxis: {textPosition: 'in'},
-			legend: {position: 'none'},
-			bar: {groupWidth: '95%'}
-		});
-		chart2.draw(data2, {
-			pieHole: 0.6,
-			chartArea:{left:'5%',top:'5%',width:'100%',height:'90%'},
-			legend: {position: 'labeled'},
-			pieSliceText: 'none'
-		});
-		chart3.draw(data3, {
-			chartArea:{left:30,top:10,width:'100%',height:'80%'},
-			legend: {position: 'none'},
-			hAxis: {textPosition: 'none'},
-			vAxis: {minValue: 0}
-		});
-	}
 }
