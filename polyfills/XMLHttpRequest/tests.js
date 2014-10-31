c28f9b2434e9dd8a92deefc15d82950daad7a340
Jonathan Neal <jonathantneal@hotmail.com>
2014-10-31 11:40:47 -0400

    Update XMLHttpRequest test
    - add notes above removals

--- polyfills/XMLHttpRequest/tests.js
+++ polyfills/XMLHttpRequest/tests.js
@@ -1,19 +1,19 @@
 var xhr;
 
-// Safari 5.1+ does not consider XMLHttpRequest a function
-if (XMLHttpRequest instanceof Function) {
-	it('has correct instance', function () {
-		expect(XMLHttpRequest).to.be.a(Function);
-	});
+// REMOVED: Safari considers XMLHttpRequest an object
+// it('has correct instance', function () {
+// 	expect(XMLHttpRequest).to.be.a(Function);
+// });
 
-	it('has correct name', function () {
-		expect(nameOf(XMLHttpRequest)).to.be('XMLHttpRequest');
-	});
+// REMOVED: Firefox considers XMLHttpRequest a function, but does not allow you to use Function.prototype.toString (yet accepts XMLHttpRequest.toString!)
+// it('has correct name', function () {
+// 	expect(nameOf(XMLHttpRequest)).to.be('XMLHttpRequest');
+// });
 
-	it('has correct argument length', function () {
-		expect(XMLHttpRequest.length).to.be(0);
-	});
-}
+// REMOVED: See above
+// it('has correct argument length', function () {
+// 	expect(XMLHttpRequest.length).to.be(0);
+// });
 
 it('can create instance', function () {
 	xhr = new XMLHttpRequest;
@@ -22,14 +22,17 @@ it('can create instance', function () {
 it('can setup request', function () {
 	expect(xhr.open).to.be.a(Function);
 	expect(nameOf(xhr.open)).to.be('open');
+	// REMOVED: Inconsistent between browsers, does not affect functionality
 	// expect(xhr.open.length).to.be(2);
 
 	expect(xhr.addEventListener).to.be.a(Function);
 	expect(nameOf(xhr.addEventListener)).to.be('addEventListener');
+	// REMOVED: Inconsistent between browsers, does not affect functionality
 	// expect(xhr.addEventListener.length).to.be(0);
 
 	expect(xhr.send).to.be.a(Function);
 	expect(nameOf(xhr.send)).to.be('send');
+	// REMOVED: Inconsistent between browsers, does not affect functionality
 	// expect(xhr.send.length).to.be(0);
 });
 
