d0ded6522be8e6743ee49ca6e6adbbf530c0cf79
Juan Ignacio Dopazo <juan@dopazo.me>
2014-11-10 12:32:10 -0300

    Test stringifying polyfill sets

--- test/node/service/test_polyfillset.js
+++ test/node/service/test_polyfillset.js
@@ -1,21 +1,30 @@
 var assert = require('assert');
 var PolyfillSet = require('../../../service/PolyfillSet');
 
-describe("#fromQueryParam(polyfillList, additionalFlags)", function() {
+describe('PolyfillSet', function () {
+	describe("#fromQueryParam(polyfillList, additionalFlags)", function() {
 
-	it("should transform a simple querystring, contaning comma separated values to a list of polyfills with flags", function() {
-		var querystring = "poly1|gated,poly2|always";
-		assert.deepEqual(PolyfillSet.fromQueryParam(querystring).get(), {
-			poly1: { flags: ['gated'] },
-			poly2: { flags: ['always'] }
+		it("should transform a simple querystring, contaning comma separated values to a list of polyfills with flags", function() {
+			var querystring = "poly1|gated,poly2|always";
+			assert.deepEqual(PolyfillSet.fromQueryParam(querystring).get(), {
+				poly1: { flags: ['gated'] },
+				poly2: { flags: ['always'] }
+			});
+		});
+
+		it("should add additional flags to each polyfill object returned if additional flags are specified", function() {
+			var querystring = "poly1|always,poly2|always";
+			assert.deepEqual(PolyfillSet.fromQueryParam(querystring, ["gated"]).get(), {
+				poly1: { flags: ['always', 'gated'] },
+				poly2: { flags: ['always', 'gated'] }
+			});
 		});
 	});
 
-	it("should add additional flags to each polyfill object returned if additional flags are specified", function() {
-		var querystring = "poly1|always,poly2|always";
-		assert.deepEqual(PolyfillSet.fromQueryParam(querystring, ["gated"]).get(), {
-			poly1: { flags: ['always', 'gated'] },
-			poly2: { flags: ['always', 'gated'] }
+	describe('#stringify()', function () {
+		it('should be a reversible process', function () {
+			var querystring = "poly1|gated,poly2|always";
+			assert.equal(PolyfillSet.fromQueryParam(querystring).stringify(), querystring);
 		});
 	});
 });
