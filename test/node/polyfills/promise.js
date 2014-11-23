81bbfbb78730c5cc14f2a54b5292026223b04f1f
Juan Ignacio Dopazo <juan@dopazo.me>
2014-11-23 19:56:34 -0300

    Use ypromise's tests

--- test/node/polyfills/promise.js
+++ test/node/polyfills/promise.js
@@ -7,32 +7,6 @@ eval(
     '}).call(global);'
 );
 
-describe('global.Promise', function () {
-    it('is correctly defined', function () {
-        assert.equal(typeof global.Promise, 'function');
-        assert.equal(typeof Promise.resolve, 'function', 'Missing Promise.resolve');
-        //assert.equal(typeof Promise.reject, 'function', 'Missing Promise.rejected');
-        assert.equal(typeof Promise.all, 'function', 'Missing Promise.all');
-        assert.equal(typeof Promise.race, 'function', 'Missing Promise.race');
-    });
-});
-
-describe('infinite recursion', function () {
-    it('should not happen!', function (done) {
-        var p = Promise.resolve(42);
-
-        var p2 = p.then(function () {
-            return p;
-        });
-
-        p2.then(function (v) {
-            done(new Error('promise should have been rejected: ' + (v instanceof Promise)));
-        }, function () {
-            done();
-        });
-    });
-});
-
 describe('Promises A+ Tests', function () {
     var adapter = {
         resolved: function (value) {
