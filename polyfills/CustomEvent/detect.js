3518a70d4bcc16b6457c70eb1e39733d35f65d14
Andrew Betts <andrew.betts@ft.com>
2014-12-05 22:00:17 +0000

    Document oddity

--- polyfills/CustomEvent/detect.js
+++ polyfills/CustomEvent/detect.js
@@ -1,3 +1,5 @@
 'CustomEvent' in window &&
+
+// In Safari, typeof CustomEvent == 'object' but it otherwise works fine
 (typeof window.CustomEvent === 'function' ||
 (window.CustomEvent.toString().indexOf('CustomEventConstructor')>-1))
