polyfillpolyfill/polyfill-service

commit 6cb3ca1fd42df583706a5a996bc9e5495fa21cba

Phantom JS -> safari/5, and support more intelligent aliasing, closes #208

lib/UA.js
@@ -1,60 +1,10 @@
 /**
- * Create mappings between the names parsed from useragent (based on
- * https://github.com/tobie/ua-parser/blob/master/regexes.yaml) to caniuse
+ * Create mappings between the names parsed from useragent to caniuse
  * name equivalents.
  *
- * Each caniuse name can match against many names.
- *
- * To add a new mapping add the UA name to the appropriate key.
+ * Multiple names may map to the same canonical caniuse name
  */
 var useragent = require('useragent');
-
-var agentmappings = {
-	"bb": [
-		"blackberry webkit",
-		"blackberry"
-	],
-	"firefox": [
-		"pale moon (firefox variant)",
-		"firefox mobile",
-		"firefox namoroka",
-		"firefox shiretoko",
-		"firefox minefield",
-		"firefox alpha",
-		"firefox beta",
-		"microb",
-		"mozilladeveloperpreview",
-		"iceweasel"
-	],
-	"opera": [
-		"opera tablet"
-	],
-	"op_mob": [
-		"opera mobile"
-	],
-	"op_mini": [
-		"opera mini"
-	],
-	"chrome": [
-		"chrome mobile",
-		"chrome frame",
-		"chromium"
-	],
-	"ie_mob": [
-		"ie mobile"
-	],
-	"ie": [
-		"ie large screen",
-		"internet explorer"
-	],
-	"ios_chr": [
-		"chrome mobile ios"
-	],
-	"ios_saf": [
-		"mobile safari",
-		"iphone"
-	]
-};
 
 var baseLineVersions = {
 	"ie": ">=6",
@@ -68,16 +18,45 @@ var baseLineVersions = {
 	"op_mob": ">=10",
 	"op_mini": ">=5",
 	"bb": ">=6"
-}
+};
 
-// Invert the mappings above mapping each user agent to the caniuse name for
-// faster lookup in agent list
-var agentlist = {};
-Object.keys(agentmappings).forEach(function(caniuseAgentName) {
-	agentmappings[caniuseAgentName].forEach(function(userAgentName) {
-		agentlist[userAgentName] = caniuseAgentName;
-	});
-});
+var aliases = {
+	"blackberry webkit": "bb",
+	"blackberry": "bb",
+
+	"pale moon (firefox variant)": "firefox",
+	"firefox mobile": "firefox",
+	"firefox namoroka": "firefox",
+	"firefox shiretoko": "firefox",
+	"firefox minefield": "firefox",
+	"firefox alpha": "firefox",
+	"firefox beta": "firefox",
+	"microb": "firefox",
+	"mozilladeveloperpreview": "firefox",
+	"iceweasel": "firefox",
+
+	"opera tablet": "opera",
+
+	"opera mobile": "op_mob",
+	"opera mini": "op_mini",
+
+	"chrome mobile": "chrome",
+	"chrome frame": "chrome",
+	"chromium": "chrome",
+
+	"ie mobile": "ie_mob",
+
+	"ie large screen": "ie",
+	"internet explorer": "ie",
+
+	"chrome mobile ios": "ios_chr",
+
+	"mobile safari": "ios_saf",
+	"iphone": "ios_saf",
+
+	"phantomjs": ["safari", 5, 0, 0]
+};
+
 
 /**
  * jQuery: isNumeric
@@ -89,6 +68,8 @@ function isNumeric(obj) {
 
 
 var UA = function(uaString) {
+	var semver, a;
+
 	var normalized = uaString.match(/^(\w+)\/(\d+)(?:\.(\d+)(?:\.(\d+))?)?$/i);
 	if (normalized) {
 		this.ua = new useragent.Agent(normalized[1], normalized[2], (normalized[3] || 0), (normalized[4] || 0));
@@ -103,8 +84,30 @@ var UA = function(uaString) {
 		this.ua.patch = '0';
 	}
 
-	// Patch ua.family with the normalised name
-	this.ua.family = agentlist[this.ua.family.toLowerCase()] || this.ua.family.toLowerCase();
+	// Resolve aliases
+	this.ua.family = this.ua.family.toLowerCase()
+	if (aliases[this.ua.family]) {
+
+		// Map to different family, use same version scheme
+		if (typeof aliases[this.ua.family] === 'string') {
+			this.ua.family = aliases[this.ua.family]
+
+		// Map to different family with constant version
+		} else if (Array.isArray(aliases[this.ua.family]) && aliases[this.ua.family].length >= 2) {
+			a = aliases[this.ua.family];
+			this.ua = new useragent.Agent(a[0], a[1], (a[2] || 0), (a[3] || 0));
+
+		// Map to different family with per-version mapping
+		} else if (typeof aliases[this.ua.family] === 'object') {
+			for (semver in aliases[this.ua.family]) {
+				if (this.ua.satisfies(semver) && Array.isArray(aliases[this.ua.family][semver])) {
+					var a = aliases[this.ua.family][semver];
+					this.ua = new useragent.Agent(a[0], a[1], (a[2] || 0), (a[3] || 0));
+					break;
+				}
+			}
+		}
+	}
 };
 
 UA.prototype.getFamily = function() {

commit 144476b8c798c44fb637754fc32eca03a322adf5

Fix function broken in 6cb3ca1fd42df583706a5a996bc9e5495fa21cba

lib/UA.js
@@ -144,8 +144,18 @@ UA.getBaselines = function() {
 	return baseLineVersions;
 }
 
-UA.normalizeName = function(uaName) {
-	return agentlist[uaName.toLowerCase()] || false;
+UA.normalizeName = function(uaName, version) {
+	var semver;
+	var name = aliases[uaName.toLowerCase()] || uaName.toLowerCase();
+	if (typeof name == 'string') return name;
+	if (Array.isArray(name)) return name[0];
+	if (typeof name === 'object') {
+		for (semver in name) {
+
+			// TODO: return the family of the matching version, not just the first one
+			return name[semver][0];
+		}
+	}
 };
 
 module.exports = UA;