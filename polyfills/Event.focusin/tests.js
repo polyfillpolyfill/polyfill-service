d1bf0208e3921b9e84c6549c33b5200eaa0101e0
Andrew Betts <andrew.betts@ft.com>
2014-12-05 22:02:04 +0000

    Fix focusin tests, were really broken, failed in Chrome even though it has native support!

--- polyfills/Event.focusin/tests.js
+++ polyfills/Event.focusin/tests.js
@@ -1,21 +1,32 @@
-it('Should dispatch the focusin event', function(done) {
-	var testEl = document.createElement('div');
-	window.addEventListener('focusin', function(e) {
+
+it('should dispatch the focusin event', function(done) {
+	var testEl = document.createElement('input');
+	testEl.id = 'test1';
+	document.body.appendChild(testEl);
+	window.addEventListener('focusin', listener);
+	testEl.focus();
+
+	function listener(e) {
 		expect(e.type).to.be('focusin');
 		expect(e.target).to.be(testEl);
+		window.removeEventListener('focusin', listener);
+		document.body.removeChild(testEl);
 		done();
-	});
-	document.body.appendChild(testEl);
-	testEl.dispatchEvent(new Event('focusin', {bubbles:true}));
+	}
 });
 
-it('Should dispatch the focusout event', function(done) {
-	var testEl = document.createElement('div');
-	window.addEventListener('focusout', function(e) {
+it('should dispatch the focusout event', function(done) {
+	var testEl2 = document.createElement('input');
+	testEl2.id = 'test2';
+	document.body.appendChild(testEl2);
+	testEl2.focus();
+	window.addEventListener('focusout', listener);
+	testEl2.blur();
+
+	function listener(e) {
 		expect(e.type).to.be('focusout');
-		expect(e.target).to.be(testEl);
+		expect(e.target).to.be(testEl2);
+		document.body.removeChild(testEl2);
 		done();
-	});
-	document.body.appendChild(testEl);
-	testEl.dispatchEvent(new Event('focusout', {bubbles:true}));
+	}
 });
