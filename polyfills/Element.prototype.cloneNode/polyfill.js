11339a3fc6118bf790794c997742a6a3744d1b2c
Andrew Betts <andrew.betts@ft.com>
2014-12-11 09:14:59 +0000

    Use native func in IE8 to avoid circular calls with html5shiv overriding createElement.  Probably faster too.

--- polyfills/Element.prototype.cloneNode/polyfill.js
+++ polyfills/Element.prototype.cloneNode/polyfill.js
@@ -1,14 +1,1 @@
-Element.prototype.cloneNode = function(deep) {
-	var tmp, clone;
-	tmp = document.createElement('div');
-	tmp.innerHTML = this.outerHTML;
-	clone = tmp.firstChild;
-
-	if ('checked' in this) clone.checked = this.checked;
-
-	if (deep === false && clone.innerHTML) {
-		clone.innerHTML = '';
-	}
-
-	return clone;
-}
+/* The only available polyfill for this feature depends on the feature already existing but being buggy.  Therefore we can't serve that polyfill to untargeted browsers. */
