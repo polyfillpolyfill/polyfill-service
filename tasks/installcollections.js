e387f8d676ea7dbe59eb098837b5e0183a09cc97
Andrew Betts <andrew.betts@ft.com>
2015-01-11 15:23:55 +0000

    Grunt task to install previous versions of the app as alternative polyfill sources

---
+++ tasks/installcollections.js
@@ -0,0 +1,53 @@
+'use strict';
+
+var fs = require('fs');
+var path = require('path');
+var Promise = require('es6-promise').Promise;
+var denodeify = require('denodeify');
+var copy = denodeify(require('ncp').ncp);
+var mkdir = denodeify(require('mkdirp'));
+var exec = denodeify(require('child_process').exec, function(err, stdout, stderr) { return [err, stdout]; });
+
+var repourl = 'git://github.com/financial-times/polyfill-service';
+
+module.exports = function(grunt) {
+
+	grunt.registerTask('installcollections', 'Install previous versions of the polyfill collection', function() {
+
+		var done = this.async();
+
+		var versions = require('./installcollections-versions.json');
+		var repodir = path.join(__dirname, '../polyfills/__repo');
+		var versionsdir = path.join(__dirname, '../polyfills/__versions');
+
+		return Promise.all([
+			mkdir(repodir), mkdir(versionsdir)
+		])
+		.then(function() {
+			grunt.log.writeln('Cloning polyfill service source from '+repourl);
+			return exec('git clone '+ repourl +' '+ repodir);
+		})
+		.then(versions.reduce.bind(versions, function(soFar, version) {
+			var dest = path.join(versionsdir, version);
+			return soFar
+			.then(function() {
+				grunt.log.writeln('Installing version '+version);
+				return exec('cd '+repodir+'; git checkout ' + version);
+			})
+			.then(function() {
+				return mkdir(dest);
+			})
+			.then(function() {
+				return copy(path.join(repodir, 'polyfills'), dest);
+			})
+			.catch(grunt.warn);
+		}, Promise.resolve(1)))
+		.then(function() {
+			grunt.log.writeln("Removing local repo");
+			return exec('rm -rf '+repodir);
+		})
+		.then(done)
+		.catch(grunt.warn);
+
+	});
+};
