82b2e37b7d76ee3d185d18a164ece987f6cee857
Juan Ignacio Dopazo <juan@dopazo.me>
2015-01-29 15:25:04 -0300

    Use es5-shim's Object.create
    The current Object.create implementation covers less edge cases and uses
    eval which won't work on CSP enabled environments.

--- polyfills/Object.create/polyfill.js
+++ polyfills/Object.create/polyfill.js
@@ -1,18 +1,84 @@
+/*jshint proto:true*/
+// Contributed by Brandon Benvie, October, 2012
+var createEmpty;
+var supportsProto = !({ __proto__: null } instanceof Object);
+                    // the following produces false positives
+                    // in Opera Mini => not a reliable check
+                    // Object.prototype.__proto__ === null
+/*global document */
+if (supportsProto || typeof document === 'undefined') {
+    createEmpty = function () {
+        return { __proto__: null };
+    };
+} else {
+    // In old IE __proto__ can't be used to manually set `null`, nor does
+    // any other method exist to make an object that inherits from nothing,
+    // aside from Object.prototype itself. Instead, create a new global
+    // object and *steal* its Object.prototype and strip it bare. This is
+    // used as the prototype to create nullary objects.
+    createEmpty = function () {
+        var iframe = document.createElement('iframe');
+        var parent = document.body || document.documentElement;
+        iframe.style.display = 'none';
+        parent.appendChild(iframe);
+        /*jshint scripturl:true*/
+        iframe.src = 'javascript:';
+        /*jshint scripturl:false*/
+        var empty = iframe.contentWindow.Object.prototype;
+        parent.removeChild(iframe);
+        iframe = null;
+        delete empty.constructor;
+        delete empty.hasOwnProperty;
+        delete empty.propertyIsEnumerable;
+        delete empty.isPrototypeOf;
+        delete empty.toLocaleString;
+        delete empty.toString;
+        delete empty.valueOf;
+        /*eslint-disable no-proto */
+        empty.__proto__ = null;
+        /*eslint-enable no-proto */
+
+        function Empty() {}
+        Empty.prototype = empty;
+        // short-circuit future calls
+        createEmpty = function () {
+            return new Empty();
+        };
+        return new Empty();
+    };
+}
+
 Object.create = function create(prototype, properties) {
-	/* jshint evil: true */
 
-	if (!(prototype instanceof Object || prototype === null)) {
-		throw new TypeError('Object prototype may only be an Object or null');
-	}
+    var object;
+    function Type() {}  // An empty constructor.
 
-	var
-	object = new Function('e', 'function Object() {}Object.prototype=e;return new Object')(prototype);
+    if (prototype === null) {
+        object = createEmpty();
+    } else {
+        if (typeof prototype !== 'object' && typeof prototype !== 'function') {
+            // In the native implementation `parent` can be `null`
+            // OR *any* `instanceof Object`  (Object|Function|Array|RegExp|etc)
+            // Use `typeof` tho, b/c in old IE, DOM elements are not `instanceof Object`
+            // like they are in modern browsers. Using `Object.create` on DOM elements
+            // is...err...probably inappropriate, but the native version allows for it.
+            throw new TypeError('Object prototype may only be an Object or null'); // same msg as Chrome
+        }
+        Type.prototype = prototype;
+        object = new Type();
+        // IE has no built-in implementation of `Object.getPrototypeOf`
+        // neither `__proto__`, but this manually setting `__proto__` will
+        // guarantee that `Object.getPrototypeOf` will work as expected with
+        // objects created using `Object.create`
+        /*eslint-disable no-proto */
+        object.__proto__ = prototype;
+        /*eslint-enable no-proto */
+    }
 
-	object.constructor.prototype = prototype;
+    if (properties !== void 0) {
+        Object.defineProperties(object, properties);
+    }
 
-	if (1 in arguments) {
-		Object.defineProperties(object, properties);
-	}
-
-	return object;
+    return object;
 };
+/*jshint proto:false*/
