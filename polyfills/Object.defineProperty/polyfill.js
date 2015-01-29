60c5e964a7a0bb8766f7a8e932841e144e49bcb8
Juan Ignacio Dopazo <juan@dopazo.me>
2015-01-29 16:59:04 -0300

    Error handling for Object.defineProperty
    Object.defineProperty was not checking if accessors were available in
    the object. It also was not throwing when both an accessor and a value
    were specified in the descriptor.

--- polyfills/Object.defineProperty/polyfill.js
+++ polyfills/Object.defineProperty/polyfill.js
@@ -1,43 +1,60 @@
-Object.defineProperty = Object.defineProperty || function defineProperty(object, property, descriptor) {
-	// handle object
-	if (object === null || !(object instanceof Object || typeof object === 'object')) {
-		throw new TypeError('Object must be an object');
+(function () {
+	if (Object.defineProperty) {
+		return;
 	}
+	
+	var supportsAccessors = Object.prototype.hasOwnProperty('__defineGetter__');
+	var ERR_ACCESSORS_NOT_SUPPORTED = 'getters & setters can not be defined on this javascript engine';
+	var ERR_VALUE_ACCESSORS = 'A property cannot both have accessors and be writable or have a value';
 
-	// handle descriptor
-	if (!(descriptor instanceof Object)) {
-		throw new TypeError('Descriptor must be an object');
-	}
+	Object.defineProperty = function defineProperty(object, property, descriptor) {
+		// handle object
+		if (object === null || !(object instanceof Object || typeof object === 'object')) {
+			throw new TypeError('Object must be an object');
+		}
 
-	var
-	propertyString = String(property),
-	getterType = 'get' in descriptor && typeof descriptor.get,
-	setterType = 'set' in descriptor && typeof descriptor.set;
+		// handle descriptor
+		if (!(descriptor instanceof Object)) {
+			throw new TypeError('Descriptor must be an object');
+		}
 
-	// handle descriptor.get
-	if (getterType && getterType !== 'function') {
-		throw new TypeError('Getter expected a function');
-	}
+		var
+		propertyString = String(property),
+		getterType = 'get' in descriptor && typeof descriptor.get,
+		setterType = 'set' in descriptor && typeof descriptor.set,
+		hasValueOrWritable = 'value' in descriptor || 'writable' in descriptor;
 
-	// handle descriptor.set
-	if (setterType && setterType !== 'function') {
-		throw new TypeError('Setter expected a function');
-	}
+		// handle descriptor.get
+		if (getterType) {
+			if (getterType !== 'function') {
+				throw new TypeError('Getter expected a function');
+			}
+			if (!supportsAccessors) {
+				throw new TypeError(ERR_ACCESSORS_NOT_SUPPORTED);
+			}
+			if (hasValueOrWritable) {
+				throw new TypeError(ERR_VALUE_ACCESSORS);
+			}
+			object.__defineGetter__(propertyString, descriptor.get);
+		}
 
-	// handle descriptor.get
-	if (getterType) {
-		object.__defineGetter__(propertyString, descriptor.get);
-	}
-	// handle descriptor.value
-	else {
+		// handle descriptor.set
+		if (setterType) {
+			if (setterType !== 'function') {
+				throw new TypeError('Setter expected a function');
+			}
+			if (!supportsAccessors) {
+				throw new TypeError(ERR_ACCESSORS_NOT_SUPPORTED);
+			}
+			if (hasValueOrWritable) {
+				throw new TypeError(ERR_VALUE_ACCESSORS);
+			}
+			object.__defineSetter__(propertyString, descriptor.set);
+		}
+
 		object[propertyString] = descriptor.value;
-	}
 
-	// handle descriptor.set
-	if (setterType) {
-		object.__defineSetter__(propertyString, descriptor.set);
-	}
-
-	// return object
-	return object;
-};
+		// return object
+		return object;
+	};
+}());
