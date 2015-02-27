898f9fc39a7be3a242fc2d1046c726fbe3b7c1e9
Juan Ignacio Dopazo <juan@dopazo.me>
2015-02-27 11:06:26 -0300

    Also throw for accessors in IE8 and IE7

--- polyfills/Object.defineProperty/polyfill-ie7.js
+++ polyfills/Object.defineProperty/polyfill-ie7.js
@@ -10,20 +10,37 @@ Object.defineProperty = function defineProperty(object, property, descriptor) {
 	}
 
 	var
+	ERR_ACCESSORS_NOT_SUPPORTED = 'getters & setters can not be defined on this javascript',
+	ERR_VALUE_ACCESSORS = 'A property cannot both have accessors and be writable or have a value',
+	hasValueOrWritable = 'value' in descriptor || 'writable' in descriptor,
 	propertyString = String(property),
 	Element = window.Element || {};
 
 	// handle descriptor.get
 	if ('get' in descriptor) {
-		object[propertyString] = object === Element.prototype ? new Element.__getter__(descriptor.get) : descriptor.get.call(object);
-	}
-	// handle descriptor.value
-	else {
-		object[propertyString] = descriptor.value;
+		if (typeof descriptor.get !== 'function') {
+			throw new TypeError('Getter expected a function');
+		}
+		if (hasValueOrWritable) {
+			throw new TypeError(ERR_VALUE_ACCESSORS);
+		}
+		if (object !== Element.prototype) {
+			throw new TypeError(ERR_ACCESSORS_NOT_SUPPORTED);
+		}
+		object[propertyString] = new Element.__getter__(descriptor.get);
 	}
 
 	// handle descriptor.set
-	if ('set' in descriptor && object.constructor === Element) {
+	if ('set' in descriptor) {
+		if (typeof descriptor.get !== 'function') {
+			throw new TypeError('Getter expected a function');
+		}
+		if (hasValueOrWritable) {
+			throw new TypeError(ERR_VALUE_ACCESSORS);
+		}
+		if (object.constructor !== Element) {
+			throw new TypeError(ERR_ACCESSORS_NOT_SUPPORTED);
+		}
 		object.attachEvent('onpropertychange', function callee(event) {
 			object.detachEvent('onpropertychange', callee);
 
@@ -33,6 +50,10 @@ Object.defineProperty = function defineProperty(object, property, descriptor) {
 
 			object.attachEvent('onpropertychange', callee);
 		});
+	}
+
+	if ('value' in descriptor) {
+		object[propertyString] = descriptor.value;
 	}
 
 	// return object
