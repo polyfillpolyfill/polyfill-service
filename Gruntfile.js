6916c7fc30118666ac0b4594ef03702b22e223c4
SamGiles <samuel.giles@ft.com>
2015-04-10 19:05:01 +0100

    Replace the grunt-service tasks with a modified, compiled version.  The normal version had quite verbose logging, which logged the environment variables (including the unencrypted sauce API key).  Also added logic to only declare the service started if the 'Server listening on' string has been seen on stdout

--- Gruntfile.js
+++ Gruntfile.js
@@ -89,7 +89,6 @@ module.exports = function(grunt) {
 	grunt.loadTasks('tasks');
 	grunt.loadNpmTasks('grunt-contrib-clean');
 	grunt.loadNpmTasks('grunt-contrib-watch');
-	grunt.loadNpmTasks('grunt-service');
 	grunt.loadNpmTasks('grunt-simple-mocha');
 
 	grunt.registerTask("test", [
