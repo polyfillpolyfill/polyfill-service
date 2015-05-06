5ccd8894b46b767dcb8dacafaa1416cc4a797b92
Max Nordlund <max.nordlund@gmail.com>
2015-05-06 22:54:22 +0200

    Object.assign avoid arguments object deopt
    For more information see https://github.com/petkaantonov/bluebird/wiki/Optimization-killers#3-managing-arguments

--- polyfills/Object.assign/polyfill.js
+++ polyfills/Object.assign/polyfill.js
@@ -1,6 +1,6 @@
 Object.assign = function assign(target, source) {
-	for (var index = 1, key; index in arguments; ++index) {
-		source = arguments[index];
+	for (var index = 1, key, src; index < arguments.length; ++index) {
+		src = arguments[index];
 
 		for (key in source) {
 			if (Object.prototype.hasOwnProperty.call(source, key)) {
