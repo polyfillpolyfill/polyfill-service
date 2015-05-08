3385d9728b42f3fd9302fa6511634a591babc0ad
Max Nordlund <max.nordlund@gmail.com>
2015-05-08 23:04:30 +0200

    Fix variable renaming

--- polyfills/Object.assign/polyfill.js
+++ polyfills/Object.assign/polyfill.js
@@ -2,9 +2,9 @@ Object.assign = function assign(target, source) {
 	for (var index = 1, key, src; index < arguments.length; ++index) {
 		src = arguments[index];
 
-		for (key in source) {
-			if (Object.prototype.hasOwnProperty.call(source, key)) {
-				target[key] = source[key];
+		for (key in src) {
+			if (Object.prototype.hasOwnProperty.call(src, key)) {
+				target[key] = src[key];
 			}
 		}
 	}
