a1278fca850f1c907bd041a3dba54e1e868adb22
Jonathan Fielding <j.fielding@me.com>
2015-07-27 22:24:00 +0100

    fix the update library task so that it runs on multiple srcs

--- tasks/updatelibrary.js
+++ tasks/updatelibrary.js
@@ -6,9 +6,11 @@ module.exports = function(grunt) {
 
 	grunt.registerMultiTask('updatelibrary', 'Update Polyfill imported from external libraries', function() {
 		this.files.forEach(function (file) {
-			grunt.log.writeln('Running ' + file.src + ': ');
-			require(path.resolve(file.src.toString()))(grunt);
-			grunt.log.writeln('-------------');
+			file.src.forEach(function(src){
+				grunt.log.writeln('Running ' + src + ': ');
+				require(path.resolve(src.toString()))(grunt);
+				grunt.log.writeln('-------------');
+			});
 		});
 
 		grunt.log.writeln('Polyfills updated successfully');
