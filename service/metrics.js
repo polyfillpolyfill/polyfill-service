52839aa89a6e50dec804f537ced08323a873e664
SamGiles <samuel.giles@ft.com>
2015-08-13 13:28:29 +0100

    Include the dyno identifier, if available, in the metric key

--- service/metrics.js
+++ service/metrics.js
@@ -5,10 +5,11 @@ var reportInterval = 5000;
 var graphiteHost = process.env.GRAPHITE_HOST || null;
 var graphitePort = process.env.GRAPHITE_PORT || 2003;
 var envName = process.env.NODE_ENV || "unknown";
+var processIdentifier = process.env.DYNO || "unknown-process";
 
 var timer = null;
 var graphite = null;
-var data = Measured.createCollection('origami.polyfill.' + envName);
+var data = Measured.createCollection('origami.polyfill.' + envName + '.' + processIdentifier);
 
 if (graphiteHost) {
 	graphite = Graphite.createClient('plaintext://'+graphiteHost+':'+graphitePort);
