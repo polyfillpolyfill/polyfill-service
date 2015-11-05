7f278c7441cbd8afe713c0d398b6da01eb15e582
Tom Parker <tom.p.parker@googlemail.com>
2015-11-05 17:00:21 +0000

    Add blocked event loop metrics

--- service/index.js
+++ service/index.js
@@ -8,11 +8,17 @@ var metrics = require('./metrics');
 var testing = require('./testing');
 var docs = require('./docs');
 var appVersion = require('../package.json').version;
+var blocked = require('blocked');
 
 'use strict';
 
 metrics.gauge('memory', function() {
 	return process.memoryUsage().rss;
+});
+
+blocked(function(ms){
+	if (ms < 100) return;
+	metrics.counter('blockedeventloop.' + (Math.ceil(ms / 100) * 100)).inc();
 });
 
 var one_day = 60 * 60 * 24;
@@ -115,7 +121,6 @@ app.get(/^\/__health$/, function(req, res) {
 app.get(/^\/v([12])\/polyfill(\.\w+)(\.\w+)?/, function(req, res) {
 	metrics.meter('hits').mark();
 	var respTimeTimer = metrics.timer('respTime').start();
-
 	var apiVersion = parseInt(req.params[0], 10);
 	var firstParameter = req.params[1].toLowerCase();
 	var minified =  firstParameter === '.min';
