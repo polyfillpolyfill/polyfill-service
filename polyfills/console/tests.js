4aa43f6f032dffe3cd34c58ad103fa44e13eae5e
Andrew Betts <andrew.betts@ft.com>
2016-01-07 00:41:32 +0000

    throwException is synonym

--- polyfills/console/tests.js
+++ polyfills/console/tests.js
@@ -17,163 +17,163 @@ describe('console', function () {
 		it('assert()', function () {
 			expect(function () {
 				console.assert();
-			}).not.to['throw']();
+			}).not.to.throwException();
 		});
 
 		it('cd()', function () {
 			expect(function () {
 				console.cd();
-			}).not.to['throw']();
+			}).not.to.throwException();
 		});
 
 		it('clear()', function () {
 			expect(function () {
 				console.clear();
-			}).not.to['throw']();
+			}).not.to.throwException();
 		});
 
 		it('count()', function () {
 			expect(function () {
 				console.count();
-			}).not.to['throw']();
+			}).not.to.throwException();
 		});
 
 		it('debug()', function () {
 			expect(function () {
 				console.debug();
-			}).not.to['throw']();
+			}).not.to.throwException();
 		});
 
 		it('dir()', function () {
 			expect(function () {
 				console.dir();
-			}).not.to['throw']();
+			}).not.to.throwException();
 		});
 
 		it('dirxml()', function () {
 			expect(function () {
 				console.dirxml();
-			}).not.to['throw']();
+			}).not.to.throwException();
 		});
 
 		it('error()', function () {
 			expect(function () {
 				console.error();
-			}).not.to['throw']();
+			}).not.to.throwException();
 		});
 
 		it('exception()', function () {
 			expect(function () {
 				console.exception();
-			}).not.to['throw']();
+			}).not.to.throwException();
 		});
 
 		it('group()', function () {
 			expect(function () {
 				console.group();
-			}).not.to['throw']();
+			}).not.to.throwException();
 		});
 
 		it('groupCollapsed()', function () {
 			expect(function () {
 				console.groupCollapsed();
-			}).not.to['throw']();
+			}).not.to.throwException();
 		});
 
 		it('groupEnd()', function () {
 			expect(function () {
 				console.groupEnd();
-			}).not.to['throw']();
+			}).not.to.throwException();
 		});
 
 		it('info()', function () {
 			expect(function () {
 				console.info();
-			}).not.to['throw']();
+			}).not.to.throwException();
 		});
 
 		it('log()', function () {
 			expect(function () {
 				console.log();
-			}).not.to['throw']();
+			}).not.to.throwException();
 		});
 
 		it('markTimeline()', function () {
 			expect(function () {
 				console.markTimeline();
-			}).not.to['throw']();
+			}).not.to.throwException();
 		});
 
 		it('profile()', function () {
 			expect(function () {
 				console.profile();
-			}).not.to['throw']();
+			}).not.to.throwException();
 		});
 
 		it('profileEnd()', function () {
 			expect(function () {
 				console.profileEnd();
-			}).not.to['throw']();
+			}).not.to.throwException();
 		});
 
 		it('profiles()', function () {
 			expect(function () {
 				console.profiles();
-			}).not.to['throw']();
+			}).not.to.throwException();
 		});
 
 		it('show()', function () {
 			expect(function () {
 				console.show();
-			}).not.to['throw']();
+			}).not.to.throwException();
 		});
 
 		it('table()', function () {
 			expect(function () {
 				console.table();
-			}).not.to['throw']();
+			}).not.to.throwException();
 		});
 
 		it('time()', function () {
 			expect(function () {
 				console.time();
-			}).not.to['throw']();
+			}).not.to.throwException();
 		});
 
 		it('timeEnd()', function () {
 			expect(function () {
 				console.timeEnd();
-			}).not.to['throw']();
+			}).not.to.throwException();
 		});
 
 		it('timeStamp()', function () {
 			expect(function () {
 				console.timeStamp();
-			}).not.to['throw']();
+			}).not.to.throwException();
 		});
 
 		it('timeline()', function () {
 			expect(function () {
 				console.timeline();
-			}).not.to['throw']();
+			}).not.to.throwException();
 		});
 
 		it('timelineEnd()', function () {
 			expect(function () {
 				console.timelineEnd();
-			}).not.to['throw']();
+			}).not.to.throwException();
 		});
 
 		it('trace()', function () {
 			expect(function () {
 				console.trace();
-			}).not.to['throw']();
+			}).not.to.throwException();
 		});
 
 		it('warn()', function () {
 			expect(function () {
 				console.warn();
-			}).not.to['throw']();
+			}).not.to.throwException();
 		});
 
 	});
