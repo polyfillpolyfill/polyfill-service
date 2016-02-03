cb6f297a7c1eb44c2b0682e316842a01027fbea8
Nic Jansma <nic@nicj.net>
2016-02-03 13:20:24 -0500

    Review feedback

--- polyfills/UserTiming/detect.js
+++ polyfills/UserTiming/detect.js
@@ -1,1 +1,1 @@
-'performance' in window && typeof window.performance.getEntriesByType === 'function' && typeof window.performance.mark === 'function'
+'performance' in this && typeof this.performance.getEntriesByType === 'function' && typeof this.performance.mark === 'function'
