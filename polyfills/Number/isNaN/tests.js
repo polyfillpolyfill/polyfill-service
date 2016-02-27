3085addb8c8f6191f2ee79b85b49f721e194742e
rblineau <romain.blineau@amadeus.com>
2016-02-27 18:27:06 +0100

    poylill Number.isNaN - refactor test and add specification reference

--- polyfills/Number/isNaN/tests.js
+++ polyfills/Number/isNaN/tests.js
@@ -9,35 +9,19 @@ it('has correct argument length', function () {
 describe('returns true with', function () {
 	it('numbers', function () {
 		expect(Number.isNaN(NaN)).to.be(true);
-	});
-	it('numbers', function () {
 		expect(Number.isNaN(Number.NaN)).to.be(true);
-	});
-	it('numbers', function () {
 		expect(Number.isNaN(0/0)).to.be(true);
-	});		
+	});
 });
 
 describe('returns false with', function () {
 	it('numbers', function () {
 		expect(Number.isNaN("NaN")).to.be(false);
-	});
-	it('numbers', function () {
 		expect(Number.isNaN(undefined)).to.be(false);
-	});
-	it('numbers', function () {
 		expect(Number.isNaN({})).to.be(false);
-	});		
-	it('numbers', function () {
 		expect(Number.isNaN("blabla")).to.be(false);
-	});
-	it('numbers', function () {
 		expect(Number.isNaN(true)).to.be(false);
-	});
-	it('numbers', function () {
 		expect(Number.isNaN(37)).to.be(false);
-	});
-	it('numbers', function () {
 		expect(Number.isNaN("37")).to.be(false);
 	});	
 });
