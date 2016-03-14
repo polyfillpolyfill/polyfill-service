1f09087cf2162926698b163cf1fce05845789907
Jake Champion <jakechampion.jake2@googlemail.com>
2016-03-14 21:18:34 +0000

    Add more tests

--- polyfills/Array/prototype/fill/tests.js
+++ polyfills/Array/prototype/fill/tests.js
@@ -44,3 +44,15 @@ it('can use a negative end index', function () {
 it('does not fill if start index is larger than array', function () {
 	expect([1, 2, 3].fill(0, 5)).to.eql([1, 2, 3]);
 });
+
+it('does fill if start index is not a number', function () {
+	expect([1, 2, 3].fill(0, NaN)).to.eql([0, 0, 0]);
+	expect([1, 2, 3].fill(1, '')).to.eql([1, 1, 1]);
+	expect([1, 2, 3].fill(2, {})).to.eql([2, 2, 2]);
+});
+
+it('does not fill if end index is not a number', function () {
+	expect([1, 2, 3].fill(0, 0, NaN)).to.eql([1, 2, 3]);
+	expect([1, 2, 3].fill(1, 0, '')).to.eql([1, 2, 3]);
+	expect([1, 2, 3].fill(2, 0, {})).to.eql([1, 2, 3]);
+});
