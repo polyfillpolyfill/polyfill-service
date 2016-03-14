1cf93107b82c436aebf2f64e5470cb80c03431e8
Jake Champion <jakechampion.jake2@googlemail.com>
2016-03-14 21:13:13 +0000

    Add tests

--- polyfills/Array/prototype/fill/tests.js
+++ polyfills/Array/prototype/fill/tests.js
@@ -1,3 +1,7 @@
+it('exists', function () {
+	expect(Array.prototype).to.have.property('fill');
+});
+
 it('has correct instance', function () {
 	expect(Array.prototype.fill).to.be.a(Function);
 });
@@ -13,4 +17,30 @@ it('has correct argument length', function () {
 	expect(Array.prototype.fill.length).to.be(1);
 });
 
-// TODO: Add functionality tests
+it('is not enumerable', function () {
+	expect(Array.prototype.propertyIsEnumerable('fill')).to.be.false;
+});
+
+it('fills whole array when using only one argument', function () {
+	expect([1, 2, 3].fill(0)).to.eql([0, 0, 0]);
+});
+
+it('starts filling from the start index given by second argument', function () {
+	expect([1, 2, 3, 4, 5, 6].fill(0, 3)).to.eql([1, 2, 3, 0, 0, 0]);
+});
+
+it('can use a negative start index', function () {
+	expect([1, 2, 3, 4, 5, 6].fill(0, -2)).to.eql([1, 2, 3, 4, 0, 0]);
+});
+
+it('stops filling at the end index given by third argument', function () {
+	expect([1, 2, 3, 4, 5, 6].fill(0, 0, 2)).to.eql([0, 0, 3, 4, 5, 6]);
+});
+
+it('can use a negative end index', function () {
+	expect([1, 2, 3, 4, 5, 6].fill(0, 1, -2)).to.eql([1, 0, 0, 0, 5, 6]);
+});
+
+it('does not fill if start index is larger than array', function () {
+	expect([1, 2, 3].fill(0, 5)).to.eql([1, 2, 3]);
+});
