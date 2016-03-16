c52214b0503fecfae168e4b2d53a0fc18132c064
Jake Champion <jakechampion.jake2@googlemail.com>
2016-03-16 23:15:55 +0000

    Add more tests

--- polyfills/Array/prototype/fill/tests.js
+++ polyfills/Array/prototype/fill/tests.js
@@ -19,6 +19,7 @@ it('has correct argument length', function () {
 
 it('fills whole array when using only one argument', function () {
 	expect([1, 2, 3].fill(0)).to.eql([0, 0, 0]);
+	expect([1, 2, 3].fill()).to.eql([undefined, undefined, undefined]);
 });
 
 it('starts filling from the start index given by second argument', function () {
@@ -41,18 +42,75 @@ it('does not fill if start index is larger than array', function () {
 	expect([1, 2, 3].fill(0, 5)).to.eql([1, 2, 3]);
 });
 
-it('does fill if start index is not a number', function () {
-	expect([1, 2, 3].fill(0, NaN)).to.eql([0, 0, 0]);
-	expect([1, 2, 3].fill(1, '')).to.eql([1, 1, 1]);
-	expect([1, 2, 3].fill(2, {})).to.eql([2, 2, 2]);
-});
-
-it('does not fill if end index is not a number', function () {
-	expect([1, 2, 3].fill(0, 0, NaN)).to.eql([1, 2, 3]);
-	expect([1, 2, 3].fill(1, 0, '')).to.eql([1, 2, 3]);
-	expect([1, 2, 3].fill(2, 0, {})).to.eql([1, 2, 3]);
-});
+// `this` tests
 
 it('works on array-like objects', function () {
   expect([].fill.call({ length: 3 }, 4)).to.eql({0: 4, 1: 4, 2: 4, length: 3});
+});
+
+it('returns the same array', function() {
+  var arr = [];
+  var result = arr.fill(1);
+
+  expect(result).to.equal(arr);
+});
+
+// start parameter tests
+
+it('undefined start coerced to 0', function() {
+  expect([0, 0].fill(1, undefined)).to.eql([1, 1])
+});
+
+it('null start coerced to 0', function() {
+  expect([0, 0].fill(1, null)).to.eql([1, 1])
+});
+
+it('true start coerced to 1', function() {
+  expect([0, 0].fill(1, true)).to.eql([0, 1])
+});
+
+it('false start coerced to 0', function() {
+  expect([0, 0].fill(1, false)).to.eql([1, 1])
+});
+
+it('NaN start coerced to 0', function() {
+  expect([0, 0].fill(1, NaN)).to.eql([1, 1])
+});
+
+it('string start coerced to integer', function() {
+  expect([0, 0].fill(1, '1')).to.eql([0, 1])
+});
+
+it('float start coerced to integer', function() {
+  expect([0, 0].fill(1, 1.5)).to.eql([0, 1])
+});
+
+// end parameter tests
+
+it('undefined end coerced to length of `this`', function() {
+  expect([0, 0].fill(1, 0, undefined)).to.eql([1, 1])
+});
+
+it('null end coerced to 0', function() {
+  expect([0, 0].fill(1, 0, null)).to.eql([0, 0])
+});
+
+it('true end coerced to 1', function() {
+  expect([0, 0].fill(1, 0, true)).to.eql([1, 0])
+});
+
+it('false end coerced to 0', function() {
+  expect([0, 0].fill(1, 0, false)).to.eql([0, 0])
+});
+
+it('NaN end coerced to 0', function() {
+  expect([0, 0].fill(1, 0, NaN)).to.eql([0, 0])
+});
+
+it('string end coerced to integer', function() {
+  expect([0, 0].fill(1, 0, '1')).to.eql([1, 0])
+});
+
+it('float end coerced to integer', function() {
+  expect([0, 0].fill(1, 0, 1.5)).to.eql([1, 0])
 });
