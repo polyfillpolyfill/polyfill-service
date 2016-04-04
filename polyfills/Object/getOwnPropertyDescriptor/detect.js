0936c3fe3251c6914e39d72598cfa0adb4033269
Jake Champion <jakechampion.jake2@googlemail.com>
2016-04-04 22:30:24 +0100

    update detect

--- polyfills/Object/getOwnPropertyDescriptor/detect.js
+++ polyfills/Object/getOwnPropertyDescriptor/detect.js
@@ -1,1 +1,11 @@
-'getOwnPropertyDescriptor' in Object && typeof Object.getOwnPropertyDescriptor === 'function'
+'getOwnPropertyDescriptor' in Object && typeof Object.getOwnPropertyDescriptor === 'function' && (function doesGetOwnPropertyDescriptorWork(object) {
+    try {
+        object.sentinel = 0;
+        return Object.getOwnPropertyDescriptor(
+            object,
+            "sentinel"
+        ).value === 0;
+    } catch (exception) {
+        // returns falsy
+    }
+}())
