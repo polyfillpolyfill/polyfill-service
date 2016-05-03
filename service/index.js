5800105b4bc1ad0bcc4fc5231bf26cd53242c343
Andrew Betts <andrew@trib.tv>
2016-05-03 14:37:25 +0100

    Use Node 6 to handle HTTP clientError scenarios correctly, fixes #567 (#686)
    * Respond correctly to invalid HTTP requests
    
    * Log uncaught exceptions (previously opaque)
    
    * Fix ref error

--- service/index.js
+++ service/index.js
@@ -212,7 +212,8 @@ function startService(port, callback) {
 			callback(err);
 		})
 		.on('clientError', function (ex, sock) {
-			console.log('HTTP clientError: ', ex.code, sock);
+			sock.end('HTTP/1.1 400 Bad Request\r\n\r\n');
+			sock.destroy();
 		})
 	;
 }
