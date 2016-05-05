28f94b0665c14a7a010b8b99821031a6c71cd908
Andrew Betts <andrew.betts@ft.com>
2016-05-05 09:48:56 +0100

    Remove global reference and disacard unnecessary UMD

--- polyfills/navigator/sendBeacon/polyfill.js
+++ polyfills/navigator/sendBeacon/polyfill.js
@@ -1,33 +1,15 @@
-(function(root) {
-
-	function sendBeacon(url, data) {
-		var xhr = ('XMLHttpRequest' in window) ? new XMLHttpRequest() : new ActiveXObject('Microsoft.XMLHTTP');
-		xhr.open('POST', url, false);
-		xhr.setRequestHeader('Accept', '*/*');
-		if (typeof data === 'string') {
-			xhr.setRequestHeader('Content-Type', 'text/plain;charset=UTF-8');
-			xhr.responseType = 'text/plain';
-		} else if (Object.prototype.toString.call(data) === '[object Blob]') {
-			if (data.type) {
-				xhr.setRequestHeader('Content-Type', data.type);
-			}
+this.navigator && this.navigator.sendBeacon = function sendBeacon(url, data) {
+	var xhr = ('XMLHttpRequest' in window) ? new XMLHttpRequest() : new ActiveXObject('Microsoft.XMLHTTP');
+	xhr.open('POST', url, false);
+	xhr.setRequestHeader('Accept', '*/*');
+	if (typeof data === 'string') {
+		xhr.setRequestHeader('Content-Type', 'text/plain;charset=UTF-8');
+		xhr.responseType = 'text/plain';
+	} else if (Object.prototype.toString.call(data) === '[object Blob]') {
+		if (data.type) {
+			xhr.setRequestHeader('Content-Type', data.type);
 		}
-		xhr.send(data);
-		return true;
 	}
-
-	if (!('sendBeacon' in navigator)) {
-		navigator.sendBeacon = sendBeacon;
-	}
-
-	if (typeof exports !== 'undefined') {
-		if (typeof module !== 'undefined' && module.exports) {
-			exports = module.exports = sendBeacon;
-		}
-		exports.sendBeacon = sendBeacon;
-	} else if (typeof define === 'function' && define.amd) {
-		define([], function() {
-			return sendBeacon;
-		});
-	}
-})(this);
+	xhr.send(data);
+	return true;
+}
