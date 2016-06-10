e855c404b32c981c5716425eb4752263c7479af6
Rhys Evans <rhys.evans@ft.com>
2016-06-10 10:07:19 +0100

    removed NodeList.iterator (pending further investigation of why it causes stack overflow in ie

--- polyfills/NodeList/prototype/@@iterator/detect.js
+++
@@ -1,5 +0,0 @@
-'Symbol' in this && 'iterator' in this.Symbol && (function () {
-	var fragment = document.createDocumentFragment();
-	fragment.appendChild(document.createElement('div'));
-	return !!fragment.childNodes[Symbol.iterator];
-})()
--- polyfills/NodeList/prototype/@@iterator/polyfill.js
+++
@@ -1,6 +0,0 @@
-(function () {
-	var ArrayIterator = require('es6-iterator/array');
-	NodeList.prototype[Symbol.iterator] = function () {
-	  return new ArrayIterator(this);
-	};
-})();
