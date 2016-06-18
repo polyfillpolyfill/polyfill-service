36e8656a27723cc62a25ba90c9d5bfe68a3a4f8e
Andrew Betts <Andrew.Betts@ft.com>
2016-06-18 13:50:17 +0900

    Upstream update from Google

--- polyfills/IntersectionObserver/polyfill.js
+++ polyfills/IntersectionObserver/polyfill.js
@@ -382,15 +382,16 @@ IntersectionObserver.prototype._getRootRect = function() {
   if (this.root) {
     rootRect = getBoundingClientRect(this.root);
   } else {
-    // Use <html> intead of window since scroll bars affect size.
+    // Use <html>/<body> intead of window since scroll bars affect size.
     var html = document.documentElement;
+    var body = document.body;
     rootRect = {
       top: 0,
       left: 0,
-      right: html.clientWidth,
-      width: html.clientWidth,
-      bottom: html.clientHeight,
-      height: html.clientHeight
+      right: html.clientWidth || body.clientWidth,
+      width: html.clientWidth || body.clientWidth,
+      bottom: html.clientHeight || body.clientHeight,
+      height: html.clientHeight || body.clientHeight
     };
   }
   return this._expandRectByRootMargin(rootRect);
