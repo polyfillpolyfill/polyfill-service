c1b8214b26929aede206cbec78df185df142a229
Andrew Betts <Andrew.Betts@ft.com>
2016-06-21 19:49:43 +0900

    Use require util to resolve path

--- service/routes/test.js
+++ service/routes/test.js
@@ -82,9 +82,9 @@ function createEndpoint(type, polyfillio) {
 		;
 	};
 }
-
-router.use('/libs/mocha', express.static(path.join(__dirname, '/../../node_modules/mocha')));
-router.use('/libs/expect', express.static(path.join(__dirname, '/../../node_modules/expect.js/')));
+console.log(require.resolve('mocha'));
+router.use('/libs/mocha', express.static(path.dirname(require.resolve('mocha'))));
+router.use('/libs/expect', express.static(path.dirname(require.resolve('expect.js'))));
 
 router.get(/\/director\/?$/, createEndpoint('director', polyfillio));
 router.get(/\/tests?\/?$/, createEndpoint('runner', polyfillio));
