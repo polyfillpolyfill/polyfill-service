250cc90cb96bcd89707215e84b9ad9d8054826c5
Andrew Betts <Andrew.Betts@ft.com>
2016-07-04 18:08:13 +0900

    Improve RUM reporting

--- service/routes/rum.js
+++ service/routes/rum.js
@@ -56,25 +56,45 @@ router.get('/v2/recordRumData', (req, res) => {
 });
 
 router.get('/v2/getRumPerfData', (req, res) => {
-	const daterange = "req_time BETWEEN (CURDATE() - INTERVAL 7 DAY) AND CURDATE()";
-	mysql.query('SELECT COUNT(*) as count FROM requests WHERE '+daterange).then(rows => {
-		const count = rows[0].count;
-		return Promise.all([
-			mysql.query('SELECT COUNT(*) as count, data_center, country, AVG(perf_dns) as perf_dns_mean, STD(perf_dns) as perf_dns_std FROM requests WHERE '+daterange+' GROUP BY data_center, country'),
-			mysql.query('SELECT data_center, country, (SELECT MAX(perf_dns) FROM requests r2 WHERE data_center=r1.data_center AND country=r1.country AND '+daterange+' ORDER BY perf_dns DESC LIMIT ?) as perf_dns_95 FROM requests r1 WHERE '+daterange+' GROUP BY data_center, country', [Math.floor(count*0.05)])
-		]);
-	})
-	.then(results => {
-		let data = {};
-		results[0].forEach(row => {
-			if (!(row.data_center in data)) data[row.data_center] = {};
-			data[row.data_center][row.country] = row;
-		});
-		results[1].forEach(row => {
-			data[row.data_center][row.country].perf_dns_95 = row.perf_dns_95;
-		});
-		res.json(data);
-	})
+	const daterange = "req_time BETWEEN (CURDATE() - INTERVAL 30 DAY) AND CURDATE()";
+	if (!mysql) {
+		return res.status(500).json('MySQL connection not available');
+	}
+	mysql.query('SELECT COUNT(*) as count FROM requests WHERE '+daterange)
+		.then(rows => {
+			const count = rows[0].count;
+			return Promise.all([
+				mysql.query(`
+					SELECT
+						data_center,
+						country,
+						COUNT(*) as count,
+						AVG(perf_dns) as perf_dns_mean,
+						STD(perf_dns) as perf_dns_std,
+						AVG(perf_connect) as perf_connect_mean,
+						STD(perf_connect) as perf_connect_std,
+						AVG(perf_req) as perf_req_mean,
+						STD(perf_req) as perf_req_std,
+						AVG(perf_resp) as perf_resp_mean,
+						STD(perf_resp) as perf_resp_std
+					FROM requests WHERE ${daterange}
+					GROUP BY data_center, country`
+				),
+				mysql.query('SELECT data_center, country, (SELECT perf_dns FROM requests r2 WHERE data_center=r1.data_center AND country=r1.country AND '+daterange+' ORDER BY perf_dns DESC LIMIT ?, 1) as perf_dns_95 FROM requests r1 WHERE '+daterange+' GROUP BY data_center, country', [Math.floor(count*0.05)]),
+				mysql.query('SELECT data_center, country, (SELECT perf_dns FROM requests r2 WHERE data_center=r1.data_center AND country=r1.country AND '+daterange+' ORDER BY perf_dns DESC LIMIT ?, 1) as perf_dns_99 FROM requests r1 WHERE '+daterange+' GROUP BY data_center, country', [Math.floor(count*0.01)])
+			]);
+		})
+		.then(results => {
+			const makeKey = row => row.country + ' ' + row.data_center;
+			const data = results.reduce((acc, resultset) => {
+				resultset.forEach(row => {
+					acc[makeKey(row)] = Object.assign({}, acc[makeKey(row)], row);
+				})
+				return acc;
+			}, {});
+			res.json(Object.keys(data).map(k => data[k]));
+		})
+	;
 });
 
 router.get('/v2/getRumCompatData', (req, res) => {
