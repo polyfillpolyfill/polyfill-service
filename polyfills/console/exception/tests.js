8375ba2cda5836a2b866e5e35261ea342aa9f8eb
Jake Champion <jakechampion.jake2@googlemail.com>
2016-08-06 18:45:35 +0100

    Switch console.exception tests to proclaim

--- polyfills/console/exception/tests.js
+++ polyfills/console/exception/tests.js
@@ -1,9 +1,12 @@
+/* eslint-env mocha, browser*/
+/* global proclaim, it */
+
 describe('console', function () {
 
 	it('exception()', function () {
-		expect(function () {
+		proclaim.doesNotThrow(function () {
 			console.exception();
-		}).not.to.throwException();
+		});
 	});
 
 });
