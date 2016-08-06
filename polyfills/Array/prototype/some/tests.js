76d08fa06f6d397209a1d2832af536d8186eb7a7
Jake Champion <jakechampion.jake2@googlemail.com>
2016-08-06 18:38:48 +0100

    Switch Array.prototype.some tests to proclaim

--- polyfills/Array/prototype/some/tests.js
+++ polyfills/Array/prototype/some/tests.js
@@ -1,3 +1,6 @@
+/* eslint-env mocha, browser*/
+/* global proclaim, it */
+
 // See: https://people.mozilla.org/~jorendorff/es6-draft.html#sec-array.prototype.some
 
 beforeEach(function() {
@@ -5,20 +8,16 @@ beforeEach(function() {
 });
 
 it("Should not accept a function argument that is not callable", function() {
-	function assertTypeError(e) {
-		expect(e).to.be.a(TypeError);
-	}
-
 	var array = this.array;
-	expect(function() { array.some({}); }).to.throwException(assertTypeError);
+	proclaim.throws(function() { array.some({}); }, TypeError);
 });
 
 it("Should accept a function with three parameters: the value of the element, the index of the element and the object being traversed", function() {
 	var array = this.array;
 
 	array.some(function(value, index, object) {
-		expect(object).to.be(array);
-		expect(object[index]).to.be(value);
+		proclaim.strictEqual(object, array);
+		proclaim.equal(object[index], value);
 		return false;
 	});
 });
@@ -28,8 +27,8 @@ it("Should accept an optional 'this' argument as its second argument which becom
 	var array = this.array;
 
 	array.some(function(value) {
-		expect(this).to.be(mockThis);
-		expect(this.foo).to.be(mockThis.foo);
+		proclaim.strictEqual(this, mockThis);
+		proclaim.equal(this.foo, mockThis.foo);
 		return false;
 	}, mockThis);
 });
@@ -42,7 +41,7 @@ it("Should pass a reference to the array as the third parameter to the function 
 		return false;
 	});
 
-	expect(array[0]).to.be(100);
+	proclaim.equal(array[0], 100);
 });
 
 it("Should not iterate over elements appended to the array after the call to some.  The range is fixed before the first call to the callback", function() {
@@ -55,17 +54,17 @@ it("Should not iterate over elements appended to the array after the call to som
 		return false;
 	});
 
-	expect(visited).to.eql([1, 2, 3, 4, 5, 6]);
+	proclaim.deepEqual(visited, [1, 2, 3, 4, 5, 6]);
 
 	// Expect the original array to be the same for the first 6 elements, with
 	// the additional 0, 1, .. 5 appended (the indices of the first 6 elements
 	// in the range covered by some at the start of its invocation
-	expect(array).to.eql([1, 2, 3, 4, 5, 6, 0, 1, 2, 3, 4, 5]);
+	proclaim.deepEqual(array, [1, 2, 3, 4, 5, 6, 0, 1, 2, 3, 4, 5]);
 });
 
 it("Should return false if the array is empty", function() {
 	var a = [];
-	expect(a.some(function(value) { return true; })).to.be(false);
+	proclaim.equal(a.some(function(value) { return true; }), false);
 });
 
 it("Should not visit elements that are deleted after the call to some begins and before being visited", function() {
@@ -79,27 +78,27 @@ it("Should not visit elements that are deleted after the call to some begins and
 	});
 
 	// Should only visit the first 5 indices
-	expect(visited).to.eql([0, 1, 2, 3, 4]);
+	proclaim.deepEqual(visited, [0, 1, 2, 3, 4]);
 });
 
 it("Should return true as soon as the callback returns true", function() {
 	var a = [0, 1, 2, 3, 4];
 	var visited = [];
-	expect(a.some(function(value) {
+	proclaim.equal(a.some(function(value) {
 		visited.push(value);
 		return value > 2;
-	})).to.be(true);
+	}), true);
 
-	expect(visited).to.eql([0, 1, 2, 3]);
+	proclaim.deepEqual(visited, [0, 1, 2, 3]);
 });
 
 it("Should return false if the callback never returns true", function() {
 	var a = [0, 1, 2, 3, 4];
 	var visited = [];
-	expect(a.some(function(value) {
+	proclaim.equal(a.some(function(value) {
 		visited.push(value);
 		return false;
-	})).to.be(false);
+	}), false);
 
-	expect(visited).to.eql(a);
+	proclaim.deepEqual(visited, a);
 });
