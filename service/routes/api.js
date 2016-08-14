71e73a10745127140798829e8b00af8b7371a062
Jake Champion <jakechampion.jake2@googlemail.com>
2016-08-14 23:00:08 +0100

    Add endpoint exposing feature detect combinator

--- service/routes/api.js
+++ service/routes/api.js
@@ -84,6 +84,21 @@ router.get(/^\/v2\/polyfill(\.\w+)(\.\w+)?/, (req, res) => {
 
 });
 
+router.get(/^\/v2\/detect(\.\w+)(\.\w+)?/, (req, res) => {
+	const polyfills = PolyfillSet.fromQueryParam(req.query.features);
+
+	const params = {
+		features: polyfills.get()
+	};
+
+	polyfillio.getDetectString(params).then(op => {
+		res.set('Content-Type', contentTypes['.js']+';charset=utf-8');
+		res.set('Access-Control-Allow-Origin', '*');
+		res.send(op);
+	});
+
+});
+
 router.get("/v2/normalizeUa", (req, res) => {
 
 	if (req.query.ua) {
