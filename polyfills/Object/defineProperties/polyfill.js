7845ed3ec83c987b02d77aec7f063106f6fc49eb
Jake Champion <me@jakechampion.name>
2016-08-21 00:19:29 +0100

    Update polyfill.js

--- polyfills/Object/defineProperties/polyfill.js
+++ polyfills/Object/defineProperties/polyfill.js
@@ -1,8 +1,6 @@
 Object.defineProperties = function defineProperties(object, descriptors) {
 	for (var property in descriptors) {
-		if (Object.hasOwnProperty(property)) {
-			Object.defineProperty(object, property, descriptors[property]);
-		}
+		Object.defineProperty(object, property, descriptors[property]);
 	}
 
 	return object;
