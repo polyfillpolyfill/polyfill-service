9fe06f30f704ad78aa90261bade1e50f57d1d15d
Jake Champion <jakechampion.jake2@googlemail.com>
2016-10-16 15:09:37 +0100

    switch from jshint to eslint

--- docs/assets/js/ui.js
+++ docs/assets/js/ui.js
@@ -1,3 +1,5 @@
+/* global google */
+'use strict';
 
 // Demos
 Array.from(document.querySelectorAll('.demo')).forEach(function(el) {
@@ -5,42 +7,78 @@ Array.from(document.querySelectorAll('.demo')).forEach(function(el) {
 	var xhr = new XMLHttpRequest();
 	xhr.open("get", el.getAttribute('data-src'));
 	xhr.addEventListener('load', function() {
-	  el.textContent = this.responseText;
+		el.textContent = this.responseText;
 	});
 	xhr.send();
 	code.classList.add('prettyprint');
-	code.innerHTML = '<code>&lt;script src="https://cdn.polyfill.io'+el.getAttribute('data-src')+'"&gt;&lt;/script&gt;</code>';
+	code.innerHTML = '<code>&lt;script src="https://cdn.polyfill.io' + el.getAttribute('data-src') + '"&gt;&lt;/script&gt;</code>';
 	el.parentNode.insertBefore(code, el);
 });
 
 // Perf charts
 function charts() {
-	var chart1, data1, chart2, data2, chart3, data3, drawCharts;
-	drawCharts = function() {
+	var chart1;
+	var data1;
+	var chart2;
+	var data2;
+	var chart3;
+	var data3;
+	var	drawCharts = function() {
 		chart1 && chart1.draw(data1, {
-			chartArea:{left:0,top:0,width:'100%',height:'90%'},
-			hAxis: {title: 'Time (by day)'},
-			vAxis: {textPosition: 'in', minValue:0},
-			legend: {position: 'none'}
+			chartArea: {
+				left: 0,
+				top: 0,
+				width: '100%',
+				height: '90%'
+			},
+			hAxis: {
+				title: 'Time (by day)'
+			},
+			vAxis: {
+				textPosition: 'in',
+				minValue: 0
+			},
+			legend: {
+				position: 'none'
+			}
 		});
 		chart2 && chart2.draw(data2, {
 			pieHole: 0.6,
-			chartArea:{left:'5%',top:'5%',width:'100%',height:'90%'},
-			legend: {position: 'labeled'},
+			chartArea: {
+				left: '5%',
+				top: '5%',
+				width: '100%',
+				height: '90%'
+			},
+			legend: {
+				position: 'labeled'
+			},
 			pieSliceText: 'none'
 		});
 		chart3 && chart3.draw(data3, {
-			chartArea:{left:30,top:10,width:'100%',height:'80%'},
-			legend: {position: 'none'},
-			hAxis: {textPosition: 'none'},
-			vAxis: {minValue: 0}
+			chartArea: {
+				left: 30,
+				top: 10,
+				width: '100%',
+				height: '80%'
+			},
+			legend: {
+				position: 'none'
+			},
+			hAxis: {
+				textPosition: 'none'
+			},
+			vAxis: {
+				minValue: 0
+			}
 		});
 	};
-	if (document.getElementById('chart-requests')) {
+	var chartel = document.getElementById('chart-requests');
+
+	if (chartel) {
 		google.load('visualization', '1.0', {
 			packages: ['corechart'],
 			callback: function() {
-				var chartel = document.getElementById('chart-requests');
 				if (chartel) {
 					data1 = google.visualization.arrayToDataTable(
 						Array.from(chartel.querySelectorAll('tr')).map(function(rowel) {
@@ -85,22 +123,29 @@ function charts() {
 
 // Test current browser
 (function() {
-	var testsRunning = false, resultsByFeature = {};
+	var testsRunning = false;
+	var resultsByFeature = {};
+
 	function runTests() {
 		window.receiveTestResult = processLiveResult;
 		if ('postMessage' in window) {
 			window.addEventListener("message", function(message) {
-				try { data = JSON.parse(msg.data); } catch (e) { return; }
+				try {
+					data = JSON.parse(msg.data);
+				} catch (e) {
+					return;
+				}
 				processLiveResult(data.featureName, data.result, data.mode);
 			}, false);
 		}
 		['control', 'all'].forEach(function(mode) {
 			var ifr = document.createElement('iframe');
-			ifr.className = 'testFrame testFrame--'+mode;
+			ifr.className = 'testFrame testFrame--' + mode;
 			document.body.appendChild(ifr);
-			ifr.src = '/test/director?mode='+mode;
+			ifr.src = '/test/director?mode=' + mode;
 		});
 	}
+
 	function processLiveResult(featureName, result, mode) {
 		resultsByFeature[featureName] = resultsByFeature[featureName] || {};
 		resultsByFeature[featureName][mode] = result;
@@ -109,16 +154,15 @@ function charts() {
 				(resultsByFeature[featureName].control === 'pass') ? 'native' :
 				(resultsByFeature[featureName].all === 'pass') ? 'polyfilled' :
 				(resultsByFeature[featureName].all === 'fail') ? 'missing' :
-				'unknown'
-			;
-			var resultCell = document.querySelector('#'+slugify(featureName)+' .compatibility__live');
+				'unknown';
+			var resultCell = document.querySelector('#' + slugify(featureName) + ' .compatibility__live');
 			if (resultCell) {
 				resultCell.innerHTML =
-					'<span class="status-'+status+'">'+status+'</span>'
-				;
+					'<span class="status-' + status + '">' + status + '</span>';
 			}
 		}
 	}
+
 	function slugify(str) {
 		return str.replace(/[^\w]/g, '_');
 	}
@@ -128,8 +172,8 @@ function charts() {
 				if (el.checked === true) {
 					var cl = document.querySelector('.compatibility').classList;
 					cl.remove('compatibility--data', 'compatibility--live');
-					cl.add('compatibility--'+el.value);
-					if (el.value == 'live' && !testsRunning) {
+					cl.add('compatibility--' + el.value);
+					if (el.value === 'live' && !testsRunning) {
 						runTests();
 						testsRunning = true;
 					}
