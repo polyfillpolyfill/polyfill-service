834fe4d4beb55d0f67599f4840230aa702a97b29
Andrew Betts <andrew@trib.tv>
2016-12-02 19:11:30 +0900

    Rum report (#999)
    * Improve architecture of RUM reporting, making it easier to expose more options to end user
    
    * Fix missing RUM documentation
    
    * Update docs to match new implementation
    
    * Fix documentation report, add median line

--- service/RumReport.js
+++ service/RumReport.js
@@ -6,27 +6,41 @@ const Stats = require('fast-stats').Stats;
 const UA = require('../lib/UA');
 const polyfillio = require('../lib/index');
 
-function Perf(options) {
+function Perf(rawopts) {
 
+	const optionsSchema = {
+		metrics: {
+			type: 'array',
+			default:['perf_dns', 'perf_connect', 'perf_req', 'perf_resp', 'perf_total'],
+			valid:['perf_dns', 'perf_connect', 'perf_req', 'perf_resp', 'perf_total']
+		},
+		dimensions: {
+			type: 'array',
+			default: ['data_center'],
+			valid: ['data_center', 'country', 'refer_domain']
+		},
+		stats: {
+			type: 'array',
+			default: ['median', '95P'],
+			valid: ['mean', 'median', '95P', '99P', 'std', 'min', 'max', 'count']
+		},
+		period: { type:'number', default: 30, min: 1, max: 60 },
+		minSample: { type:'number', default: 500, min: 50 }
+	};
 	let dbconn;
 
 	if (!process.env.RUM_MYSQL_DSN) throw new Error('RUM disabled.  See README for environment variables required for RUM reporting.');
 
-	options = Object.assign({
-		groupingFields: ['data_center'],
-		metricFields: ['perf_dns', 'perf_connect', 'perf_req', 'perf_resp', 'perf_total'],
-		stats: [],
-		period: 30,
-		minSampleSize: 20
-	}, options);
+	const options = validateOptions(rawopts, optionsSchema);
 
-	const sqlFields = options.groupingFields
-		.concat(options.metricFields)
+	const sqlFields = options.dimensions
+		.concat(options.metrics)
 		.map(fName => (fName === 'perf_total') ? '(perf_dns+perf_connect+perf_req+perf_resp) as perf_total' : fName)
 		.join(', ')
 	;
 
-	const sqlQuery = `SELECT ${sqlFields} FROM requests WHERE req_time BETWEEN (CURDATE() - INTERVAL ${options.period} DAY) AND CURDATE() AND data_center IS NOT NULL LIMIT 1000000`;
+	const sqlQuery = `SELECT ${sqlFields} FROM requests WHERE req_time BETWEEN (CURDATE() - INTERVAL ${options.period} DAY) AND CURDATE() AND data_center IS NOT NULL AND perf_req IS NOT NULL LIMIT 1000000`;
+console.log(options.minSample);
 
 	const dataPromise = MySQL.createConnection(process.env.RUM_MYSQL_DSN)
 		.then(conn => {
@@ -36,18 +50,18 @@ function Perf(options) {
 		.then(results => {
 			return dbconn.end().then(() => {
 				const objdata = results[0].reduce((out, row) => {
-					const key = options.groupingFields.map(fieldName => row[fieldName]).join('-');
+					const key = options.dimensions.map(fieldName => row[fieldName]).join('-');
 					if (!(key in out)) {
 						const aggregateRow = {count:0};
-						options.groupingFields.forEach(fieldName => {
+						options.dimensions.forEach(fieldName => {
 							aggregateRow[fieldName] = row[fieldName];
 						});
-						options.metricFields.forEach(fieldName => {
+						options.metrics.forEach(fieldName => {
 							aggregateRow[fieldName] = new Stats();
 						});
 						out[key] = aggregateRow;
 					}
-					options.metricFields.forEach(fieldName => {
+					options.metrics.forEach(fieldName => {
 						if (row[fieldName]) out[key][fieldName].push(row[fieldName]);
 					});
 					out[key].count++;
@@ -59,22 +73,25 @@ function Perf(options) {
 					.map(key => objdata[key])
 
 					// Remove any rows that don't have enough datapoints
-					.filter(row => row.count > options.minSampleSize)
+					.filter(row => row.count > options.minSample)
 
 					// Sort by number of datapoints
-					.sort((a, b) => a[options.metricFields[0]].length < b[options.metricFields[0]].length ? 1 : -1)
+					.sort((rowa, rowb) => rowa.count < rowb.count ? 1 : -1)
 
 					// Add derived data
 					.map(row => {
-						options.metricFields.forEach(metric => {
+						options.metrics.forEach(metric => {
 							if (options.stats.includes('95P')) {
-								row[metric+'_95'] = row[metric].percentile(95);
+								row[metric+'_95P'] = row[metric].percentile(95);
 							}
 							if (options.stats.includes('99P')) {
-								row[metric+'_99'] = row[metric].percentile(99);
+								row[metric+'_99P'] = row[metric].percentile(99);
 							}
 							if (options.stats.includes('mean')) {
 								row[metric+'_mean'] = row[metric].amean();
+							}
+							if (options.stats.includes('median')) {
+								row[metric+'_median'] = row[metric].median();
 							}
 							if (options.stats.includes('std')) {
 								row[metric+'_std'] = row[metric].stddev();
@@ -86,7 +103,7 @@ function Perf(options) {
 								row[metric+'_max'] = row[metric].range()[1];
 							}
 							if (options.stats.includes('count')) {
-								row[metric+'_count'] = row[metric].length;
+								row[metric+'_count'] = row[metric].length; // Number of non-zero values for this metric
 							}
 							if (options.stats.length) {
 								delete row[metric];
@@ -163,6 +180,32 @@ function Compat() {
 		getStats: () => dataPromise
 	};
 }
+
+const validateOptions = (raw, schema) => {
+	return Object.keys(schema).reduce((out, k) => {
+		if (!(k in raw)) {
+			// Do nothing
+		} else if (schema[k].type === 'array') {
+			let candidate = Array.isArray(raw[k]) ? raw[k] : raw[k].split(',');
+			if ('valid' in schema[k]) candidate = candidate.filter(x => schema[k].valid.includes(x));
+			if (candidate.length) {
+				out[k] = candidate;
+			}
+		} else if (schema[k].type === 'number') {
+			let candidate = Number.parseFloat(raw[k]);
+			if (!Number.isNaN(candidate)) {
+				if ('min' in schema[k]) candidate = Math.max(candidate, schema[k].min);
+				if ('max' in schema[k]) candidate = Math.min(candidate, schema[k].max);
+				out[k] = candidate;
+			}
+		}
+		if (!(k in out) && ('default' in schema[k])) {
+			out[k] = schema[k].default;
+		}
+		return out;
+	}, {});
+};
+
 
 module.exports = {
 	Perf: Perf,
