8ed2b214f61bba1be0d8d3d81e9a7088c01898a4
Jake Champion <jakechampion.jake2@googlemail.com>
2017-01-03 14:10:39 +0000

    Update detect to catch safari 5 bug

--- polyfills/Object/keys/detect.js
+++ polyfills/Object/keys/detect.js
@@ -1,1 +1,4 @@
-'keys' in Object
+'keys' in Object && (function () {
+	// Safari 5.0 bug where Object.keys doesn't work with arguments
+	return (Object.keys(arguments) || '').length === 2;
+}(1, 2))
