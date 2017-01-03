dbc4cb6377e63f7605dbb7d667b0357c336b4353
Jake Champion <jakechampion.jake2@googlemail.com>
2017-01-03 14:10:30 +0000

    Add es6 compliant polyfill for Object.keys

--- polyfills/Object/keys/polyfill.js
+++ polyfills/Object/keys/polyfill.js
@@ -1,8 +1,14 @@
 Object.keys = (function() {
 	'use strict';
-	var hasOwnProperty = Object.prototype.hasOwnProperty,
-	hasDontEnumBug = !({ toString: null }).propertyIsEnumerable('toString'),
-	dontEnums = [
+
+	// modified from https://github.com/es-shims/object-keys
+
+	var has = Object.prototype.hasOwnProperty;
+	var toStr = Object.prototype.toString;
+	var isEnumerable = Object.prototype.propertyIsEnumerable;
+	var hasDontEnumBug = !isEnumerable.call({ toString: null }, 'toString');
+	var hasProtoEnumBug = isEnumerable.call(function () {}, 'prototype');
+	var dontEnums = [
 		'toString',
 		'toLocaleString',
 		'valueOf',
@@ -10,29 +16,102 @@ Object.keys = (function() {
 		'isPrototypeOf',
 		'propertyIsEnumerable',
 		'constructor'
-	],
-	dontEnumsLength = dontEnums.length;
+	];
+	var equalsConstructorPrototype = function (o) {
+		var ctor = o.constructor;
+		return ctor && ctor.prototype === o;
+	};
+	var excludedKeys = {
+		$console: true,
+		$external: true,
+		$frame: true,
+		$frameElement: true,
+		$frames: true,
+		$innerHeight: true,
+		$innerWidth: true,
+		$outerHeight: true,
+		$outerWidth: true,
+		$pageXOffset: true,
+		$pageYOffset: true,
+		$parent: true,
+		$scrollLeft: true,
+		$scrollTop: true,
+		$scrollX: true,
+		$scrollY: true,
+		$self: true,
+		$webkitIndexedDB: true,
+		$webkitStorageInfo: true,
+		$window: true
+	};
+	var hasAutomationEqualityBug = (function () {
+		/* global window */
+		if (typeof window === 'undefined') { return false; }
+		for (var k in window) {
+			try {
+				if (!excludedKeys['$' + k] && has.call(window, k) && window[k] !== null && typeof window[k] === 'object') {
+					try {
+						equalsConstructorPrototype(window[k]);
+					} catch (e) {
+						return true;
+					}
+				}
+			} catch (e) {
+				return true;
+			}
+		}
+		return false;
+	}());
+	var equalsConstructorPrototypeIfNotBuggy = function (o) {
+		/* global window */
+		if (typeof window === 'undefined' || !hasAutomationEqualityBug) {
+			return equalsConstructorPrototype(o);
+		}
+		try {
+			return equalsConstructorPrototype(o);
+		} catch (e) {
+			return false;
+		}
+	};
 
-	return function(obj) {
-		if (typeof obj !== 'object' && (typeof obj !== 'function' || obj === null)) {
-			throw new TypeError('Object.keys called on non-object');
+	return function keys(object) {
+		var isObject = object !== null && typeof object === 'object';
+		var isFunction = toStr.call(object) === '[object Function]';
+		var isArguments = isArguments(object);
+		var isString = isObject && toStr.call(object) === '[object String]';
+		var theKeys = [];
+
+		if (!isObject && !isFunction && !isArguments) {
+			throw new TypeError('Object.keys called on a non-object');
 		}
 
-		var result = [], prop, i;
+		var skipProto = hasProtoEnumBug && isFunction;
+		if (isString && object.length > 0 && !has.call(object, 0)) {
+			for (var i = 0; i < object.length; ++i) {
+				theKeys.push(String(i));
+			}
+		}
 
-		for (prop in obj) {
-			if (hasOwnProperty.call(obj, prop)) {
-				result.push(prop);
+		if (isArguments && object.length > 0) {
+			for (var j = 0; j < object.length; ++j) {
+				theKeys.push(String(j));
+			}
+		} else {
+			for (var name in object) {
+				if (!(skipProto && name === 'prototype') && has.call(object, name)) {
+					theKeys.push(String(name));
+				}
 			}
 		}
 
 		if (hasDontEnumBug) {
-			for (i = 0; i < dontEnumsLength; i++) {
-				if (hasOwnProperty.call(obj, dontEnums[i])) {
-					result.push(dontEnums[i]);
+			var skipConstructor = equalsConstructorPrototypeIfNotBuggy(object);
+
+			for (var k = 0; k < dontEnums.length; ++k) {
+				if (!(skipConstructor && dontEnums[k] === 'constructor') && has.call(object, dontEnums[k])) {
+					theKeys.push(dontEnums[k]);
 				}
 			}
 		}
-		return result;
+		return theKeys;
 	};
 }());
