bc1fe9c3d830ac73a938128572c728d94c14904a
Jake Champion <jakechampion.jake2@googlemail.com>
2017-02-24 16:25:12 +0000

    add backends using vcl

--- tasks/node/deployvcl.js
+++ tasks/node/deployvcl.js
@@ -13,7 +13,7 @@ const serviceIds = {
 
 const options = {
     service: argv.env || 'qa',
-    vclFilePath: argv.vclPath || path.join(__dirname, '../../fastly-config.vcl'),
+    vclFilePath: argv.vclPath || path.join(__dirname, '../../fastly/main.vcl'),
     vclName: argv.vclName || 'default',
     dryRun: argv.dryRun
 };
@@ -26,7 +26,9 @@ if (!fs.statSync(options.vclFilePath).isFile()) {
     throw new Error ("Missing VCL file");
 }
 
-const vclContent = fs.readFileSync(options.vclFilePath, 'UTF-8');
+const backends = options.service === 'prod' ? fs.readFileSync(path.join(__dirname, '../../fastly/backends-prod.vcl', 'UTF-8')) : fs.readFileSync(path.join(__dirname, '../../fastly/backends-qa.vcl'), 'UTF-8');
+
+const vclContent = backends + fs.readFileSync(options.vclFilePath, 'UTF-8');
 
 console.log('Pushing VCL to Fastly');
 
