48febcf26f54d547953c18899df97125602a8b66
colthreepv <mister.gamer@gmail.com>
2017-03-17 18:22:45 +0100

    fetch update script returns exit code 1 in case patch fails to apply

--- polyfills/fetch/update.task.js
+++ polyfills/fetch/update.task.js
@@ -7,4 +7,6 @@ const polyfill = fs.readFileSync('./polyfill.js', 'utf8');
 const patch = fs.readFileSync('./patch.jsdiff', 'utf8');
 
 const patched = diff.applyPatch(polyfill, patch);
+
+if (patched === false) process.exit(1);
 fs.writeFileSync('./polyfill.js', patched);
