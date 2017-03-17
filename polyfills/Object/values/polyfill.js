a1950d2ee3f5555c64c260ac7ccb568e472d627a
JounQin <admin@1stg.me>
2017-03-17 14:30:43 +0800

    use Object.defineProperty to polyfill

--- polyfills/Object/values/polyfill.js
+++ polyfills/Object/values/polyfill.js
@@ -1,5 +1,12 @@
-Object.values = function values(object) {
+var values = function values(object) {
 	return Object.keys(object).map(function (key) {
 		return object[key];
 	});
-};
+}
+
+Object.defineProperty ? Object.defineProperty(Object, 'values', {
+	configurable: true,
+	enumerate: false,
+	value: values,
+	writable: true
+}) : (Object.values = values);
