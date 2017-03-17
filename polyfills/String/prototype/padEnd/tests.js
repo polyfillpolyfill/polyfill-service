3525670c58ff93fd96fc95cb2b651b1a83dfbe22
Regis Kuckaertz <regis.kuckaertz@theguardian.com>
2017-03-17 09:51:56 +0000

    Add more test coverage

--- polyfills/String/prototype/padEnd/tests.js
+++ polyfills/String/prototype/padEnd/tests.js
@@ -10,9 +10,14 @@ it('has correct argument length', function () {
 });
 
 it('works with strings', function () {
+	proclaim.equal('a'.padEnd(-1, 'a'), 'a');
 	proclaim.equal('a'.padEnd(0, 'a'), 'a');
 	proclaim.equal('a'.padEnd(1, 'a'), 'a');
 	proclaim.equal('a'.padEnd(5, 'a'), 'aaaaa');
 	proclaim.equal('a'.padEnd(5, 'bc'), 'abcbc');
-	proclaim.equal('a'.padEnd(5, 'bcdef'), 'abcde');
+	proclaim.equal('a'.padEnd(5, 5), 'a5555');
+	proclaim.equal('a'.padEnd(5, { nil: 0 }), 'a[obj');
+	proclaim.equal('a'.padEnd(5, [0, 1, 2]), 'a0,1,');
+	proclaim.equal('a'.padEnd(10, [0, "hello!", 2]), 'a0,hello,');
+	proclaim.equal('a'.padEnd(10), 'a         ');
 });
--- polyfills/String/prototype/padStart/tests.js
+++ polyfills/String/prototype/padStart/tests.js
@@ -10,9 +10,15 @@ it('has correct argument length', function () {
 });
 
 it('works with strings', function () {
+	proclaim.equal('a'.padStart(-1, 'a'), 'a');
 	proclaim.equal('a'.padStart(0, 'a'), 'a');
 	proclaim.equal('a'.padStart(1, 'a'), 'a');
 	proclaim.equal('a'.padStart(5, 'a'), 'aaaaa');
 	proclaim.equal('a'.padStart(5, 'bc'), 'bcbca');
 	proclaim.equal('a'.padStart(5, 'bcdef'), 'bcdea');
+	proclaim.equal('a'.padStart(5, 5), '5555a');
+	proclaim.equal('a'.padStart(5, { nil: 0 }), '[obja'); // String(x:Object) = [object Object]
+	proclaim.equal('a'.padStart(5, [0, 1, 2]), '0,1,a');  // String(x:Array)  = x.toString()
+	proclaim.equal('a'.padStart(10, [0, "hello!", 2]), '0,hello,a');
+	proclaim.equal('a'.padStart(10), '         a');
 });
