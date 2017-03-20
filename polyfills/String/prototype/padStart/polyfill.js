bd913001343dd5a9719e7ff5107e2bfb8f1a84e9
Jake Champion <me@jakechampion.name>
2017-03-20 14:54:35 +0000

    Update polyfill.js

--- polyfills/String/prototype/padStart/polyfill.js
+++ polyfills/String/prototype/padStart/polyfill.js
@@ -1,10 +1,17 @@
-String.prototype.padStart = function (targetLength, padString) {
-	targetLength = targetLength | 0;
-	if (targetLength <= this.length) return String(this);
-	padString = String(padString || " ");
-	var repeat = Math.ceil((targetLength - this.length) / padString.length);
-	while (repeat--) {
-		padString += padString;
-	}
-	return padString.substr(0, targetLength - this.length) + String(this);
-};
+(function () {
+	Object.defineProperty(String.prototype, 'padStart', {
+		configurable: true,
+		enumerable: false,
+		value: function padStart (targetLength, padString) {
+			targetLength = targetLength | 0;
+			if (targetLength <= this.length) return String(this);
+			padString = String(padString || " ");
+			var repeat = Math.ceil((targetLength - this.length) / padString.length);
+			while (repeat--) {
+				padString += padString;
+			}
+			return padString.substr(0, targetLength - this.length) + String(this);
+		},
+		writable: true
+	})
+}())
