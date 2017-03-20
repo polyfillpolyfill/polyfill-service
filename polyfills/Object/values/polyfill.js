28c212cb490962e2b8cbb2ff800fd65c82f34eb7
JounQin <admin@1stg.me>
2017-03-20 16:13:47 +0800

    enumerate -> enumerable, inline function

--- polyfills/Object/values/polyfill.js
+++ polyfills/Object/values/polyfill.js
@@ -1,14 +1,12 @@
 (function () {
-	function values(object) {
-		return Object.keys(object).map(function (key) {
-			return object[key];
-		});
-	}
-
 	Object.defineProperty(Object, 'values', {
 		configurable: true,
-		enumerate: false,
-		value: values,
+		enumerable: false,
+		value: function (object) {
+			return Object.keys(object).map(function (key) {
+				return object[key];
+			});
+		},
 		writable: true
 	})
 }())
