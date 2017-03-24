48f853b3595104ee268ce941f393aa7f5aaae917
Jake Champion <jakechampion.jake2@googlemail.com>
2017-03-24 16:14:23 +0000

    fix the update script

--- polyfills/fetch/update.task.js
+++ polyfills/fetch/update.task.js
@@ -2,11 +2,13 @@
 
 const fs = require('fs');
 const diff = require('diff');
+const process = require('process');
+const path = require('path');
 
-const polyfill = fs.readFileSync('./polyfill.js', 'utf8');
-const patch = fs.readFileSync('./patch.jsdiff', 'utf8');
+const polyfill = fs.readFileSync(path.join(__dirname, './polyfill.js'), 'utf8');
+const patch = fs.readFileSync(path.join(__dirname, './patch.jsdiff'), 'utf8');
 
 const patched = diff.applyPatch(polyfill, patch);
 
 if (patched === false) process.exit(1);
-fs.writeFileSync('./polyfill.js', patched);
+fs.writeFileSync(path.join(__dirname, './polyfill.js'), patched);
