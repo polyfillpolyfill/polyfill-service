f466c59636946d9032209b1af7e2d661cb3ff622
Jake Champion <jakechampion.jake2@googlemail.com>
2017-03-24 14:26:20 +0000

    Add integration tests

---
+++ test/integration/v1/api.js
@@ -0,0 +1,31 @@
+/* eslint-env mocha */
+
+'use strict';
+
+const itRespondsWithHeader = require('../helpers/it-responds-with-header');
+const itRespondsWithStatus = require('../helpers/it-responds-with-status');
+const setupRequest = require('../helpers/setup-request');
+
+describe('GET /v1/polyfill.js', function() {
+	setupRequest('GET', '/v1/polyfill.js');
+	itRespondsWithStatus(301);
+	itRespondsWithHeader('Location', '/v2/polyfill.js');
+});
+
+describe('GET /v1/polyfill.js?features=default&libVersion=1&gated=true', function() {
+	setupRequest('GET', '/v1/polyfill.js?features=default&libVersion=1&gated=true');
+	itRespondsWithStatus(301);
+	itRespondsWithHeader('Location', '/v2/polyfill.js?features=default');
+});
+
+describe('GET /v1/polyfill.min.js', function() {
+	setupRequest('GET', '/v1/polyfill.min.js');
+	itRespondsWithStatus(301);
+	itRespondsWithHeader('Location', '/v2/polyfill.min.js');
+});
+
+describe('GET /v1/polyfill.min.js?features=default&libVersion=1&gated=true', function() {
+	setupRequest('GET', '/v1/polyfill.min.js?features=default&libVersion=1&gated=true');
+	itRespondsWithStatus(301);
+	itRespondsWithHeader('Location', '/v2/polyfill.min.js?features=default');
+});
