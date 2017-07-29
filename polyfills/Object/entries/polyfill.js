1edf4d9aea675f0b2c2e33a048656fafac4cdf80
caub <cyril.auburtin@gmail.com>
2017-07-29 09:27:51 +0200

    using existing polyfills

--- polyfills/Object/entries/polyfill.js
+++ polyfills/Object/entries/polyfill.js
@@ -1,11 +1,1 @@
-Object.entries = function entries(object) {
-	var keys = Object.keys(object);
-
-	return keys
-		.reduce(function(entries, key) {
-			var entry = typeof key === 'string' && object.propertyIsEnumerable(key) ? [
-				[key, object[key]]
-			] : [];
-			return entries.concat(entry);
-		}, []);
-};
+require('object.entries').shim();
