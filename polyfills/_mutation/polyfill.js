f671b6215ce79ddead4e7e1f5b9dd85b05019472
caub <cyril.auburtin@gmail.com>
2017-08-19 11:38:13 +0200

    .append, .after, .. can be called with no arg

--- polyfills/_mutation/polyfill.js
+++ polyfills/_mutation/polyfill.js
@@ -1,22 +1,14 @@
 // http://dom.spec.whatwg.org/#mutation-method-macro
-function _mutation(nodes) { // eslint-disable-line no-unused-vars
-	if (!nodes.length) {
-		throw new Error('DOM Exception 8');
-	} else if (nodes.length === 1) {
+function _mutation(nodes) {
+	if (nodes.length === 1) {
 		return nodes[0] instanceof Node ? nodes[0] : document.createTextNode(nodes[0]+'');
-	} else {
-		var
-		fragment = document.createDocumentFragment(),
-		length = nodes.length,
-		index = -1,
-		node;
-
-		while (++index < length) {
-			node = nodes[index];
-
-			fragment.appendChild(node instanceof Node ? node :  document.createTextNode(node+''));
-		}
-
-		return fragment;
 	}
+
+	var fragment = document.createDocumentFragment();
+
+	for (var i=0; i<nodes.length; i++) {
+		fragment.appendChild(nodes[i] instanceof Node ? nodes[i] : document.createTextNode(nodes[i]+''));
+	}
+
+	return fragment;
 }
