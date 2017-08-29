ef0659e652290a847db7fe4ea1e4fdcb75b8dfff
Jake Champion <jakechampion.jake2@googlemail.com>
2017-08-29 11:14:39 +0100

    update io polyfill and tests

--- polyfills/IntersectionObserver/detect.js
+++ polyfills/IntersectionObserver/detect.js
@@ -1,1 +1,4 @@
-"IntersectionObserver" in this
+'IntersectionObserver' in self &&
+'IntersectionObserverEntry' in self &&
+'intersectionRatio' in self.IntersectionObserverEntry.prototype &&
+'isIntersecting' in self.IntersectionObserverEntry.prototype
