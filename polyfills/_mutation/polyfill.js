66ea10ecf0501c9f68e331304060800bd5ebda66
Jake Champion <me@jakechampion.name>
2017-08-31 22:38:26 +0100

    Use a safer isDOM check

--- polyfills/_mutation/polyfill.js
+++ polyfills/_mutation/polyfill.js
@@ -1,17 +1,38 @@
-// http://dom.spec.whatwg.org/#mutation-method-macro
-function _mutation(nodes) { // eslint-disable-line no-unused-vars
+var _mutation;
+(function () {
 
-	var _Node = typeof window.Node === 'function' ? window.Node : window.Element; // it's missing DocumentFragment, Text, Comment.. but they probably don't exist too on ie8-
+	// it's missing DocumentFragment, Text, Comment.. but they probably don't exist too on ie8-
+	var _Node = typeof window.Node === 'function' ? window.Node : window.Element;
 
-	if (nodes.length === 1) {
-		return nodes[0] instanceof _Node ? nodes[0] : document.createTextNode(nodes[0]+'');
+	function isDOM(object) {
+		// DOM, Level2
+		if ("HTMLElement" in window) {
+			try {
+				return (object && object instanceof _Node);
+			} catch (err) {
+				// We enter here if the object is not an instanceof _Node, likely object is a string.
+			}
+		}
+		// Older browsers
+		return !!(object && typeof object === "object" && object.nodeType === 1 && object.nodeName);
 	}
 
-	var fragment = document.createDocumentFragment();
+	// http://dom.spec.whatwg.org/#mutation-method-macro
+	function mutation(nodes) { // eslint-disable-line no-unused-vars
 
-	for (var i=0; i<nodes.length; i++) {
-		fragment.appendChild(nodes[i] instanceof _Node ? nodes[i] : document.createTextNode(nodes[i]+''));
+
+		if (nodes.length === 1) {
+			return isDOM(nodes[0]) ? nodes[0] : document.createTextNode(nodes[0] + '');
+		}
+
+		var fragment = document.createDocumentFragment();
+		for (var i = 0; i < nodes.length; i++) {
+			fragment.appendChild(isDOM(nodes[i]) ? nodes[i] : document.createTextNode(nodes[i] + ''));
+
+		}
+
+		return fragment;
 	}
 
-	return fragment;
-}
+	_mutation = mutation;
+}());
