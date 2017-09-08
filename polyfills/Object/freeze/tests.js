aa9e73dca5ce4527b47c7bc5a9df7bb800c1127a
Jake Champion <jakechampion.jake2@googlemail.com>
2017-09-08 16:46:03 +0100

    Test only es6 version of method

--- polyfills/Object/freeze/tests.js
+++ polyfills/Object/freeze/tests.js
@@ -5,28 +5,49 @@ it("Should has a length of 1", function() {
 	proclaim.equal(Object.freeze.length, 1);
 });
 
-it("Should not throw a TypeError if called with undefined", function() {
-	proclaim.doesNotThrow(function() { Object.freeze(undefined); });
+var hasES6ObjectFreeze = true;
+try {
+	Object.freeze(1);
+} catch (e) {
+	hasES6ObjectFreeze = false;
+}
+
+var ifHasES6ObjectFreeze = hasES6ObjectFreeze ? it : xit;
+
+ifHasES6ObjectFreeze("Should not throw a TypeError if called with undefined", function() {
+	var obj = undefined;
+	proclaim.doesNotThrow(function() { Object.freeze(obj); });
+	proclaim.deepEqual(Object.freeze(obj), obj);
 });
 
-it("Should not throw a TypeError if called with null", function () {
-	proclaim.doesNotThrow(function() { Object.freeze(null); });
+ifHasES6ObjectFreeze("Should not throw a TypeError if called with null", function () {
+	var obj = null;
+	proclaim.doesNotThrow(function() { Object.freeze(obj); });
+	proclaim.deepEqual(Object.freeze(obj), obj);
 });
 
-it("Should not throw a TypeError if called with a boolean primitive", function() {
-	proclaim.doesNotThrow(function() { Object.freeze(true); });
+ifHasES6ObjectFreeze("Should not throw a TypeError if called with a boolean primitive", function() {
+	var obj = true;
+	proclaim.doesNotThrow(function() { Object.freeze(obj); });
+	proclaim.deepEqual(Object.freeze(obj), obj);
 });
 
-it("Should not throw a TypeError if called with a number primitive", function() {
-	proclaim.doesNotThrow(function() { Object.freeze(100); });
+ifHasES6ObjectFreeze("Should not throw a TypeError if called with a number primitive", function() {
+	var obj = 100;
+	proclaim.doesNotThrow(function() { Object.freeze(obj); });
+	proclaim.deepEqual(Object.freeze(obj), obj);
 });
 
-it("Should not throw a TypeError if called with an array primitive", function() {
-	proclaim.doesNotThrow(function() { Object.freeze([]); });
+ifHasES6ObjectFreeze("Should not throw a TypeError if called with an array primitive", function() {
+	var obj = [];
+	proclaim.doesNotThrow(function() { Object.freeze(obj); });
+	proclaim.deepEqual(Object.freeze(obj), obj);
 });
 
-it("Should not throw a TypeError if called with a string primitive", function() {
-	proclaim.doesNotThrow(function() { Object.freeze(''); });
+ifHasES6ObjectFreeze("Should not throw a TypeError if called with a string primitive", function() {
+	var obj = '';
+	proclaim.doesNotThrow(function() { Object.freeze(obj); });
+	proclaim.deepEqual(Object.freeze(obj), obj);
 });
 
 it("Should not throw a TypeError if called with a function", function() {
