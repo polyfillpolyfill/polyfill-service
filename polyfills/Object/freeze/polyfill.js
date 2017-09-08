8d1f29dc89c494d0dad50b1faf2b22a59a59a6e2
Jake Champion <me@jakechampion.name>
2017-09-08 17:03:54 +0100

    Add Object.freeze polyfill (#1297)
    * Add Object.freeze polyfill
    
    * Create polyfill for es6 version
    
    * Test only es6 version of method
    
    * remove commented code

---
+++ polyfills/Object/freeze/polyfill.js
@@ -0,0 +1,6 @@
+Object.freeze = function freeze(object) {
+	// This feature cannot be implemented fully as a polyfill.
+	// We choose to silently fail which allows "securable" code
+	// to "gracefully" degrade to working but insecure code.
+	return object;
+};
