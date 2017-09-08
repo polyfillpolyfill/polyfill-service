4824a2b03a50582aedec7f1f1d47d2dda7360cd6
Jake Champion <jakechampion.jake2@googlemail.com>
2017-09-08 16:37:31 +0100

    Create polyfill for es6 version

--- polyfills/Object/freeze/polyfill.js
+++ polyfills/Object/freeze/polyfill.js
@@ -1,7 +1,10 @@
 Object.freeze = function freeze(object) {
-	if (Object(object) !== object) {
-		throw new TypeError('Object.freeze can only be called on Objects.');
-	}
+	// In ES5, if the argument to this method is not an object (a primitive),
+	// then it will cause a TypeError.In ES2015, a non- object argument will
+	// be treated as if it were a frozen ordinary object, and be simply returned.
+	// if (Object(object) !== object) {
+	// 	throw new TypeError('Object.freeze can only be called on Objects.');
+	// }
 	// This feature cannot be implemented fully as a polyfill.
 	// We choose to silently fail which allows "securable" code
 	// to "gracefully" degrade to working but insecure code.
--- polyfills/Object/freeze/tests.js
+++ polyfills/Object/freeze/tests.js
@@ -5,37 +5,37 @@ it("Should has a length of 1", function() {
 	proclaim.equal(Object.freeze.length, 1);
 });
 
-it("Should throw a TypeError if called with undefined", function() {
-	proclaim.throws(function() { Object.freeze(undefined); }, TypeError);
+it("Should not throw a TypeError if called with undefined", function() {
+	proclaim.doesNotThrow(function() { Object.freeze(undefined); });
 });
 
-it("Should throw a TypeError if called with null", function () {
-	proclaim.throws(function() { Object.freeze(null); }, TypeError);
+it("Should not throw a TypeError if called with null", function () {
+	proclaim.doesNotThrow(function() { Object.freeze(null); });
 });
 
-it("Should throw a TypeError if called with a boolean primitive", function() {
-	proclaim.throws(function() { Object.freeze(true); }, TypeError);
+it("Should not throw a TypeError if called with a boolean primitive", function() {
+	proclaim.doesNotThrow(function() { Object.freeze(true); });
 });
 
-it("Should throw a TypeError if called with a number primitive", function() {
-	proclaim.throws(function() { Object.freeze(100); }, TypeError);
+it("Should not throw a TypeError if called with a number primitive", function() {
+	proclaim.doesNotThrow(function() { Object.freeze(100); });
 });
 
-it("Should throw a TypeError if called with an array primitive", function() {
-	proclaim.throws(function() { Object.freeze([]); }, TypeError);
+it("Should not throw a TypeError if called with an array primitive", function() {
+	proclaim.doesNotThrow(function() { Object.freeze([]); });
 });
 
-it("Should throw a TypeError if called with a string primitive", function() {
-	proclaim.throws(function() { Object.freeze(''); }, TypeError);
+it("Should not throw a TypeError if called with a string primitive", function() {
+	proclaim.doesNotThrow(function() { Object.freeze(''); });
 });
 
-it("Should throw a TypeError if called with a function", function() {
-	proclaim.throws(function() {
+it("Should not throw a TypeError if called with a function", function() {
+	proclaim.doesNotThrow(function() {
 		Object.freeze(function(){});
 	});
 });
 
-it("Should return an the object called with", function () {
+it("Should return the object called with", function () {
 	var obj = {};
 	proclaim.deepEqual(Object.freeze(obj), obj);
 });
