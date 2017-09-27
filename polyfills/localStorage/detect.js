af804b8e16bfd79f7e960091982e7db1353378f3
Jake Champion <me@jakechampion.name>
2017-09-27 11:29:18 +0100

    Detect if we are in safari private browser

--- polyfills/localStorage/detect.js
+++ polyfills/localStorage/detect.js
@@ -1,1 +1,10 @@
-'localStorage' in this
+'localStorage' in this && (function() {
+	try {
+		// Needed for Safari private browsing.
+		window.localStorage.setItem('storage_test', 1);
+		window.localStorage.removeItem('storage_test');
+		return true;
+	} catch (e) {
+		return false;
+	}
+}())
