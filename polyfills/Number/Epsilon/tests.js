837fafee9e00df1b8d78f77fc786419b0c4c289c
Jake Champion <me@jakechampion.name>
2017-10-25 23:08:39 +0100

    Update tests.js

--- polyfills/Number/Epsilon/tests.js
+++ polyfills/Number/Epsilon/tests.js
@@ -8,14 +8,3 @@ it('is correct type', function () {
 it('is 2^-52', function () {
 	proclaim.equal(Number.EPSILON, 2.2204460492503130808472633361816e-16);
 });
-
-it('not enumerable', function () {
-	proclaim.equal(Object.prototype.propertyIsEnumerable.call(Number, 'EPSILON'), false);
-});
-
-it('not writable', function () {
-	var NumberEPSILON = Number.EPSILON;
-	Number.EPSILON = 1;
-	proclaim.strictEqual(Number.EPSILON, NumberEPSILON);
-	proclaim.notEqual(Number.EPSILON, 1);
-});
