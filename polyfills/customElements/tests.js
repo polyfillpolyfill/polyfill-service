66b4836f958bfcc911bacc0efa384ba66555b6c8
Bede Overend <bede.overend@gmail.com>
2017-11-03 10:00:51 +1100

    Dont test Shadow DOM and support IE11 in tests

--- polyfills/customElements/tests.js
+++ polyfills/customElements/tests.js
@@ -1,74 +1,41 @@
 /* eslint-env mocha, browser */
 /* global proclaim */
 
-/**
- * Resolve on the next "tick", so that the custom elements can be initialized
- * before the assertions happen
- *
- * This is not necssary when the polyfill is not active, but is necessary with the polyfill
- *
- * @return {Promise} resolves after the current event loop finishes
- */
-function pause() {
-	return new Promise(function(resolve) {
-		setTimeout(resolve, 0);
-	});
-}
+describe('customElements', function() {
+	var XFoo;
 
-describe('defining custom elements', function() {
 	before(function() {
-		customElements.define('x-foo', class extends HTMLElement {
-			static get is() {
-				return 'x-foo';
-			}
-		});
+		XFoo = function XFoo() {
+			return HTMLElement.apply(this, arguments);
+		};
+
+		XFoo.__proto__ = HTMLElement;
+		XFoo.prototype = Object.create(HTMLElement.prototype);
+		XFoo.prototype.constructor = XFoo;
+
+		customElements.define('x-foo', XFoo);
 	});
 
 	it('can define a new custom element', function() {
-		const el = document.createElement('x-foo');
+		var el = document.createElement('x-foo');
 		proclaim.isInstanceOf(el, HTMLElement);
-		proclaim.equal(el.tagName, 'X-FOO');
+		proclaim.isInstanceOf(el, XFoo);
 	});
-});
-
-describe('using custom elements', function() {
-	before(function() {
-		customElements.define('x-foo-b', class extends HTMLElement {
-			constructor() {
-				super();
-
-				const shadow = this.attachShadow({mode: 'open'});
-				shadow.innerHTML = `
-					Hello, world!
-					<slot></slot>
-				`;
-			}
-		});
-	});
-
+	
 	it('can insert a custom element into the DOM', function() {
-		const p = document.createElement('p');
-		p.innerHTML = `
-			<x-foo-b></x-foo-b>
-		`;
+		var p = document.createElement('p');
+		var xFooInstance;
 
-		return pause().then(function() {
-			proclaim.equal(p.textContent.trim(), '');
-			proclaim.equal(p.querySelector('x-foo-b').shadowRoot.textContent.trim(), 'Hello, world!');
-		});
+		p.innerHTML = '<x-foo></x-foo>';
+
+		xFooInstance = p.querySelector('x-foo');
+		proclaim.isInstanceOf(xFooInstance, XFoo);
 	});
 
-	it('can slot content into a custom element', function() {
-		const p = document.createElement('p');
-		p.innerHTML = `
-			<x-foo-b>
-				Some inner text
-			</x-foo-b>
-		`;
-
-		return pause().then(function() {
-			proclaim.equal(p.textContent.trim(), 'Some inner text');
-			proclaim.equal(p.querySelector('x-foo-b').shadowRoot.textContent.trim(), 'Hello, world!');
-		});
+	it('can create a custom element from constructor', function() {
+		var xFooInstance = new XFoo();
+		proclaim.isInstanceOf(xFooInstance, HTMLElement);
+		proclaim.isInstanceOf(xFooInstance, XFoo);
 	});
+	
 });
