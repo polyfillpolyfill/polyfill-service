165b3f7425bf34d63b17f5c222463fdd5f2b9bcf
Jake Champion <me@jakechampion.name>
2017-11-13 14:10:54 +0000

    Add createElement helper

--- polyfills/Element/prototype/getAttributeNames/tests.js
+++ polyfills/Element/prototype/getAttributeNames/tests.js
@@ -1,58 +1,96 @@
 /* eslint-env mocha, browser */
 /* global proclaim */
 
-var element;
-
 function nameOf(fn) {
     return Function.prototype.toString.call(fn).match(/function\s*([^\s]*)\(/)[1];
 }
 
-beforeEach(function () {
-    element = document.createElement('div');
-});
+function createElement(type, attributes) {
+	var element = null;
+
+	try {
+		var attributesString = '';
+		for (var attribute in attributes) {
+			if (Object.prototype.hasOwnProperty.call(attributes, attribute)) {
+				attributesString += ' ' + attribute + '="' + attributes[attribute] + '"';
+			}
+		}
+		element = document.createElement( "<" + type + attributesString + ">" );
+	} catch( e ) {};
+
+	if ( !element || (attributes && attributes.name && element.name.toLowerCase() !== attributes.name.toLowerCase())) {
+		element = document.createElement(type);
+		for (var attribute in attributes) {
+			if (Object.prototype.hasOwnProperty.call(attributes, attribute)) {
+				if (attribute === 'className') {
+					element.setAttribute('class', attributes[attribute]);
+				} else if (attribute === 'htmlFor') {
+					element.setAttribute('for', attributes[attribute]);
+				} else {
+					element.setAttribute(attribute, attributes[attribute]);
+				}
+			}
+		}
+	};
+
+	return element;
+ }
 
 it('has correct instance', function () {
+	var element = createElement('div');
     proclaim.isInstanceOf(element.getAttributeNames, Function);
 });
 
 it('has correct name', function () {
+	var element = createElement('div');
     proclaim.equal(nameOf(element.getAttributeNames), 'getAttributeNames');
 });
 
 it('has correct argument length', function () {
+	var element = createElement('div');
     proclaim.equal(element.getAttributeNames.length, 0);
 });
 
-it("should return an array", function() {
-    element.setAttribute("name", "foo");
+it("should return an array", function () {
+	var element = createElement('div', {name: 'foo'});
     var attributeNames = element.getAttributeNames();
     proclaim.isArray(attributeNames);
 });
 
-it("should return an empty array when no attributes", function() {
+it("should return an empty array when no attributes", function () {
+	var element = createElement('div');
     var attributeNames = element.getAttributeNames();
     proclaim.equal(attributeNames.length, 0);
 });
 
-it("should return the list of all attribute names", function() {
-    element.setAttribute("name", "foo");
-    element.setAttribute("class", "is-big");
+it("should return the list of all attribute names", function () {
+	var element = createElement('div', {
+		name: 'foo',
+		className: 'is-big'
+	});
     var attributeNames = element.getAttributeNames();
     proclaim.equal(attributeNames.length, 2);
     proclaim.include(attributeNames, "name");
-    proclaim.include(attributeNames, "class");
+	try {
+		proclaim.include(attributeNames, "class");
+	} catch (e) {
+		// IE 7 doesn't allow setAttribute to work with `class`, have to use property assignment use the name `className`
+		proclaim.include(attributeNames, "className");
+	}
 });
 
-it("should return the list of all attribute names including id", function() {
-    element.id = "container";
+it("should return the list of all attribute names including id", function () {
+	var element = createElement('div', {id:'container'});
     var attributeNames = element.getAttributeNames();
     proclaim.equal(attributeNames.length, 1);
     proclaim.include(attributeNames, "id");
 });
 
-it("should return the list of all attribute names including data attributes", function() {
-    element.setAttribute("data-drop", true);
-    element.setAttribute("data-drag", false);
+it("should return the list of all attribute names including data attributes", function () {
+	var element = createElement('div', {
+		'data-drop': true,
+		'data-drag': false
+	});
     var attributeNames = element.getAttributeNames();
     proclaim.equal(attributeNames.length, 2);
     proclaim.include(attributeNames, "data-drop");
