8af9e34c1ae3574737726e57117fc73be089d002
Jake Champion <jakechampion.jake2@googlemail.com>
2017-11-15 20:20:00 +0000

    Ensure classList exists on Element.prototype

--- polyfills/Element/prototype/classList/detect.js
+++ polyfills/Element/prototype/classList/detect.js
@@ -1,4 +1,4 @@
-'document' in this && "classList" in document.documentElement && (function () {
+'document' in this && "classList" in document.documentElement && 'Element' in this && 'classList' in Element.prototype && (function () {
 	var e = document.createElement('span');
 	e.classList.add('a', 'b');
 	return e.classList.contains('b');
