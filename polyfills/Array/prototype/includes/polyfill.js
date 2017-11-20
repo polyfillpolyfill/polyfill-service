bf194e4616038ff24a8e08f4fb2def6d971c0845
Jake Champion <jakechampion.jake2@googlemail.com>
2017-11-20 22:23:03 +0000

    Add back function name

--- polyfills/Array/prototype/includes/polyfill.js
+++ polyfills/Array/prototype/includes/polyfill.js
@@ -1,32 +1,34 @@
-Object.defineProperty(Array.prototype, 'includes', {
-	configurable: true,
-	value: function (searchElement /*, fromIndex*/ ) {
-		'use strict';
-		var O = Object(this);
-		var len = parseInt(O.length) || 0;
-		if (len === 0) {
+(function () {
+	Object.defineProperty(Array.prototype, 'includes', {
+		configurable: true,
+		value: function includes (searchElement /*, fromIndex*/) {
+			'use strict';
+			var O = Object(this);
+			var len = parseInt(O.length) || 0;
+			if (len === 0) {
+				return false;
+			}
+			var n = parseInt(arguments[1]) || 0;
+			var k;
+			if (n >= 0) {
+				k = n;
+			} else {
+				k = len + n;
+				if (k < 0) {
+					k = 0;
+				}
+			}
+			var currentElement;
+			while (k < len) {
+				currentElement = O[k];
+				if (searchElement === currentElement ||
+					(searchElement !== searchElement && currentElement !== currentElement)) {
+					return true;
+				}
+				k++;
+			}
 			return false;
-		}
-		var n = parseInt(arguments[1]) || 0;
-		var k;
-		if (n >= 0) {
-			k = n;
-		} else {
-			k = len + n;
-			if (k < 0) {
-				k = 0;
-			}
-		}
-		var currentElement;
-		while (k < len) {
-			currentElement = O[k];
-			if (searchElement === currentElement ||
-				(searchElement !== searchElement && currentElement !== currentElement)) {
-				return true;
-			}
-			k++;
-		}
-		return false;
-	},
-	writable: true
-});
+		},
+		writable: true
+	});
+}());
