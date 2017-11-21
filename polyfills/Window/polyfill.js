4b9e49fdaa57e46fd63597e1cee2bc16608e1da4
Jake Champion <me@jakechampion.name>
2017-11-21 13:58:36 +0000

    dont polyfill if inside a web worker, fixes #1292 (#1394)

--- polyfills/Window/polyfill.js
+++ polyfills/Window/polyfill.js
@@ -1,7 +1,9 @@
-(function(global) {
-	if (global.constructor) {
-		global.Window = global.constructor;
-	} else {
-		(global.Window = global.constructor = new Function('return function Window() {}')()).prototype = this;
-	}
-}(this));
+if ((typeof WorkerGlobalScope === "undefined") && (typeof importScripts !== "function")) {
+	(function (global) {
+		if (global.constructor) {
+			global.Window = global.constructor;
+		} else {
+			(global.Window = global.constructor = new Function('return function Window() {}')()).prototype = this;
+		}
+	}(this));
+}
