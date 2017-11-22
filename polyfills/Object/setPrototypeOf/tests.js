947ab22f9106bcd4f7ffd51120ae8352251f5e04
Shaun Stanworth <shaun@freeformers.com>
2017-11-22 10:47:45 +0000

    Complex test case for setPrototypeOf

--- polyfills/Object/setPrototypeOf/tests.js
+++ polyfills/Object/setPrototypeOf/tests.js
@@ -1,7 +1,7 @@
 /* eslint-env mocha, browser */
 /* global proclaim */
 
-it('changes prototype to regular objects', function() {
+it('changes prototype to null objects', function() {
 	var obj = {a: 123};
 	proclaim.equal(obj instanceof Object, true);
 	// sham requires assignment to work cross browser
@@ -10,7 +10,7 @@ it('changes prototype to regular objects', function() {
 	proclaim.equal(obj.a, 123);
 });
 
-it('changes prototype to null objects', function() {
+it('changes prototype to regular simple objects', function() {
 	var obj = Object.create(null);
 	obj.a = 456;
 	proclaim.equal(obj instanceof Object, false);
@@ -19,4 +19,12 @@ it('changes prototype to null objects', function() {
 	obj = Object.setPrototypeOf(obj, {});
 	proclaim.equal(obj instanceof Object, true);
 	proclaim.equal(obj.a, 456);
+});
+
+it('changes prototype to regular complex objects', function() {
+	var Child = function () {};
+	var Parent = {a: 123};
+	var child = new Child();
+	Object.setPrototypeOf(child, Parent);
+	proclaim.equal(obj.a, 123);
 });
