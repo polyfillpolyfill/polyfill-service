276e25fbde12f87efa24cf9aa5b44fdd01bc052b
Jake Champion <jakechampion.jake2@googlemail.com>
2018-01-03 16:50:19 +0000

    Rewrite Array.prototype.find to be spec compliant and use es abstract methods

--- polyfills/Array/prototype/find/polyfill.js
+++ polyfills/Array/prototype/find/polyfill.js
@@ -1,31 +1,32 @@
-Object.defineProperty(Array.prototype, 'find', {
-	configurable: true,
-	value: function find(callback) {
-		if (this === undefined || this === null) {
-			throw new TypeError(this + ' is not an object');
+// 22.1.3.8 Array.prototype.find ( predicate [ , thisArg ] )
+CreateMethodProperty(Array.prototype, 'find', function find( predicate /* [ , thisArg ] */) {
+	// 1. Let O be ? ToObject(this value).
+	var O = ToObject(this);
+	// 2. Let len be ? ToLength(? Get(O, "length")).
+	var len = ToLength(Get(O, "length"));
+	// 3. If IsCallable(predicate) is false, throw a TypeError exception.
+	if (IsCallable(predicate) === false) {
+		throw new TypeError(predicate + ' is not a function');
+	}
+	// 4. If thisArg is present, let T be thisArg; else let T be undefined.
+	var T = arguments.length > 1 ? arguments[1] : undefined;
+	// 5. Let k be 0.
+	var k = 0;
+	// 6. Repeat, while k < len
+	while (k < len) {
+		// a. Let Pk be ! ToString(k).
+		var Pk = ToString(k);
+		// b. Let kValue be ? Get(O, Pk).
+		var kValue = Get(O, Pk);
+		// c. Let testResult be ToBoolean(? Call(predicate, T, « kValue, k, O »)).
+		var testResult = ToBoolean(Call(predicate, T, [kValue, k, O ]));
+		// d. If testResult is true, return kValue.
+		if (testResult) {
+			return kValue;
 		}
-
-		if (typeof callback !== 'function') {
-			throw new TypeError(callback + ' is not a function');
-		}
-
-		var
-		object = Object(this),
-		scope = arguments[1],
-		arraylike = object instanceof String ? object.split('') : object,
-		length = Math.max(Math.min(arraylike.length, 9007199254740991), 0) || 0,
-		index = -1,
-		element;
-
-		while (++index < length) {
-			if (index in arraylike) {
-				element = arraylike[index];
-
-				if (callback.call(scope, element, index, object)) {
-					return element;
-				}
-			}
-		}
-	},
-	writable: true
+		// e. Increase k by 1.
+		var k = k + 1;
+	}
+	// 7. Return undefined.
+	return undefined;
 });
