771b9fe87dd62e1a2954244145f9c3879ea5a871
Jake Champion <jakechampion.jake2@googlemail.com>
2018-01-03 17:01:09 +0000

    Rewrite Math.sign to be spec compliant and use es abstract methods

--- polyfills/Math/sign/polyfill.js
+++ polyfills/Math/sign/polyfill.js
@@ -1,3 +1,24 @@
-Math.sign = function sign(x) {
-	return !(x = Number(x)) ? x : x > 0 ? 1 : -1;
-};
+// 20.2.2.29. Math.sign ( x )
+CreateMethodProperty(Math, 'sign', function sign(x) {
+	var x = Number(x);
+	// If x is NaN, the result is NaN.
+	if (isNaN(x)) {
+		return NaN;
+	}
+	// If x is -0, the result is -0.
+	if (1 / x === -Infinity) {
+		return -0;
+	}
+	// If x is +0, the result is +0.
+	if (1 / x === Infinity) {
+		return 0;
+	}
+	// If x is negative and not -0, the result is -1.
+	if (x < 0) {
+		return -1;
+	}
+	// If x is positive and not +0, the result is +1.
+	if (x > 0) {
+		return 1;
+	}
+});
