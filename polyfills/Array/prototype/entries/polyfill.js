59954f62fc89e0aa52e2e162bad6243a2859c71b
Jake Champion <jakechampion.jake2@googlemail.com>
2018-01-03 16:38:15 +0000

    Rewrite Array.prototype.entries to be spec compliant and use es abstract methods

--- polyfills/Array/prototype/entries/polyfill.js
+++ polyfills/Array/prototype/entries/polyfill.js
@@ -1,5 +1,8 @@
-Object.defineProperty(Array.prototype, 'entries', {
-	value: function () {
-		return new ArrayIterator(this, 'key+value');
-	}
+// 22.1.3.4. Array.prototype.entries ( )
+CreateMethodProperty(Array.prototype, 'entries', function () {
+	// 1. Let O be ? ToObject(this value).
+	var O = ToObject(this);
+	// 2. Return CreateArrayIterator(O, "key+value").
+	// TODO: Add CreateArrayIterator
+	return new ArrayIterator(O, 'key+value');
 });
