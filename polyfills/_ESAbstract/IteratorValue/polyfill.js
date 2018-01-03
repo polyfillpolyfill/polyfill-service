8ce545fa6c9ad32ebf260310403b25608c507876
Jake Champion <jakechampion.jake2@googlemail.com>
2018-01-03 16:33:26 +0000

    Add all the ES Abstract methods as private features

---
+++ polyfills/_ESAbstract/IteratorValue/polyfill.js
@@ -0,0 +1,9 @@
+// 7.4.4 IteratorValue ( iterResult )
+function IteratorValue(iterResult) { // eslint-disable-line no-unused-vars
+	// Assert: Type(iterResult) is Object.
+	if (Type(iterResult) !== 'object') {
+		throw new Error(Object.prototype.toString.call(iterResult) + 'is not an Object.');
+	}
+	// Return ? Get(iterResult, "value").
+	return Get(iterResult, "value");
+}
