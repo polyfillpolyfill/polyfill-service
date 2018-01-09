f700d7a4b7315f9ca91a1052100edb2cc3f55c15
Jake Champion <me@jakechampion.name>
2018-01-09 14:31:16 +0000

    Add Get abstract operation (#1452)

---
+++ polyfills/_ESAbstract/Get/polyfill.js
@@ -0,0 +1,7 @@
+// 7.3.1. Get ( O, P )
+function Get(O, P) { // eslint-disable-line no-unused-vars
+	// 1. Assert: Type(O) is Object.
+	// 2. Assert: IsPropertyKey(P) is true.
+	// 3. Return ? O.[[Get]](P, O).
+	return O[P];
+}
