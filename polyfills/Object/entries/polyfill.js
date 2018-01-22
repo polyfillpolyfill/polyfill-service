d3b7147da927e362c3e5a07b036a3e5e0a6cb1c3
Jake Champion <me@jakechampion.name>
2018-01-22 09:41:51 +0000

    Rewrite Object.entries to be spec compliant and use es abstract methods (#1564)

--- polyfills/Object/entries/polyfill.js
+++ polyfills/Object/entries/polyfill.js
@@ -1,11 +1,10 @@
-Object.entries = function entries(object) {
-	var keys = Object.keys(object);
-
-	return keys
-		.reduce(function(entries, key) {
-			var entry = typeof key === 'string' && object.propertyIsEnumerable(key) ? [
-				[key, object[key]]
-			] : [];
-			return entries.concat(entry);
-		}, []);
-};
+// 19.1.2.5. Object.entries ( O )
+CreateMethodProperty(Object, 'entries', function entries(O) {
+	// 1. Let obj be ? ToObject(O).
+	var obj = ToObject(O);
+	// 2. Let nameList be ? EnumerableOwnProperties(obj, "key+value").
+	var nameList = EnumerableOwnProperties(obj, "key+value");
+	// 3. Return CreateArrayFromList(nameList).
+	// Polyfill.io - nameList is already an array.
+	return nameList;
+});
