8d37ef1c73579f6ac021f408624f5cfa32804f15
Jake Champion <me@jakechampion.name>
2018-01-22 12:49:04 +0000

    Rewrite Array.from to be spec compliant and use es abstract methods (#1510)
    * Rewrite Array.from to be spec compliant and use es abstract methods
    
    * Update code comment.
    
    * Update code comment.

--- polyfills/Array/from/polyfill.js
+++ polyfills/Array/from/polyfill.js
@@ -1,301 +1,152 @@
-// Wrapped in IIFE to prevent leaking to global scope.
+/* globals
+	IsCallable, GetMethod, Symbol, IsConstructor, Construct, ArrayCreate, GetIterator, IteratorClose,
+	ToString, IteratorStep, IteratorValue, Call, CreateDataPropertyOrThrow, ToObject, ToLength, Get, CreateMethodProperty
+*/
 (function () {
-	'use strict';
-
-	function toInteger(value) {
-		var number = Number(value);
-		return sign(number) * Math.floor(Math.abs(Math.min(Math.max(number || 0, 0), 9007199254740991)));
-	}
-
-	var has = Object.prototype.hasOwnProperty;
-	var strValue = String.prototype.valueOf;
-
-	var tryStringObject = function tryStringObject(value) {
-		try {
-			strValue.call(value);
-			return true;
-		} catch (e) {
-			return false;
-		}
-	};
-
-	function sign(number) {
-		return number >= 0 ? 1 : -1;
-	}
-
-	var toStr = Object.prototype.toString;
-	var strClass = '[object String]';
-	var hasSymbols = typeof Symbol === 'function';
-	var hasToStringTag = hasSymbols && 'toStringTag' in Symbol;
-
+	var toString = Object.prototype.toString;
+	var stringMatch = String.prototype.match;
+	// A cross-realm friendly way to detect if a value is a String object or literal.
 	function isString(value) {
-		if (typeof value === 'string') {
-			return true;
-		}
-		if (typeof value !== 'object') {
-			return false;
-		}
-		return hasToStringTag ? tryStringObject(value) : toStr.call(value) === strClass;
+		if (typeof value === 'string') { return true; }
+		if (typeof value !== 'object') { return false; }
+		return toString.call(value) === '[object String]';
 	}
 
-	var fnToStr = Function.prototype.toString;
-
-	var constructorRegex = /^\s*class /;
-	var isES6ClassFn = function isES6ClassFn(value) {
-		try {
-			var fnStr = fnToStr.call(value);
-			var singleStripped = fnStr.replace(/\/\/.*\n/g, '');
-			var multiStripped = singleStripped.replace(/\/\*[.\s\S]*\*\//g, '');
-			var spaceStripped = multiStripped.replace(/\n/mg, ' ').replace(/ {2}/g, ' ');
-			return constructorRegex.test(spaceStripped);
-		} catch (e) {
-			return false; // not a function
-		}
-	};
-
-	var tryFunctionObject = function tryFunctionObject(value) {
-		try {
-			if (isES6ClassFn(value)) {
-				return false;
+	// 22.1.2.1. Array.from ( items [ , mapfn [ , thisArg ] ] )
+	CreateMethodProperty(Array, 'from', function from(items /* [ , mapfn [ , thisArg ] ] */) { // eslint-disable-line no-undef
+		// 1. Let C be the this value.
+		var C = this;
+		// 2. If mapfn is undefined, let mapping be false.
+		var mapfn = arguments.length > 1 ? arguments[1] : undefined;
+		if (mapfn === undefined) {
+			var mapping = false;
+			// 3. Else,
+		} else {
+			// a. If IsCallable(mapfn) is false, throw a TypeError exception.
+			if (IsCallable(mapfn) === false) {
+				throw new TypeError(Object.prototype.toString.call(mapfn) + ' is not a function.');
 			}
-			fnToStr.call(value);
-			return true;
-		} catch (e) {
-			return false;
-		}
-	};
-	var fnClass = '[object Function]';
-	var genClass = '[object GeneratorFunction]';
-
-	function isCallable(value) {
-		if (!value) {
-			return false;
-		}
-		if (typeof value !== 'function' && typeof value !== 'object') {
-			return false;
-		}
-		if (hasToStringTag) {
-			return tryFunctionObject(value);
-		}
-		if (isES6ClassFn(value)) {
-			return false;
-		}
-		var strClass = toStr.call(value);
-		return strClass === fnClass || strClass === genClass;
-	};
-	var isArray = Array.isArray;
-
-	var parseIterable = function (iterator) {
-		var done = false;
-		var iterableResponse;
-		var tempArray = [];
-
-		if (iterator && typeof iterator.next === 'function') {
-			while (!done) {
-				iterableResponse = iterator.next();
-				if (
-					has.call(iterableResponse, 'value') &&
-					has.call(iterableResponse, 'done')
-				) {
-					if (iterableResponse.done === true) {
-						done = true;
-						break; // eslint-disable-line no-restricted-syntax
-
-					} else if (iterableResponse.done !== false) {
-						break; // eslint-disable-line no-restricted-syntax
-					}
-
-					tempArray.push(iterableResponse.value);
-				} else if (iterableResponse.done === true) {
-					done = true;
-					break; // eslint-disable-line no-restricted-syntax
-				} else {
-					break; // eslint-disable-line no-restricted-syntax
-				}
+			// b. If thisArg is present, let T be thisArg; else let T be undefined.
+			var thisArg = arguments.length > 2 ? arguments[2] : undefined;
+			if (thisArg !== undefined) {
+				var T = thisArg;
+			} else {
+				T = undefined;
 			}
+			// c. Let mapping be true.
+			mapping = true;
+
 		}
-
-		return done ? tempArray : false;
-	};
-
-	var iteratorSymbol;
-	var forOf;
-	var hasSet = typeof Set === 'function';
-	var hasMap = typeof Map === 'function';
-
-	if (hasSymbols) {
-		iteratorSymbol = Symbol.iterator;
-	} else {
-		var iterate;
-		try {
-			iterate = Function('iterable', 'var arr = []; for (var value of iterable) arr.push(value); return arr;'); // eslint-disable-line no-new-func
-		} catch (e) {}
-		var supportsStrIterator = (function () {
-			try {
-				var supported = false;
-				var obj = { // eslint-disable-line no-unused-vars
-					'@@iterator': function () {
-						return {
-							'next': function () {
-								supported = true;
-								return {
-									'done': true,
-									'value': undefined
-								};
-							}
-						};
-					}
-				};
-
-				iterate(obj);
-				return supported;
-			} catch (e) {
-				return false;
+		// 4. Let usingIterator be ? GetMethod(items, @@iterator).
+		var usingIterator = GetMethod(items, Symbol.iterator);
+		// 5. If usingIterator is not undefined, then
+		if (usingIterator !== undefined) {
+			// a. If IsConstructor(C) is true, then
+			if (IsConstructor(C)) {
+				// i. Let A be ? Construct(C).
+				var A = Construct(C);
+				// b. Else,
+			} else {
+				// i. Let A be ! ArrayCreate(0).
+				A = ArrayCreate(0);
 			}
-		}());
-
-		if (supportsStrIterator) {
-			iteratorSymbol = '@@iterator';
-		} else if (typeof Set === 'function') {
-			var s = new Set();
-			s.add(0);
-			try {
-				if (iterate(s).length === 1) {
-					forOf = iterate;
-				}
-			} catch (e) {}
-		}
-	}
-
-	var isSet;
-	if (hasSet) {
-		var setSize = Object.getOwnPropertyDescriptor(Set.prototype, 'size').get;
-		isSet = function (set) {
-			try {
-				setSize.call(set);
-				return true;
-			} catch (e) {
-				return false;
-			}
-		};
-	}
-
-	var isMap;
-	if (hasMap) {
-		var mapSize = Object.getOwnPropertyDescriptor(Map.prototype, 'size').get;
-		isMap = function (m) {
-			try {
-				mapSize.call(m);
-				return true;
-			} catch (e) {
-				return false;
-			}
-		};
-	}
-
-	var setForEach = hasSet && Set.prototype.forEach;
-	var mapForEach = hasMap && Map.prototype.forEach;
-	var usingIterator = function (items) {
-		var tempArray = [];
-		if (has.call(items, iteratorSymbol)) {
-			return items[iteratorSymbol]();
-		} else if (setForEach && isSet(items)) {
-			setForEach.call(items, function (val) {
-				tempArray.push(val);
-			});
-			return {
-				next: function () {
-					return tempArray.length === 0
-						? {
-							done: true
-						}
-						: {
-							value: tempArray.splice(0, 1)[0],
-							done: false
-						};
-				}
-			};
-		} else if (mapForEach && isMap(items)) {
-			mapForEach.call(items, function (val, key) {
-				tempArray.push([key, val]);
-			});
-			return {
-				next: function () {
-					return tempArray.length === 0
-						? {
-							done: true
-						}
-						: {
-							value: tempArray.splice(0, 1)[0],
-							done: false
-						};
-				}
-			};
-		}
-		return items;
-	};
-
-	var strMatch = String.prototype.match;
-
-	var parseIterableLike = function (items) {
-		var arr = parseIterable(usingIterator(items));
-
-		if (!arr) {
-			if (isString(items)) {
-				arr = strMatch.call(items, /[\uD800-\uDBFF][\uDC00-\uDFFF]?|[^\uD800-\uDFFF]|./g) || [];
-			} else if (forOf && !isArray(items)) {
-				// Safari 8's native Map or Set can't be iterated except with for..of
-				try {
-					arr = forOf(items);
-				} catch (e) {}
-			}
-		}
-		return arr || items;
-	};
-
-	/*! https://mths.be/array-from v0.2.0 by @mathias */
-	Object.defineProperty(Array, 'from', {
-		configurable: true,
-		value: function from(items) {
-			var C = this;
-			if (items === null || typeof items === 'undefined') {
-				throw new TypeError('`Array.from` requires an array-like object, not `null` or `undefined`');
-			}
-			var mapFn, T;
-			if (typeof arguments[1] !== 'undefined') {
-				mapFn = arguments[1];
-				if (!isCallable(mapFn)) {
-					throw new TypeError('When provided, the second argument to `Array.from` must be a function');
-				}
-				if (arguments.length > 2) {
-					T = arguments[2];
-				}
-			}
-
-			var arrayLike = Object(parseIterableLike(items));
-			var len = toInteger(arrayLike.length);
-			var A = isCallable(C) ? Object(new C(len)) : new Array(len);
+			// c. Let iteratorRecord be ? GetIterator(items, usingIterator).
+			var iteratorRecord = GetIterator(items, usingIterator);
+			// d. Let k be 0.
 			var k = 0;
-			var kValue, mappedValue;
-
-			while (k < len) {
-				kValue = arrayLike[k];
-				if (mapFn) {
-					mappedValue = typeof T === 'undefined' ? mapFn(kValue, k) : mapFn.apply(T, [kValue, k]);
-				} else {
-					mappedValue = kValue;
+			// e. Repeat,
+			while (true) {
+				// i. If k ≥ 2^53-1, then
+				if (k >= (Math.pow(2, 53) - 1)) {
+					// 1. Let error be Completion{[[Type]]: throw, [[Value]]: a newly created TypeError object, [[Target]]: empty}.
+					var error = new TypeError('Iteration count can not be greater than or equal 9007199254740991.');
+					// 2. Return ? IteratorClose(iteratorRecord, error).
+					return IteratorClose(iteratorRecord, error);
 				}
-				Object.defineProperty(A, k, {
-					'configurable': true,
-					'enumerable': true,
-					'value': mappedValue,
-					'writable': true
-				});
-				k += 1;
+				// ii. Let Pk be ! ToString(k).
+				var Pk = ToString(k);
+				// iii. Let next be ? IteratorStep(iteratorRecord).
+				var next = IteratorStep(iteratorRecord);
+				// iv. If next is false, then
+				if (next === false) {
+					// 1. Perform ? Set(A, "length", k, true).
+					A["length"] = k;
+					// 2. Return A.
+					return A;
+				}
+				// v. Let nextValue be ? IteratorValue(next).
+				var nextValue = IteratorValue(next);
+				// vi. If mapping is true, then
+				if (mapping) {
+					try {
+						// Polyfill.io - The try catch accounts for step 2.
+						// 1. Let mappedValue be Call(mapfn, T, « nextValue, k »).
+						var mappedValue = Call(mapfn, T, [nextValue, k]);
+						// 2. If mappedValue is an abrupt completion, return ? IteratorClose(iteratorRecord, mappedValue).
+						// 3. Let mappedValue be mappedValue.[[Value]].
+					} catch (e) {
+						return IteratorClose(iteratorRecord, e);
+					}
+
+					// vii. Else, let mappedValue be nextValue.
+				} else {
+					mappedValue = nextValue;
+				}
+				try {
+					// Polyfill.io - The try catch accounts for step ix.
+					// viii. Let defineStatus be CreateDataPropertyOrThrow(A, Pk, mappedValue).
+					CreateDataPropertyOrThrow(A, Pk, mappedValue);
+					// ix. If defineStatus is an abrupt completion, return ? IteratorClose(iteratorRecord, defineStatus).
+				} catch (e) {
+					return IteratorClose(iteratorRecord, e);
+				}
+				// x. Increase k by 1.
+				k = k + 1;
 			}
-			A.length = len;
-			return A;
-		},
-		writable: true
+		}
+		// 6. NOTE: items is not an Iterable so assume it is an array-like object.
+		// 7. Let arrayLike be ! ToObject(items).
+		// Polyfill.io - For Strings we need to split astral symbols into surrogate pairs.
+		if (isString(items)) {
+			var arrayLike = stringMatch.call(items, /[\uD800-\uDBFF][\uDC00-\uDFFF]?|[^\uD800-\uDFFF]|./g) || [];
+		} else {
+			arrayLike = ToObject(items);
+		}
+		// 8. Let len be ? ToLength(? Get(arrayLike, "length")).
+		var len = ToLength(Get(arrayLike, "length"));
+		// 9. If IsConstructor(C) is true, then
+		if (IsConstructor(C)) {
+			// a. Let A be ? Construct(C, « len »).
+			A = Construct(C, [len]);
+			// 10. Else,
+		} else {
+			// a. Let A be ? ArrayCreate(len).
+			A = ArrayCreate(len);
+		}
+		// 11. Let k be 0.
+		k = 0;
+		// 12. Repeat, while k < len
+		while (k < len) {
+			// a. Let Pk be ! ToString(k).
+			Pk = ToString(k);
+			// b. Let kValue be ? Get(arrayLike, Pk).
+			var kValue = Get(arrayLike, Pk);
+			// c. If mapping is true, then
+			if (mapping === true) {
+				// i. Let mappedValue be ? Call(mapfn, T, « kValue, k »).
+				mappedValue = Call(mapfn, T, [kValue, k]);
+				// d. Else, let mappedValue be kValue.
+			} else {
+				mappedValue = kValue;
+			}
+			// e. Perform ? CreateDataPropertyOrThrow(A, Pk, mappedValue).
+			CreateDataPropertyOrThrow(A, Pk, mappedValue);
+			// f. Increase k by 1.
+			k = k + 1;
+		}
+		// 13. Perform ? Set(A, "length", len, true).
+		A["length"] = len;
+		// 14. Return A.
+		return A;
 	});
 }());
