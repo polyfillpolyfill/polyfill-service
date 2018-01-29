eaba12b73d2cbb31d5e03429cef7eb6c0a4cb165
Jake Champion <me@jakechampion.name>
2018-01-29 23:20:51 +0000

    Rewrite Array.prototype.forEach to be spec compliant and use es abstract methods (#1523)
    * Rewrite Array.prototype.forEach to be spec compliant and use es abstract methods
    
    * Keep filtering the unused features until no more features are removed

--- polyfills/Array/prototype/forEach/polyfill.js
+++ polyfills/Array/prototype/forEach/polyfill.js
@@ -1,22 +1,37 @@
-Array.prototype.forEach = function forEach(callback) {
-	if (this === undefined || this === null) {
-		throw new TypeError(this + ' is not an object');
+// 22.1.3.10. Array.prototype.forEach ( callbackfn [ , thisArg ] )
+
+CreateMethodProperty(Array.prototype, 'forEach', function forEach(callbackfn /* [ , thisArg ] */) {
+	// 1. Let O be ? ToObject(this value).
+	var O = ToObject(this);
+	// Polyfill.io - If O is a String object, split it into an array in order to iterate correctly.
+	// We will use arrayLike in place of O when we are iterating through the list.
+	var arraylike = O instanceof String ? O.split('') : O;
+	// 2. Let len be ? ToLength(? Get(O, "length")).
+	var len = ToLength(Get(O, "length"));
+	// 3. If IsCallable(callbackfn) is false, throw a TypeError exception.
+	if (IsCallable(callbackfn) === false) {
+		throw new TypeError(callbackfn + ' is not a function');
 	}
-
-	if (typeof callback !== 'function') {
-		throw new TypeError(callback + ' is not a function');
-	}
-
-	var
-	object = Object(this),
-	scope = arguments[1],
-	arraylike = object instanceof String ? object.split('') : object,
-	length = Math.max(Math.min(arraylike.length, 9007199254740991), 0) || 0,
-	index = -1;
-
-	while (++index < length) {
-		if (index in arraylike) {
-			callback.call(scope, arraylike[index], index, object);
+	// 4. If thisArg is present, let T be thisArg; else let T be undefined.
+	var T = arguments.length > 1 ? arguments[1] : undefined;
+	// 5. Let k be 0.
+	var k = 0;
+	// 6. Repeat, while k < len
+	while (k < len) {
+		// a. Let Pk be ! ToString(k).
+		var Pk = ToString(k);
+		// b. Let kPresent be ? HasProperty(O, Pk).
+		var kPresent = HasProperty(arraylike, Pk);
+		// c. If kPresent is true, then
+		if (kPresent) {
+			// i. Let kValue be ? Get(O, Pk).
+			var kValue = Get(arraylike, Pk);
+			// ii. Perform ? Call(callbackfn, T, « kValue, k, O »).
+			Call(callbackfn, T, [kValue, k, O]);
 		}
+		// d. Increase k by 1.
+		k = k + 1;
 	}
-};
+	// 7. Return undefined.
+	return undefined;
+});
