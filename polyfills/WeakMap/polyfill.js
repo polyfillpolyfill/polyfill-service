efc457759318ebaf41c4401306e0f251c9f4458c
Jake Champion <me@jakechampion.name>
2018-02-08 16:44:10 +0000

    Update polyfill.js

--- polyfills/WeakMap/polyfill.js
+++ polyfills/WeakMap/polyfill.js
@@ -1,5 +1,8 @@
+/* globals Symbol, OrdinaryCreateFromConstructor, IsCallable, GetIterator, IteratorStep, IteratorValue, IteratorClose, Get, Call, CreateMethodProperty, Type, SameValue */
 (function (global) {
-	// 23.3.1.1. WeakMap ( [ iterable ] )
+	// Deleted map items mess with iterator pointers, so rather than removing them mark them as deleted. Can't use undefined or null since those both valid keys so use a private symbol.
+	var undefMarker = Symbol('undef');
+	// 23.3.1.1 WeakMap ( [ iterable ] )
 	var WeakMap = function WeakMap(/* iterable */) {
 		// 1. If NewTarget is undefined, throw a TypeError exception.
 		if (!(this instanceof WeakMap)) {
@@ -9,78 +12,86 @@
 		var map = OrdinaryCreateFromConstructor(this, WeakMap.prototype, {
 			_keys: [],
 			_values: [],
-			_size: 0,
 			_es6WeakMap: true
 		});
+
 		// 3. Set map.[[WeakMapData]] to a new empty List.
 		// Polyfill.io - This step was done as part of step two.
 
 		// 4. If iterable is not present, let iterable be undefined.
 		var iterable = arguments.length > 0 ? arguments[0] : undefined;
+
 		// 5. If iterable is either undefined or null, return map.
 		if (iterable === null || iterable === undefined) {
 			return map;
 		}
+
 		// 6. Let adder be ? Get(map, "set").
-		var adder = Get(map, 'set');
+		var adder = Get(map, "set");
+
 		// 7. If IsCallable(adder) is false, throw a TypeError exception.
 		if (!IsCallable(adder)) {
 			throw new TypeError("WeakMap.prototype.set is not a function");
 		}
+
 		// 8. Let iteratorRecord be ? GetIterator(iterable).
 		try {
 			var iteratorRecord = GetIterator(iterable);
-		// 9. Repeat,
-		while (true) {
-		// a. Let next be ? IteratorStep(iterRecord).
-		var next = IteratorStep(iteratorRecord);
-		// b. If next is false, return map.
-		if (next === false) {
-			return map;
-		}
-		// c. Let nextItem be ? IteratorValue(next).
-		var nextItem = IteratorValue(next);
-		// d. If Type(nextItem) is not Object, then
-		if (Type(nextItem) !== 'object') {
-		// i. Let error be Completion{[[Type]]: throw, [[Value]]: a newly created TypeError object, [[Target]]: empty}.
-		try {
-			throw new TypeError('Iterator value ' + nextItem + ' is not an entry object');
-		} catch (error) {
-		// ii. Return ? IteratorClose(iteratorRecord, error).
-		return IteratorClose(iteratorRecord, error);
+			// 9. Repeat,
+			while (true) {
+				// a. Let next be ? IteratorStep(iteratorRecord).
+				var next = IteratorStep(iteratorRecord);
+				// b. If next is false, return map.
+				if (next === false) {
+					return map;
+				}
+				// c. Let nextItem be ? IteratorValue(next).
+				var nextItem = IteratorValue(next);
+				// d. If Type(nextItem) is not Object, then
+				if (Type(nextItem) !== 'object') {
+					// i. Let error be Completion{[[Type]]: throw, [[Value]]: a newly created TypeError object, [[Target]]: empty}.
+					try {
+						throw new TypeError('Iterator value ' + nextItem + ' is not an entry object');
+					} catch (error) {
+						// ii. Return ? IteratorClose(iteratorRecord, error).
+						return IteratorClose(iteratorRecord, error);
 					}
 				}
-			try {
+				try {
 					// Polyfill.io - The try catch accounts for steps: f, h, and j.
-		// e. Let k be Get(nextItem, "0").
-		var k = Get(nextItem, "0");
-		// f. If k is an abrupt completion, return ? IteratorClose(iteratorRecord, k).
-		// g. Let v be Get(nextItem, "1").
-		var v = Get(nextItem, "1");
-		// h. If v is an abrupt completion, return ? IteratorClose(iteratorRecord, v).
-		// i. Let status be Call(adder, map, « k.[[Value]], v.[[Value]] »).
-				Call(adder, map, [k, v]);
-			} catch (e) {
-		// j. If status is an abrupt completion, return ? IteratorClose(iteratorRecord, status).
-		return IteratorClose(iteratorRecord, e);
+
+					// e. Let k be Get(nextItem, "0").
+					var k = Get(nextItem, "0");
+					// f. If k is an abrupt completion, return ? IteratorClose(iteratorRecord, k).
+					// g. Let v be Get(nextItem, "1").
+					var v = Get(nextItem, "1");
+					// h. If v is an abrupt completion, return ? IteratorClose(iteratorRecord, v).
+					// i. Let status be Call(adder, map, « k.[[Value]], v.[[Value]] »).
+					Call(adder, map, [k, v]);
+				} catch (e) {
+					// j. If status is an abrupt completion, return ? IteratorClose(iteratorRecord, status).
+					return IteratorClose(iteratorRecord, e);
 				}
 			}
 		} catch (e) {
 			// Polyfill.io - For user agents which do not have iteration methods on argument objects or arrays, we can special case those.
-			if (IsArray(iterable) ||
+			if (Array.isArray(iterable) ||
 				Object.prototype.toString.call(iterable) === '[object Arguments]' ||
 				// IE 7 & IE 8 return '[object Object]' for the arguments object, we can detect by checking for the existence of the callee property
 				(!!iterable.callee)) {
 				var index;
 				var length = iterable.length;
 				for (index = 0; index < length; index++) {
-					adder.call(map, iterable[index][0], iterable[index][1]);
+					var k = iterable[index][0];
+					var v = iterable[index][1];
+					Call(adder, map, [k, v]);
 				}
 			}
 		}
 		return map;
 	};
-	// 23.3.2.1. WeakMap.prototype
+
+	// 23.3.2.1 WeakMap.prototype
 	// The initial value of WeakMap.prototype is the intrinsic object %WeakMapPrototype%.
 	// This property has the attributes { [[Writable]]: false, [[Enumerable]]: false, [[Configurable]]: false }.
 	Object.defineProperty(WeakMap, 'prototype', {
@@ -90,10 +101,10 @@
 		value: {}
 	});
 
-	// 23.3.3.1. WeakMap.prototype.constructor
+	// 23.3.3.1 WeakMap.prototype.constructor
 	CreateMethodProperty(WeakMap.prototype, 'constructor', WeakMap);
 
-	// 23.3.3.2. WeakMap.prototype.delete ( key )
+	// 23.3.3.2 WeakMap.prototype.delete ( key )
 	CreateMethodProperty(WeakMap.prototype, 'delete', function (key) {
 		// 1. Let M be the this value.
 		var M = this;
@@ -103,7 +114,7 @@
 		}
 		// 3. If M does not have a [[WeakMapData]] internal slot, throw a TypeError exception.
 		if (M._es6WeakMap !== true) {
-			throw new TypeError('Method Map.prototype.clear called on incompatible receiver ' + Object.prototype.toString.call(M));
+			throw new TypeError('Method WeakMap.prototype.clear called on incompatible receiver ' + Object.prototype.toString.call(M));
 		}
 		// 4. Let entries be the List that is M.[[WeakMapData]].
 		var entries = M._keys;
@@ -119,6 +130,7 @@
 				this._keys[i] = undefMarker;
 				// ii. Set p.[[Value]] to empty.
 				this._values[i] = undefMarker;
+				this._size = --this._size;
 				// iii. Return true.
 				return true;
 			}
@@ -127,8 +139,8 @@
 		return false;
 	});
 
-	// 23.3.3.3. WeakMap.prototype.get ( key )
-	CreateMethodProperty(Map.prototype, 'get', function get(key) {
+	// 23.3.3.3 WeakMap.prototype.get ( key )
+	CreateMethodProperty(WeakMap.prototype, 'get', function get(key) {
 		// 1. Let M be the this value.
 		var M = this;
 		// 2. If Type(M) is not Object, throw a TypeError exception.
@@ -137,7 +149,7 @@
 		}
 		// 3. If M does not have a [[WeakMapData]] internal slot, throw a TypeError exception.
 		if (M._es6WeakMap !== true) {
-			throw new TypeError('Method Map.prototype.get called on incompatible receiver ' + Object.prototype.toString.call(M));
+			throw new TypeError('Method WeakMap.prototype.get called on incompatible receiver ' + Object.prototype.toString.call(M));
 		}
 		// 4. Let entries be the List that is M.[[WeakMapData]].
 		var entries = M._keys;
@@ -156,16 +168,17 @@
 		return undefined;
 	});
 
-	// 23.3.3.4. WeakMap.prototype.has ( key )
+	// 23.3.3.4 WeakMap.prototype.has ( key )
 	CreateMethodProperty(WeakMap.prototype, 'has', function has(key) {
+		// 1. Let M be the this value.
 		var M = this;
 		// 2. If Type(M) is not Object, throw a TypeError exception.
-		if (Type(M) !== 'object') {
+		if (typeof M !== 'object') {
 			throw new TypeError('Method WeakMap.prototype.has called on incompatible receiver ' + Object.prototype.toString.call(M));
 		}
 		// 3. If M does not have a [[WeakMapData]] internal slot, throw a TypeError exception.
 		if (M._es6WeakMap !== true) {
-			throw new TypeError('Method Map.prototype.has called on incompatible receiver ' + Object.prototype.toString.call(M));
+			throw new TypeError('Method WeakMap.prototype.has called on incompatible receiver ' + Object.prototype.toString.call(M));
 		}
 		// 4. Let entries be the List that is M.[[WeakMapData]].
 		var entries = M._keys;
@@ -184,7 +197,7 @@
 		return false;
 	});
 
-	// 23.3.3.5. WeakMap.prototype.set ( key, value )
+	// 23.3.3.5 WeakMap.prototype.set ( key, value )
 	CreateMethodProperty(WeakMap.prototype, 'set', function set(key, value) {
 		// 1. Let M be the this value.
 		var M = this;
@@ -194,13 +207,13 @@
 		}
 		// 3. If M does not have a [[WeakMapData]] internal slot, throw a TypeError exception.
 		if (M._es6WeakMap !== true) {
-			throw new TypeError('Method Map.prototype.set called on incompatible receiver ' + Object.prototype.toString.call(M));
+			throw new TypeError('Method WeakMap.prototype.set called on incompatible receiver ' + Object.prototype.toString.call(M));
 		}
 		// 4. Let entries be the List that is M.[[WeakMapData]].
 		var entries = M._keys;
 		// 5. If Type(key) is not Object, throw a TypeError exception.
 		if (Type(key) !== 'object') {
-			throw new TypeError('Invalid value used as weak map key');
+			throw new TypeError("Invalid value used as weak map key");
 		}
 		// 6. For each Record {[[Key]], [[Value]]} p that is an element of entries, do
 		for (var i = 0; i < entries.length; i++) {
@@ -213,21 +226,22 @@
 			}
 		}
 		// 7. Let p be the Record {[[Key]]: key, [[Value]]: value}.
-		var p = {};
-			p['[[Key]]'] = key;
-			p['[[Value]]'] = value;
+		var p = {
+			'[[Key]]': key,
+			'[[Value]]': value
+		};
 		// 8. Append p as the last element of entries.
 		M._keys.push(p['[[Key]]']);
-			M._values.push(p['[[Value]]']);
+		M._values.push(p['[[Value]]']);
 		// 9. Return M.
 		return M;
 	});
 
-	// 23.3.3.6. WeakMap.prototype [ @@toStringTag ]
+	// 23.3.3.6 WeakMap.prototype [ @@toStringTag ]
 	// The initial value of the @@toStringTag property is the String value "WeakMap".
 	// This property has the attributes { [[Writable]]: false, [[Enumerable]]: false, [[Configurable]]: true }.
 
-	// Polyfill.io - Safari 8 implements Map.name but as a non-configurable property, which means it would throw an error if we try and configure it here.
+	// Polyfill.io - Safari 8 implements WeakMap.name but as a non-writable property, which means it would throw an error if we try and write to it here.
 	if (!('name' in WeakMap)) {
 		// 19.2.4.2 name
 		Object.defineProperty(WeakMap, 'name', {
