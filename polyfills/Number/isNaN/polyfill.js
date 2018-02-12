963c37bddba081239804054ebd3b30b0ded2af43
Jake Champion <me@jakechampion.name>
2018-02-12 18:22:00 +0000

    Improve tests for number.isnan (#1660)

--- polyfills/Number/isNaN/polyfill.js
+++ polyfills/Number/isNaN/polyfill.js
@@ -1,13 +1,16 @@
-// 20.1.2.4. Number.isNaN ( number )
-CreateMethodProperty(Number, 'isNaN', function (number) {
-	// 1. If Type(number) is not Number, return false.
-	if (Type(number) !== 'number') {
+(function () {
+	var that = this;
+	// 20.1.2.4. Number.isNaN ( number )
+	CreateMethodProperty(Number, 'isNaN', function isNaN(number) {
+		// 1. If Type(number) is not Number, return false.
+		if (Type(number) !== 'number') {
+			return false;
+		}
+		// 2. If number is NaN, return true.
+		if (that.isNaN(number)) {
+			return true;
+		}
+		// 3. Otherwise, return false.
 		return false;
-	}
-	// 2. If number is NaN, return true.
-	if (isNaN(number)) {
-		return true;
-	}
-	// 3. Otherwise, return false.
-	return false;
-});
+	});
+}());
