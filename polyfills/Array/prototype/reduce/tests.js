79b8eaa99aabed25898ccb9d8e7cf10ec3d082ff
Jake Champion <me@jakechampion.name>
2018-02-12 10:57:36 +0000

    improve test coverage for Array.prototype.reduce (#1628)

--- polyfills/Array/prototype/reduce/tests.js
+++ polyfills/Array/prototype/reduce/tests.js
@@ -1,5 +1,21 @@
-/* eslint-env mocha, browser */
-/* global proclaim */
+/* eslint-env mocha */
+/* globals proclaim */
+
+it('is a function', function () {
+	proclaim.isFunction(Array.prototype.reduce);
+});
+
+it('has correct arity', function () {
+	proclaim.arity(Array.prototype.reduce, 1);
+});
+
+it('has correct name', function () {
+	proclaim.hasName(Array.prototype.reduce, 'reduce');
+});
+
+it('is not enumerable', function () {
+	proclaim.nonEnumerable(Array.prototype, 'reduce');
+});
 
 var spycalls = [];
 
