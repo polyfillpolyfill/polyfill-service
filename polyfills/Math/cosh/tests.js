05dd5d41b1ca8b95fd2ad700a3f86e151ce8a7f2
Jake Champion <jakechampion.jake2@googlemail.com>
2018-02-15 01:26:40 +0000

    use correct assertion

--- polyfills/Math/cosh/tests.js
+++ polyfills/Math/cosh/tests.js
@@ -49,7 +49,7 @@ it('works as expected when called with positive integers', function () {
 	proclaim.strictEqual(Math.cosh(1), 1.5430806348152437);
 	proclaim.strictEqual(Math.cosh(90), 6.102016471589204e38);
 	proclaim.strictEqual(Math.cosh(709), 4.109203730777486e307);
-	proclaim.strictEqual(Math.cosh(710) > 1.1169973830808552e+308);
+	proclaim.isTrue(Math.cosh(710) >= 1.1169973830808552e+308);
 	proclaim.notStrictEqual(Math.cosh(710), Infinity);
 	proclaim.notNaN(Math.cosh(710));
 	proclaim.strictEqual(Math.cosh(711), Infinity);
