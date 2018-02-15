876dd9f94a1bec5ed1c25866d789ce3168aa34b1
Jake Champion <me@jakechampion.name>
2018-02-15 01:41:50 +0000

    Add tests for math.cosh (#1653)

--- polyfills/Math/cosh/polyfill.js
+++ polyfills/Math/cosh/polyfill.js
@@ -16,11 +16,15 @@ CreateMethodProperty(Math, 'cosh', function cosh(x) {
 	if (x === Infinity) {
 		return Infinity;
 	}
-	// If x is -∞, the result is -∞.
+	// If x is -∞, the result is +∞.
 	if (x === -Infinity) {
-		return -Infinity;
+		return Infinity;
+	}
+	x = Math.abs(x);
+	if (x > 709) {
+		var y = Math.exp(0.5 * x);
+		return y / 2 * y;
 	}
 	var y = Math.exp(x);
-
 	return (y + 1 / y) / 2;
 });
