3394c9307cc87dcb6f67b8778d15d1cad6839a0f
Jake Champion <me@jakechampion.name>
2018-02-16 01:11:16 +0000

    improve tests for string.prototype.fromcodepoint (#1666)
    * improve tests for string.prototype.fromcodepoint
    
    * Update tests.js
    
    * Make polyfill work under high load
    
    * Allow up to 10 seconds for tests to pass (required for IE7 on large inputs)

--- polyfills/String/fromCodePoint/polyfill.js
+++ polyfills/String/fromCodePoint/polyfill.js
@@ -1,5 +1,7 @@
 // 21.1.2.2. String.fromCodePoint ( ...codePoints )
 CreateMethodProperty(String, 'fromCodePoint', function fromCodePoint(_) {
+	// Polyfill.io - List to store the characters whilst iterating over the code points.
+	var result = [];
 	// 1. Let codePoints be a List containing the arguments passed to this function.
 	var codePoints = arguments;
 	// 2. Let length be the number of elements in codePoints.
@@ -10,6 +12,8 @@ CreateMethodProperty(String, 'fromCodePoint', function fromCodePoint(_) {
 	var nextIndex = 0;
 	// 5. Repeat, while nextIndex < length
 	while (nextIndex < length) {
+		// Polyfill.io - We reset the elements List as we store the partial results in the result List.
+		var elements = [];
 		// a. Let next be codePoints[nextIndex].
 		var next = codePoints[nextIndex];
 		// b. Let nextCP be ? ToNumber(next).
@@ -25,14 +29,17 @@ CreateMethodProperty(String, 'fromCodePoint', function fromCodePoint(_) {
 		// e. Append the elements of the UTF16Encoding of nextCP to the end of elements.
 		// Polyfill.io - UTF16Encoding can return a single codepoint or a list of multiple codepoints.
 		var cp = UTF16Encoding(nextCP);
-		if (isArray(cp)) {
+		if (IsArray(cp)) {
 			elements = elements.concat(cp);
 		} else {
 			elements.push(cp);
 		}
 		// f. Let nextIndex be nextIndex + 1.
 		nextIndex = nextIndex + 1;
+
+		// Polyfill.io - Retrieving the characters whilst iterating enables the function to work in a memory efficient and performant way.
+		result.push(String.fromCharCode.apply(null, elements))
 	}
 	// 6. Return the String value whose elements are, in order, the elements in the List elements. If length is 0, the empty string is returned.
-	return length === 0 ? '' : String.fromCharCode.apply(null, elements);
+	return length === 0 ? '' : result.join('');
 });
