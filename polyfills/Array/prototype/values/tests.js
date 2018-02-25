937036dd7fe294a046bac3d6eda024acfde5ce05
Jake Champion <me@jakechampion.name>
2018-02-25 13:42:08 +0000

    Update tests.js

--- polyfills/Array/prototype/values/tests.js
+++ polyfills/Array/prototype/values/tests.js
@@ -10,28 +10,21 @@ it('has correct arity', function () {
 });
 
 it('has correct name', function () {
-	proclaim.hasName(Array.prototype.values, 'values');
+	try {
+		proclaim.hasName(Array.prototype.values, 'values');
+	} catch (e) {
+		// Chrome 40 implements the Symbol.iterator function for Arrays but has it named ArrayValues.
+		try {
+			proclaim.equal([].values.name, 'ArrayValues');
+		} catch (e) {
+			// Firefox 44 has it named [Symbol.iterator].
+			proclaim.equal([].values.name, '[Symbol.iterator]');
+		}
+	}
 });
 
 it('is not enumerable', function () {
 	proclaim.nonEnumerable(Array.prototype, 'values');
-});
-
-it('is named \'values\' or \'ArrayValues\'', function () {
-	// Don't fail tests just because browser doesn't support the Function.name polyfill.
-	if ([].values.name) {
-		try {
-			proclaim.equal([].values.name, 'values');
-		} catch (e) {
-			// Chrome 40 implements the Symbol.iterator function for Arrays but has it named ArrayValues.
-			try {
-				proclaim.equal([].values.name, 'ArrayValues');
-			} catch (e) {
-				// Firefox 44 has it named [Symbol.iterator].
-				proclaim.equal([].values.name, '[Symbol.iterator]');
-			}
-		}
-	}
 });
 
 it('returns a next-able object', function () {
