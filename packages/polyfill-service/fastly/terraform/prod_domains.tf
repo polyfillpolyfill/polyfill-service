bdef310bf8ddaea23c504b84954ab9d22316b485
Jake Champion <jakechampion.jake2@googlemail.com>
2018-10-16 16:22:49 +0100

    fix linting issues

--- packages/polyfill-service/fastly/terraform/prod_domains.tf
+++ packages/polyfill-service/fastly/terraform/prod_domains.tf
@@ -1,21 +1,21 @@
 resource "fastly_service_v1" "app" {
   domain {
-    name    = "polyfill.io"
+    name = "polyfill.io"
   }
 
   domain {
-    name    = "cdn.polyfill.io"
+    name = "cdn.polyfill.io"
   }
 
   domain {
-    name    = "cdn.polyfills.io"
+    name = "cdn.polyfills.io"
   }
 
   domain {
-    name    = "polyfills.io"
+    name = "polyfills.io"
   }
 
   domain {
-    name    = "polyfill.webservices.ft.com"
+    name = "polyfill.webservices.ft.com"
   }
 }
