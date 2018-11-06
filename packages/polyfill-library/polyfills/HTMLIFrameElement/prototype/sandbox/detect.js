56fd45886b886cdae86029a61d900b9b443e8620
Jake Champion <jakechampion.jake2@googlemail.com>
2018-11-06 14:13:05 +0000

    Make the feature detection not throw an error if `sandox` property is not a DOMTokenList

--- packages/polyfill-library/polyfills/HTMLIFrameElement/prototype/sandbox/detect.js
+++ packages/polyfill-library/polyfills/HTMLIFrameElement/prototype/sandbox/detect.js
@@ -1,5 +1,13 @@
 'document' in this && 'HTMLIFrameElement' in this && 'sandbox' in HTMLIFrameElement.prototype && (function () {
 	var e = document.createElement('iframe');
-	e.sandbox.add('allow-scripts', 'allow-modals');
-	return e.sandbox.contains('allow-modals');
+	if ('sandbox' in e) {
+		if (typeof e.sandbox.add === 'function') {
+			e.sandbox.add('allow-scripts', 'allow-modals');
+			return e.sandbox.contains('allow-modals');
+		} else {
+			return false;
+		}
+	} else {
+		return false;
+	}
 }())
