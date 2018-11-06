a1d5803c0b419432fb4c930f93bbba7ec8a23ae6
Jake Champion <jakechampion.jake2@googlemail.com>
2018-11-06 15:14:09 +0000

    Add comment about Safari 9

--- packages/polyfill-library/polyfills/Element/prototype/classList/polyfill.js
+++ packages/polyfill-library/polyfills/Element/prototype/classList/polyfill.js
@@ -1,4 +1,5 @@
 /* global _addDOMTokenListProperty */
 _addDOMTokenListProperty(window.Element, "classList", "class");
-// Internet Explorer's HTMLElement does not inherit from Element, we need to patch classList on HTMLElement as well
+// Internet Explorer's HTMLElement does not inherit from Element, we need to patch classList on HTMLElement as well.
+// Safari 9 does not let you configure Element.prototype.classList, we need to patch HTMLElement instead.
 _addDOMTokenListProperty(window.HTMLElement, "classList", "class");
