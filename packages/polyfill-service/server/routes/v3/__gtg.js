23bb346c8fb1fe4c42a0edf8e440ed1373f30ac6
Jake Champion <jakechampion.jake2@googlemail.com>
2018-11-14 12:21:48 +0000

    fix content-type header

--- packages/polyfill-service/server/routes/v3/__gtg.js
+++ packages/polyfill-service/server/routes/v3/__gtg.js
@@ -3,7 +3,10 @@
 module.exports = app => {
 	app.get("/v3/__gtg", (request, response) => {
 		response.status(200);
-		response.set("Cache-Control", "max-age=0, must-revalidate, no-cache, no-store, private");
+		response.set({
+			"Cache-Control": "max-age=0, must-revalidate, no-cache, no-store, private",
+			"Content-Type": "text/html; charset=utf-8"
+		});
 		response.send("OK");
 	});
 };
