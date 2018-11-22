d56b3f029b202c59164530b7ff2a3cc04c82f62d
Jake Champion <jakechampion.jake2@googlemail.com>
2018-11-22 16:28:22 +0000

    Add ability to use polyfills from v3.25.2 in the v3 endpoint

--- packages/polyfill-service/server/routes/v3/polyfill.js
+++ packages/polyfill-service/server/routes/v3/polyfill.js
@@ -5,22 +5,58 @@ const compressBundle = require("../../lib/compress-bundle");
 const createPolyfillLibrary = require("../../lib/create-polyfill-library");
 const getPolyfillParameters = require("../../lib/get-polyfill-parameters");
 const createPolyfillBundle = require("../../lib/create-polyfill-bundle");
-
+const latestVersion = require("polyfill-library/package.json").version;
+const polyfillio_3_25_2 = require("polyfill-library-3.25.2");
 module.exports = app => {
 	app.get(["/v3/polyfill.js", "/v3/polyfill.min.js"], async (request, response) => {
 		const params = getPolyfillParameters(request);
-		const polyfillio = await createPolyfillLibrary(params.version);
-		const bundle = await createPolyfillBundle(params, polyfillio);
-		const etag = await sha256(bundle);
-		const file = await compressBundle(params.compression, bundle);
-		response.status(200);
-		response.set({
-			"Cache-Control": "public, s-maxage=31536000, max-age=604800, stale-while-revalidate=604800, stale-if-error=604800",
-			"Content-Encoding": params.compression,
-			"Content-Type": "application/javascript; charset=utf-8",
-			Etag: etag,
-			"surrogate-key": "polyfill-service"
-		});
-		response.send(file);
+		switch (params.version) {
+			case latestVersion: {
+				const polyfillio = await createPolyfillLibrary(params.version);
+				const bundle = await createPolyfillBundle(params, polyfillio);
+				const etag = await sha256(bundle);
+				const file = await compressBundle(params.compression, bundle);
+				response.status(200);
+				response.set({
+					"Cache-Control": "public, s-maxage=31536000, max-age=604800, stale-while-revalidate=604800, stale-if-error=604800",
+					"Content-Encoding": params.compression,
+					"Content-Type": "application/javascript; charset=utf-8",
+					Etag: etag,
+					"surrogate-key": "polyfill-service"
+				});
+				response.send(file);
+				break;
+			}
+			case "3.25.2": {
+				let bundle = await polyfillio_3_25_2.getPolyfillString(params);
+
+				if (params.callback) {
+					bundle += "\ntypeof " + params.callback + "==='function' && " + params.callback + "();";
+				}
+
+				const etag = await sha256(bundle);
+				const file = await compressBundle(params.compression, bundle);
+
+				response.status(200);
+				response.set({
+					"Cache-Control": "public, s-maxage=31536000, max-age=604800, stale-while-revalidate=604800, stale-if-error=604800",
+					"Content-Encoding": params.compression,
+					"Content-Type": "application/javascript; charset=utf-8",
+					Etag: etag,
+					"surrogate-key": "polyfill-service"
+				});
+				response.send(file);
+				break;
+			}
+			default: {
+				response.status(400);
+				response.set({
+					"Cache-Control": "public, s-maxage=31536000, max-age=604800, stale-while-revalidate=604800, stale-if-error=604800",
+					"surrogate-key": "polyfill-service"
+				});
+				response.send(`version: ${params.version} does not exist`);
+				break;
+			}
+		}
 	});
 };
