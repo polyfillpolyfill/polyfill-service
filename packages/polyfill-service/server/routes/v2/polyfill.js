ea578c71075ab8693cf79d8e2a8dbb72d1088dd3
Jake Champion <jakechampion.jake2@googlemail.com>
2018-11-22 16:14:53 +0000

    Only respond to the routes /v2/polyfill.js and /v2/polyfill.min.js

--- packages/polyfill-service/server/routes/v2/polyfill.js
+++ packages/polyfill-service/server/routes/v2/polyfill.js
@@ -8,9 +8,8 @@ const mergeStream = require("merge2");
 const streamFromString = require("from2-string");
 
 module.exports = app => {
-	app.get(/^\/v2\/polyfill(\.\w+)(\.js)?/, (req, res) => {
-		const firstParameter = req.params[0].toLowerCase();
-		const minified = firstParameter === ".min";
+	app.get(["/v2/polyfill.js", "/v2/polyfill.min.js"], (req, res) => {
+		const minified = req.path.endsWith(".min.js");
 		const uaString = (typeof req.query.ua === "string" && req.query.ua) || req.header("user-agent");
 		// If inbound request did not specify UA on the query string, the cache key must use the HTTP header
 		if (!req.query.ua) {
