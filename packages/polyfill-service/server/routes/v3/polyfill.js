3802de056022118a18b1ab6104144b24aa793d0b
Jake Champion <jakechampion.jake2@googlemail.com>
2018-11-22 17:36:07 +0000

    dry up the code

--- packages/polyfill-service/server/routes/v3/polyfill.js
+++ packages/polyfill-service/server/routes/v3/polyfill.js
@@ -1,11 +1,23 @@
 "use strict";
 
-const sha256 = require("../../lib/sha-256");
 const compressBundle = require("../../lib/compress-bundle");
 const getPolyfillParameters = require("../../lib/get-polyfill-parameters");
 const latestVersion = require("polyfill-library/package.json").version;
 const PolyfillLibrary = require("polyfill-library");
 const polyfillio_3_25_2 = require("polyfill-library-3.25.2");
+
+async function respondWithBundle(response, params, bundle) {
+	const file = await compressBundle(params.compression, bundle);
+	const headers = {
+		"Cache-Control": "public, s-maxage=31536000, max-age=604800, stale-while-revalidate=604800, stale-if-error=604800",
+		"Content-Encoding": params.compression,
+		"Content-Type": "application/javascript; charset=utf-8",
+		"surrogate-key": "polyfill-service"
+	};
+	response.status(200);
+	response.set(headers);
+	response.send(file);
+}
 
 module.exports = app => {
 	app.get(["/v3/polyfill.js", "/v3/polyfill.min.js"], async (request, response) => {
@@ -14,17 +26,7 @@ module.exports = app => {
 			case latestVersion: {
 				const polyfillio = new PolyfillLibrary();
 				const bundle = await polyfillio.getPolyfillString(params);
-				const etag = await sha256(bundle);
-				const file = await compressBundle(params.compression, bundle);
-				response.status(200);
-				response.set({
-					"Cache-Control": "public, s-maxage=31536000, max-age=604800, stale-while-revalidate=604800, stale-if-error=604800",
-					"Content-Encoding": params.compression,
-					"Content-Type": "application/javascript; charset=utf-8",
-					Etag: etag,
-					"surrogate-key": "polyfill-service"
-				});
-				response.send(file);
+				await respondWithBundle(response, params, bundle);
 				break;
 			}
 			case "3.25.2": {
@@ -34,18 +36,7 @@ module.exports = app => {
 					bundle += "\ntypeof " + params.callback + "==='function' && " + params.callback + "();";
 				}
 
-				const etag = await sha256(bundle);
-				const file = await compressBundle(params.compression, bundle);
-
-				response.status(200);
-				response.set({
-					"Cache-Control": "public, s-maxage=31536000, max-age=604800, stale-while-revalidate=604800, stale-if-error=604800",
-					"Content-Encoding": params.compression,
-					"Content-Type": "application/javascript; charset=utf-8",
-					Etag: etag,
-					"surrogate-key": "polyfill-service"
-				});
-				response.send(file);
+				await respondWithBundle(response, params, bundle);
 				break;
 			}
 			default: {
