4c0a61838d8e54ca35d0cf1e5d73285b83f6ffc9
Jake Champion <jakechampion.jake2@googlemail.com>
2018-11-30 14:59:36 +0000

    remove contains

--- packages/polyfill-library/polyfills/Array/prototype/contains/tests.js
+++
@@ -1,6 +0,0 @@
-/* eslint-env mocha, browser */
-/* global proclaim */
-
-it('is an alias to Array.prototype.includes', function () {
-	proclaim.deepEqual(Array.prototype.contains, Array.prototype.includes);
-});
