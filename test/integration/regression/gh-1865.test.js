494b9e87b9e2af4583e7f58445f5e2f73a52a159
Jake Champion <jakechampion.jake2@googlemail.com>
2019-01-16 17:27:58 +0000

    add more paths

--- test/integration/regression/gh-1865.test.js
+++ test/integration/regression/gh-1865.test.js
@@ -8,13 +8,16 @@ const host = require("../helpers").host;
 const vm = require("vm");
 
 describe("https://github.com/Financial-Times/polyfill-service/issues/1865", function() {
-	it("responds with a bundle containing the correct polyfills", function() {
-		this.timeout(30000);
-		return request(host)
-			.get("/v2/polyfill.minify.js")
-			.then(response => {
-				assert.isString(response.text);
-				assert.doesNotThrow(() => new vm.Script(response.text));
-			});
-	});
+	const pathsWebsitesHaveUsedWhichWereNotDocumented = ["/v2/polyfill.debug.js", "/v2/polyfill.min.js.js", "/v2/polyfill.js.js", "/v2/polyfill.minify.js", "/v2/polyfill.production.min.js"];
+	for (const path of pathsWebsitesHaveUsedWhichWereNotDocumented) {
+		it("responds with a bundle containing the correct polyfills", function() {
+			this.timeout(30000);
+			return request(host)
+				.get(path)
+				.then(response => {
+					assert.isString(response.text);
+					assert.doesNotThrow(() => new vm.Script(response.text));
+				});
+		});
+	}
 });
