a022ffe61db72e77fff422e166e0ddac6a33ae0d
Jake Champion <me@jakechampion.name>
2019-01-24 12:44:55 +0000

    Add healthchecks to cdn config (#1869)

--- fastly/terraform/qa_override.tf
+++ fastly/terraform/qa_override.tf
@@ -7,6 +7,7 @@ resource "fastly_service_v1" "app" {
     name                  = "v3_eu"
     address               = "origami-polyfill-service-qa-eu.herokuapp.com"
     port                  = 443
+    healthcheck           = "v3_eu_healthcheck"
     ssl_cert_hostname     = "*.herokuapp.com"
     auto_loadbalance      = false
     connect_timeout       = 5000
@@ -15,16 +16,35 @@ resource "fastly_service_v1" "app" {
     error_threshold       = 0
   }
 
+  healthcheck {
+    name      = "v3_eu_healthcheck"
+    host      = "origami-polyfill-service-qa-eu.herokuapp.com"
+    path      = "/__gtg"
+    timeout   = 5000
+    threshold = 2
+    window    = 5
+  }
+
   backend {
     name                  = "v3_us"
     address               = "origami-polyfill-service-qa-us.herokuapp.com"
     port                  = 443
+    healthcheck           = "v3_us_healthcheck"
     ssl_cert_hostname     = "*.herokuapp.com"
     auto_loadbalance      = false
     connect_timeout       = 5000
     first_byte_timeout    = 120000
     between_bytes_timeout = 120000
     error_threshold       = 0
+  }
+
+  healthcheck {
+    name      = "v3_us_healthcheck"
+    host      = "origami-polyfill-service-qa-us.herokuapp.com"
+    path      = "/__gtg"
+    timeout   = 5000
+    threshold = 2
+    window    = 5
   }
 
   condition {
