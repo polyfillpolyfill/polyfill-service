34ea5097fac34706c7d655505a25c428cdb029d3
Jake Champion <jakechampion.jake2@googlemail.com>
2019-03-11 22:22:51 +0000

    Revert "make the test description nicer"
    This reverts commit b36b09e77726028e0d928eb89d1619fcefc1000b.

--- test/integration/regression/gh-1865.test.js
+++ test/integration/regression/gh-1865.test.js
@@ -10,16 +10,14 @@ const vm = require("vm");
 describe("https://github.com/Financial-Times/polyfill-service/issues/1865", function() {
 	const pathsWebsitesHaveUsedWhichWereNotDocumented = ["/v2/polyfill.debug.js", "/v2/polyfill.min.js.js", "/v2/polyfill.js.js", "/v2/polyfill.minify.js", "/v2/polyfill.production.min.js"];
 	for (const path of pathsWebsitesHaveUsedWhichWereNotDocumented) {
-		context(host + path, function() {
-			it("responds with a bundle containing the correct polyfills", function() {
-				this.timeout(30000);
-				return request(host)
-					.get(path)
-					.then(response => {
-						assert.isString(response.text);
-						assert.doesNotThrow(() => new vm.Script(response.text));
-					});
-			});
+		it("responds with a bundle containing the correct polyfills", function() {
+			this.timeout(30000);
+			return request(host)
+				.get(path)
+				.then(response => {
+					assert.isString(response.text);
+					assert.doesNotThrow(() => new vm.Script(response.text));
+				});
 		});
 	}
 });
