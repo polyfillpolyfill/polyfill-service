3b2330f8698f686545d77d0db6dc906aca797a52
Jake Champion <jakechampion.jake2@googlemail.com>
2019-03-13 14:50:46 +0000

    remove assetgraph builder

--- server/service.js
+++ server/service.js
@@ -33,7 +33,7 @@ function service(options) {
 	app.use(compression({ level: 9 }));
 	app.use(extractHeaders());
 	app.use(
-		serveStatic(path.join(__dirname, "../production"), {
+		serveStatic(path.join(__dirname, "../dist"), {
 			setHeaders: function(res) {
 				res.setHeader("Cache-Control", "public, s-maxage=31536000, max-age=604800, stale-while-revalidate=604800, stale-if-error=604800");
 				res.setHeader("Surrogate-Key", "polyfill-service, polyfill-service-website");
