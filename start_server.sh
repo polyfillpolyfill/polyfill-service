759d2f225571a1fc67acd2bc9fc26e85a7d643d9
Jake Champion <jakechampion.jake2@googlemail.com>
2019-03-13 18:32:05 +0000

    remove old flag and add some newer ones

--- start_server.sh
+++ start_server.sh
@@ -2,11 +2,11 @@
 
 if [ ! "$WEB_MEMORY" = "" ]; then
   if [ $WEB_MEMORY -le 512 ]; then
-    NODE_FLAGS="--max_semi_space_size=2 --max_old_space_size=256 --max_executable_size=192"
+    NODE_FLAGS="--gc_interval=100 --optimize_for_size --max_semi_space_size=2 --max_old_space_size=256"
   elif [ $WEB_MEMORY -le 768 ]; then
-    NODE_FLAGS="--max_semi_space_size=8 --max_old_space_size=512 --max_executable_size=384"
+    NODE_FLAGS="--gc_interval=100 --optimize_for_size --max_semi_space_size=8 --max_old_space_size=512"
   elif [ $WEB_MEMORY -le 1024 ]; then
-    NODE_FLAGS="--max_semi_space_size=16 --max_old_space_size=1024 --max_executable_size=512"
+    NODE_FLAGS="--gc_interval=100 --optimize_for_size --max_semi_space_size=16 --max_old_space_size=1024"
   fi
 fi
 
