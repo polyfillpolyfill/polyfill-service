675a40929e87ff29c1ea5ee265886fbe0cd68b49
Jake Champion <jakechampion.jake2@googlemail.com>
2019-03-19 10:57:08 +0000

    Add test which shows url-encoded flags return same content as non-url-encoded flags

---
+++ test/integration/regression/gh-1910.test.js
@@ -0,0 +1,26 @@
+/* eslint-env mocha */
+
+"use strict";
+
+const axios = require("axios");
+const proclaim = require("proclaim");
+const host = require("../helpers").host;
+
+describe("https://github.com/Financial-Times/polyfill-service/issues/1865", function() {
+	this.timeout(30000);
+	describe("GET /v2/polyfill.js?features=all&unknown=polyfill&flags=always,gated", function() {
+		it("responds with same javascript when flags parameter is url encoded", async function() {
+			const nonUrlEncodedFlag = (await axios.get(host + "/v2/polyfill.js?features=all&unknown=polyfill&flags=always,gated")).data;
+			const urlEncodedFlag = (await axios.get(host + "/v2/polyfill.js?features=all&unknown=polyfill&flags=always%2Cgated")).data;
+			proclaim.strictEqual(nonUrlEncodedFlag, urlEncodedFlag);
+		});
+	});
+
+	describe("GET /v3/polyfill.js?features=all&unknown=polyfill&flags=always,gated", function() {
+		it("responds with same javascript when flags parameter is url encoded", async function() {
+			const nonUrlEncodedFlag = (await axios.get(host + "/v3/polyfill.js?features=all&unknown=polyfill&flags=always,gated")).data;
+			const urlEncodedFlag = (await axios.get(host + "/v3/polyfill.js?features=all&unknown=polyfill&flags=always%2Cgated")).data;
+			proclaim.strictEqual(nonUrlEncodedFlag, urlEncodedFlag);
+		});
+	});
+});
