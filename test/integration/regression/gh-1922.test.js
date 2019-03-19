3273cbe899eb74df76fd09e4a0f7c930bfc75439
Jake Champion <jakechampion.jake2@googlemail.com>
2019-03-19 15:54:55 +0000

    update the assertion and test endpoint

--- test/integration/regression/gh-1922.test.js
+++ test/integration/regression/gh-1922.test.js
@@ -8,7 +8,7 @@ const host = require("../helpers").host;
 
 describe("https://github.com/Financial-Times/polyfill-service/issues/1922", function() {
 	this.timeout(30000);
-	describe("GET /v3/normalise_querystring_parameters_for_polyfill_bundle?callback=&compression=br&excludes=&features=IntersectionObserver%2Cdefault%2Cfetch&flags=&rum=0&ua=chrome%2F72.0.0&unknown=ignore&version=3.25.1", function() {
+	describe(`GET ${host}/v3/normalise_querystring_parameters_for_polyfill_bundle?callback=&compression=br&excludes=&features=IntersectionObserver%2Cdefault%2Cfetch&flags=&rum=0&ua=chrome%2F72.0.0&unknown=ignore&version=3.25.1`, function() {
 		it("responds with same javascript when ua parameter is url encoded", async function() {
 			const nonUrlEncodedUa = (await axios.get(
 				host +
@@ -18,7 +18,7 @@ describe("https://github.com/Financial-Times/polyfill-service/issues/1922", func
 				host +
 					"/v3/normalise_querystring_parameters_for_polyfill_bundle?callback=&compression=br&excludes=&features=IntersectionObserver%2Cdefault%2Cfetch&flags=&rum=0&ua=chrome%2F72.0.0&unknown=ignore&version=3.25.1"
 			)).data;
-			proclaim.strictEqual(nonUrlEncodedUa, urlEncodedUa);
+			proclaim.deepStrictEqual(nonUrlEncodedUa, urlEncodedUa);
 		});
 	});
 });
