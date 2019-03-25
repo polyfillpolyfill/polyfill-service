973e3840ff92145737417c10bfb924897859ce03
Jake Champion <jakechampion.jake2@googlemail.com>
2019-03-25 16:02:12 +0000

    Replace subset with looping and sampling

--- test/integration/v3/generative.test.js
+++ test/integration/v3/generative.test.js
@@ -3,7 +3,6 @@
 "use strict";
 
 const request = require("supertest");
-const assert = require("proclaim");
 const host = require("../helpers").host;
 const querystring = require("querystring");
 const _ = require("lodash");
@@ -13,41 +12,43 @@ process.env.NODE_ENV = "production";
 // const polyfillio_3_25_3 = require("polyfill-library-3.25.3");
 // const polyfillio_3_25_1 = require("polyfill-library-3.25.1");
 
-function* subsets(array, offset = 0) {
-	while (offset < array.length) {
-		const first = array[offset++];
-		for (const subset of subsets(array, offset)) {
-			subset.push(first);
-			yield subset;
-		}
+function* mapWith(fn, iterable) {
+	for (const element of iterable) {
+		yield fn(element);
 	}
-	yield [];
+}
+
+const sample = (amount, iterable) => mapWith(element => _.sampleSize(element, amount), iterable);
+
+function* take(n, iter) {
+	let index = 0;
+	for (const val of iter) {
+		if (index >= n) {
+			return;
+		}
+		index = index + 1;
+		yield val;
+	}
+}
+
+function* loop(amount, thing) {
+	while (amount > 0) {
+		yield thing;
+		amount--;
+	}
 }
 
 function createTest(polyfillBundleOptions, polyfillBundle) {
 	describe("test all combinations of polyfills/aliases", function() {
-		context(polyfillBundleOptions, function() {
+		context(host + polyfillBundleOptions, function() {
 			this.timeout(30000);
-			it("responds with a correct status, content-type, cache-control, surrogate-key", () => {
+			it("responds with a correct polyfill bundle", () => {
 				return request(host)
 					.get(polyfillBundleOptions)
-					.set("Fastly-Debug", "true")
-					.expect(200)
-					.expect("Content-Type", "text/javascript; charset=utf-8")
-					.expect("cache-control", "public, s-maxage=31536000, max-age=604800, stale-while-revalidate=604800, stale-if-error=604800")
-					.expect("surrogate-key", "polyfill-service")
-					.then(response => {
-						assert.isString(response.text);
-						assert.include(response.text, polyfillBundle);
-						assert.notMatch(response.text, /\/\/#\ssourceMappingURL(.+)/);
-					});
+					.expect(polyfillBundle);
 			});
 		});
 	});
-}
-
-function sample(collection, amount = 10) {
-	return _.sampleSize(collection, amount);
 }
 
 function arrayToObject(arr) {
@@ -56,15 +57,17 @@ function arrayToObject(arr) {
 		return accumulator;
 	}, {});
 }
+
 async function tests() {
 	// const useragents = Object.keys(require("@financial-times/polyfill-useragent-normaliser/data.json").baselineVersions);
 	const polyfills = (await polyfillio.listAllPolyfills()).filter(feature => !feature.startsWith("_"));
 	const polyfillsWithoutIntl = polyfills.filter(feature => !feature.startsWith("Intl"));
-	// const aliases = Object.keys(await polyfillio.listAliases()).filter(alias => !alias.startsWith("modernizr") && !alias.startsWith("caniuse"));
-	// const intlPolyfills = polyfills.filter(feature => feature.startsWith("Intl"));
-	const features = [].concat(sample(polyfillsWithoutIntl));
+	const aliases = Object.keys(await polyfillio.listAliases()).filter(alias => !alias.startsWith("modernizr") && !alias.startsWith("caniuse"));
+	const intlPolyfills = polyfills.filter(feature => feature.startsWith("Intl"));
+	const polyfillsWithOnlyOneIntlLocale = polyfillsWithoutIntl.concat(_.sample(intlPolyfills));
+	const features = [].concat(polyfillsWithOnlyOneIntlLocale, aliases);
 	const ua = "ie/10";
-	for (const subset of subsets(features)) {
+	for (const subset of take(15, sample(10, loop(Infinity, features)))) {
 		const polyfillBundle = await polyfillio.getPolyfillString({
 			minify: false,
 			uaString: ua,
