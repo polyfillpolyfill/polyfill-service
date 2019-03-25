8f1d022b8ae91d29c70b990a31568cc0442c3c61
Jake Champion <jakechampion.jake2@googlemail.com>
2019-03-25 12:47:20 +0000

    Add generative tests which test a random combintation of 10 polyfills and ensures that the expected bundle is correct

---
+++ test/integration/v3/generative.test.js
@@ -0,0 +1,89 @@
+/* eslint-env mocha */
+
+"use strict";
+
+const request = require("supertest");
+const assert = require("proclaim");
+const host = require("../helpers").host;
+const querystring = require("querystring");
+const _ = require("lodash");
+const streamToString = require("stream-to-string");
+const polyfillio = require("polyfill-library");
+const polyfillioSources = require("polyfill-library/lib/sources");
+// const polyfillio_3_27_4 = require("polyfill-library-3.27.4");
+// const polyfillio_3_25_3 = require("polyfill-library-3.25.3");
+// const polyfillio_3_25_1 = require("polyfill-library-3.25.1");
+
+function* subsets(array, offset = 0) {
+	while (offset < array.length) {
+		const first = array[offset++];
+		for (const subset of subsets(array, offset)) {
+			subset.push(first);
+			yield subset;
+		}
+	}
+	yield [];
+}
+
+function createTest(polyfillBundleOptions, polyfillSources) {
+	describe("test all combinations of polyfills/aliases", function() {
+		context(polyfillBundleOptions, function() {
+			this.timeout(30000);
+			it("responds with a correct status, content-type, cache-control, surrogate-key", () => {
+				return request(host)
+					.get(polyfillBundleOptions)
+					.set("Fastly-Debug", "true")
+					.expect(200)
+					.expect("Content-Type", "text/javascript; charset=utf-8")
+					.expect("cache-control", "public, s-maxage=31536000, max-age=604800, stale-while-revalidate=604800, stale-if-error=604800")
+					.expect("surrogate-key", "polyfill-service")
+					.then(response => {
+						assert.isString(response.text);
+						for (const polyfill of polyfillSources) {
+							assert.include(response.text, polyfill);
+						}
+						assert.notMatch(response.text, /\/\/#\ssourceMappingURL(.+)/);
+					});
+			});
+		});;
+	});
+}
+
+function sample(collection, amount = 10) {
+	return _.sampleSize(collection, amount);
+}
+
+function arrayToObject(arr) {
+	return arr.reduce(function(accumulator, item) {
+		accumulator[item] = {};
+		return accumulator;
+	}, {});
+}
+async function tests() {
+	// const useragents = Object.keys(require("@financial-times/polyfill-useragent-normaliser/data.json").baselineVersions);
+	const polyfills = (await polyfillio.listAllPolyfills()).filter(feature => !feature.startsWith("_"));
+	const polyfillsWithoutIntl = polyfills.filter(feature => !feature.startsWith("Intl"));
+	// const aliases = Object.keys(await polyfillio.listAliases()).filter(alias => !alias.startsWith("modernizr") && !alias.startsWith("caniuse"));
+	// const intlPolyfills = polyfills.filter(feature => feature.startsWith("Intl"));
+	const features = [].concat(sample(polyfillsWithoutIntl));
+	const ua = "ie/10";
+	for (const subset of subsets(features)) {
+		const polyfills = Object.keys(
+			await polyfillio.getPolyfills({
+				minify: false,
+				uaString: ua,
+				features: arrayToObject(subset)
+			})
+		);
+		const polyfillSources = await Promise.all(polyfills.map(polyfill => streamToString(polyfillioSources.streamPolyfillSource(polyfill, "raw"))));
+		const qs = querystring.stringify({
+			features: subset.join(","),
+			ua
+		});
+		const polyfillBundleOptions = `/v3/polyfill.js?${qs}`;
+		createTest(polyfillBundleOptions, polyfillSources);
+	}
+	run();
+}
+
+tests();
