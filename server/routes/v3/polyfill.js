4793e2a74cf69f6bd6d4b1f0565d1db1451dc70e
Jake Champion <jakechampion.jake2@googlemail.com>
2019-07-23 14:09:58 +0100

    update polyfill-library to 3.36.0 and keep previous version

--- server/routes/v3/polyfill.js
+++ server/routes/v3/polyfill.js
@@ -9,6 +9,7 @@ const polyfillio_3_25_3 = require("polyfill-library-3.25.3");
 const polyfillio_3_25_1 = require("polyfill-library-3.25.1");
 const polyfillio_3_28_1 = require("polyfill-library-3.28.1");
 const polyfillio_3_34_0 = require("polyfill-library-3.34.0");
+const polyfillio_3_35_0 = require("polyfill-library-3.35.0");
 
 async function respondWithBundle(response, params, bundle) {
 	const file = await compressBundle(params.compression, bundle);
@@ -31,6 +32,11 @@ module.exports = app => {
 		switch (params.version) {
 			case latestVersion: {
 				const bundle = await polyfillio.getPolyfillString(params);
+				await respondWithBundle(response, params, bundle);
+				break;
+			}
+			case "3.35.0": {
+				const bundle = await polyfillio_3_35_0.getPolyfillString(params);
 				await respondWithBundle(response, params, bundle);
 				break;
 			}
