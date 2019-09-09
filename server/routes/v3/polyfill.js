763bc8feb05027e1358ae2f6032d58caf43d11fb
Jake Champion <jakechampion.jake2@googlemail.com>
2019-09-09 10:56:56 +0100

    update polyfill-library to 3.40, which serves IntersectionObserver to Safari 12.1

--- server/routes/v3/polyfill.js
+++ server/routes/v3/polyfill.js
@@ -13,6 +13,7 @@ const polyfillio_3_35_0 = require("polyfill-library-3.35.0");
 const polyfillio_3_36_0 = require("polyfill-library-3.36.0");
 const polyfillio_3_37_0 = require("polyfill-library-3.37.0");
 const polyfillio_3_38_0 = require("polyfill-library-3.38.0");
+const polyfillio_3_39_0 = require("polyfill-library-3.39.0");
 
 async function respondWithBundle(response, params, bundle) {
 	const file = await compressBundle(params.compression, bundle);
@@ -35,6 +36,11 @@ module.exports = app => {
 		switch (params.version) {
 			case latestVersion: {
 				const bundle = await polyfillio.getPolyfillString(params);
+				await respondWithBundle(response, params, bundle);
+				break;
+			}
+			case "3.39.0": {
+				const bundle = await polyfillio_3_39_0.getPolyfillString(params);
 				await respondWithBundle(response, params, bundle);
 				break;
 			}
