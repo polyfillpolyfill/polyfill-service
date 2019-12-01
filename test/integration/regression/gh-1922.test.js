92ebefd1531bdb901fd2022ce7d230ee43fa36db
Jake Champion <jakechampion.jake2@googlemail.com>
2019-12-01 02:18:26 +0000

    fix linting

--- test/integration/regression/gh-1922.test.js
+++ test/integration/regression/gh-1922.test.js
@@ -10,14 +10,18 @@ describe("https://github.com/Financial-Times/polyfill-service/issues/1922", func
 	this.timeout(30000);
 	describe(`GET ${host}/v3/normalise_querystring_parameters_for_polyfill_bundle?callback=&compression=br&excludes=&features=IntersectionObserver%2Cdefault%2Cfetch&flags=&rum=0&ua=chrome%2F72.0.0&unknown=ignore&version=3.25.1`, function() {
 		it("responds with same javascript when ua parameter is url encoded", async function() {
-			const nonUrlEncodedUa = (await axios.get(
-				host +
-					"/v3/normalise_querystring_parameters_for_polyfill_bundle?callback=&compression=br&excludes=&features=IntersectionObserver%2Cdefault%2Cfetch&flags=&rum=0&ua=chrome/72.0.0&unknown=ignore&version=3.25.1"
-			)).data;
-			const urlEncodedUa = (await axios.get(
-				host +
-					"/v3/normalise_querystring_parameters_for_polyfill_bundle?callback=&compression=br&excludes=&features=IntersectionObserver%2Cdefault%2Cfetch&flags=&rum=0&ua=chrome%2F72.0.0&unknown=ignore&version=3.25.1"
-			)).data;
+			const nonUrlEncodedUa = (
+				await axios.get(
+					host +
+						"/v3/normalise_querystring_parameters_for_polyfill_bundle?callback=&compression=br&excludes=&features=IntersectionObserver%2Cdefault%2Cfetch&flags=&rum=0&ua=chrome/72.0.0&unknown=ignore&version=3.25.1"
+				)
+			).data;
+			const urlEncodedUa = (
+				await axios.get(
+					host +
+						"/v3/normalise_querystring_parameters_for_polyfill_bundle?callback=&compression=br&excludes=&features=IntersectionObserver%2Cdefault%2Cfetch&flags=&rum=0&ua=chrome%2F72.0.0&unknown=ignore&version=3.25.1"
+				)
+			).data;
 			proclaim.deepStrictEqual(nonUrlEncodedUa, urlEncodedUa);
 		});
 	});
