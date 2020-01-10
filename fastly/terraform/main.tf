a190f70b6ed93e088d9b7ddc420083026c17bedf
Jake Champion <me@jakechampion.name>
2020-01-10 13:32:21 +0000

    Add a default resource which samples no requests

--- fastly/terraform/main.tf
+++ fastly/terraform/main.tf
@@ -80,3 +80,13 @@ resource "fastly_service_v1" "app" {
     name = "toppops_config"
   }
 }
+
+resource "fastly_service_dictionary_items_v1" "items" {
+  service_id    = fastly_service_v1.app.id
+  dictionary_id = "${ { for dictionary in fastly_service_v1.app.dictionary : dictionary.name => dictionary.dictionary_id }["secrets"]}"
+
+  items = {
+    datacenters : "LCY,NRT,HAM,BWI,DCA"
+    sample_percent : "0"
+  }
+}
