f32e273a9c4bcb9d06bc2c0aae1f1eb1d5bcc438
Jake Champion <me@jakechampion.name>
2020-01-10 14:09:23 +0000

    use correct dictionary name toppops_config and not secrets

--- fastly/terraform/production_override.tf
+++ fastly/terraform/production_override.tf
@@ -86,7 +86,7 @@ resource "fastly_service_v1" "app" {
 
 resource "fastly_service_dictionary_items_v1" "items" {
   service_id    = fastly_service_v1.app.id
-  dictionary_id = "${ { for dictionary in fastly_service_v1.app.dictionary : dictionary.name => dictionary.dictionary_id }["secrets"]}"
+  dictionary_id = "${ { for dictionary in fastly_service_v1.app.dictionary : dictionary.name => dictionary.dictionary_id }["toppops_config"]}"
 
   items = {
     datacenters : "LCY,NRT,HAM,BWI,DCA"
