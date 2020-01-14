e7f7e7c0c2a78c9a5de583a394a17c66c2abff98
Jake Champion <me@jakechampion.name>
2020-01-14 16:42:52 +0000

    update 404 tests for synthetic responses to not include cache-control header or html content-type

--- test/integration/404.test.js
+++ test/integration/404.test.js
@@ -17,24 +17,18 @@ describe("GET /404", function() {
 	it("responds with a 404 status", () => {
 		return request(host)
 			.get("/pages/fixedData/")
-			.expect(404)
-			.expect("Cache-Control", "max-age=30, public, s-maxage=31536000, stale-while-revalidate=604800, stale-if-error=604800")
-			.expect("Content-Type", "text/html; charset=utf-8");
+			.expect(404);
 	});
 
 	it("responds with a 404 status", () => {
 		return request(host)
 			.get("/https://polyfill.io/")
-			.expect(404)
-			.expect("Cache-Control", "max-age=30, public, s-maxage=31536000, stale-while-revalidate=604800, stale-if-error=604800")
-			.expect("Content-Type", "text/html; charset=utf-8");
+			.expect(404);
 	});
 
 	it("responds with a 404 status", () => {
 		return request(host)
 			.get("/https://cdn.polyfill.io/")
-			.expect(404)
-			.expect("Cache-Control", "max-age=30, public, s-maxage=31536000, stale-while-revalidate=604800, stale-if-error=604800")
-			.expect("Content-Type", "text/html; charset=utf-8");
+			.expect(404);
 	});
 });
