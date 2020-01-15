eda6657e174430f53dc5379c0578047cee31c4a8
Jake Champion <me@jakechampion.name>
2020-01-15 11:11:36 +0000

    Add test to ensure 304 responses reset the Age header to 0

---
+++ test/integration/regression/304-age.test.js
@@ -0,0 +1,42 @@
+/* eslint-env mocha */
+
+"use strict";
+
+const request = require("supertest");
+const host = require("../helpers").host;
+
+describe("Request with a If-None-Match value which is the same as the ETag", function() {
+	it(`responds with a 304 and an Age reset to 0`, function() {
+		this.timeout(30000);
+		return request(host)
+			.get("/v3/polyfill.min.js")
+			.expect(200)
+			.then(response => {
+				const eTag = response.headers["etag"];
+
+				return request(host)
+					.get("/v3/polyfill.min.js")
+					.set("if-none-match", eTag)
+					.expect(304)
+					.expect("Age", 0);
+			});
+	});
+});
+
+describe("Request with a If-None-Match value which is the different to the ETag", function() {
+	it(`responds with a 200 and an Age reset to 0`, function() {
+		this.timeout(30000);
+		return request(host)
+			.get("/v3/polyfill.min.js")
+			.expect(200)
+			.then(response => {
+				const eTag = response.headers["etag"];
+
+				return request(host)
+					.get("/v3/polyfill.min.js")
+					.set("if-none-match", eTag)
+					.expect(304)
+					.expect("Age", 0);
+			});
+	});
+});
