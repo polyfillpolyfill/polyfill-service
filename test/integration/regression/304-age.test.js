4b8ad9f1e1ddc0db8f9ff0d1b496520942f1be51
Jake Champion <me@jakechampion.name>
2020-01-23 16:49:51 +0000

    update tets to use lastModified

--- test/integration/regression/304-age.test.js
+++ test/integration/regression/304-age.test.js
@@ -5,38 +5,36 @@
 const request = require("supertest");
 const host = require("../helpers").host;
 
-describe("Request with a If-None-Match value which is the same as the ETag", function() {
+describe("Request with a If-Modified-Since value which is the same as the lastModified", function() {
 	it(`responds with a 304 and an Age reset to 0`, function() {
 		this.timeout(30000);
 		return request(host)
 			.get("/v3/polyfill.min.js")
 			.expect(200)
 			.then(response => {
-				const eTag = response.headers["etag"];
 				const lastModified = response.headers["last-modified"];
-				console.log({ eTag, lastModified });
 
 				return request(host)
 					.get("/v3/polyfill.min.js")
-					.set("if-none-match", eTag)
+					.set("if-modified-since", lastModified)
 					.expect(304)
 					.expect("Age", "0");
 			});
 	});
 });
 
-describe("Request with a If-None-Match value which is different to the ETag", function() {
+describe("Request with a If-Modified-Since value which is different to the lastModified", function() {
 	it(`responds with a 200 and an Age reset to 0`, function() {
 		this.timeout(30000);
 		return request(host)
 			.get("/v3/polyfill.min.js")
 			.expect(200)
 			.then(response => {
-				const eTag = response.headers["etag"];
+				const lastModified = response.headers["last-modified"];
 
 				return request(host)
 					.get("/v3/polyfill.min.js")
-					.set("if-none-match", eTag)
+					.set("if-modified-since", lastModified)
 					.expect(304)
 					.expect("Age", "0");
 			});
