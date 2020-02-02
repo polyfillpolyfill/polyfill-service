49f60e214837901c52d650676f5754e591997a11
Jake Champion <me@jakechampion.name>
2020-02-02 00:51:34 +0000

    make json and extract latest version

--- .circleci/toppops.sh
+++ .circleci/toppops.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-version=`curl -X GET --fail -H "Fastly-Key: $FASTLY_API_KEY_POLYFILL_ACCOUNT" "https://api.fastly.com/service/$FASTLY_SERVICE_ID_PROD/version"`
+version=`curl -X GET --fail -H "Fastly-Key: $FASTLY_API_KEY_POLYFILL_ACCOUNT" "https://api.fastly.com/service/$FASTLY_SERVICE_ID_PROD/version" | jq '.[-1].number'`
 
 cloned_version=`curl -X PUT --fail -H "Fastly-Key: $FASTLY_API_KEY_POLYFILL_ACCOUNT" "https://api.fastly.com/service/$FASTLY_SERVICE_ID_PROD/version/$version/clone" | jq '.number'`
 
 cat <<EOT >> toppops-logging-config.json
 {
-    name: 'toppops-collector',
-    url: `https://toppops-ingest.fastlylabs.com/ingest`,
-    message_type: 'blank',
-    format_version: 2,
-    format: "",
-    content_type: 'text/plain',
-    method: 'POST',
-    placement: 'none'
+    "name": "toppops-collector",
+    "url": "https://toppops-ingest.fastlylabs.com/ingest",
+    "message_type": "blank",
+    "format_version": 2,
+    "format": "",
+    "content_type": "text/plain",
+    "method": "POST",
+    "placement": "none"
 }
 EOT
 
