7ef349b41a9fe487f339eb036cf7c61f65c493ca
chee <chee@snoot.club>
2020-12-03 12:53:30 +0000

    Format .eslintrc.js with .prettierrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,19 @@
 module.exports = {
-  env: {
-    commonjs: true,
-    es6: true,
-    node: true
-  },
-  extends: ['eslint:recommended', 'plugin:unicorn/recommended'],
-  rules: {
-    'unicorn/no-reduce': 0,
-    'unicorn/no-useless-undefined': 0,
-  },
-  globals: {
-    Atomics: 'readonly',
-    SharedArrayBuffer: 'readonly'
-  },
-  parserOptions: {
-    ecmaVersion: 2018
-  }
+	env: {
+		commonjs: true,
+		es6: true,
+		node: true,
+	},
+	extends: ["eslint:recommended", "plugin:unicorn/recommended"],
+	rules: {
+		"unicorn/no-reduce": 0,
+		"unicorn/no-useless-undefined": 0,
+	},
+	globals: {
+		Atomics: "readonly",
+		SharedArrayBuffer: "readonly",
+	},
+	parserOptions: {
+		ecmaVersion: 2018,
+	},
 };
