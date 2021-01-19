f3d190ca253ccbe2552c798b430bec7c805ba64d
Jake Champion <me@jakechampion.name>
2021-01-19 23:19:32 +0000

    Update get-polyfill-parameters.js

--- server/lib/get-polyfill-parameters.js
+++ server/lib/get-polyfill-parameters.js
@@ -15,14 +15,14 @@ function getCompressionFromAcceptEncoding(request) {
 module.exports = function getPolyfillParameters(request = {}) {
 	const query = request.query || {};
 	const path = request.path || "";
-	const { excludes = "", features = "default", rum, unknown = "polyfill", version, callback } = query;
-	const uaString = query.ua || (request.headers && request.headers["user-agent"]) || (typeof request.get === "function" && request.get("User-Agent")) || "";
+	const { excludes = "", features = "default", rum, unknown = "polyfill", version, callback, ua, flags } = query;
+	const uaString = ua || (request.headers && request.headers["user-agent"]) || (typeof request.get === "function" && request.get("User-Agent")) || "";
 	const compression = query.compression && query.compression !== "identity" ? query.compression : getCompressionFromAcceptEncoding(request);
 	const strict = Object.prototype.hasOwnProperty.call(query, "strict");
 
 	return {
 		excludes: excludes ? excludes.split(",") : [],
-		features: featuresfromQueryParameter(features, query.flags),
+		features: featuresfromQueryParameter(features, flags),
 		minify: path.endsWith(".min.js"),
 		rum: Number.parseInt(rum, 10) === 1,
 		stream: true,
