25006e4b30d79a669e4c22b97dc7cde496de9f74
Jake Champion <me@jakechampion.name>
2021-11-01 23:49:15 +0000

    Update api.test.js

--- test/integration/v2/api.test.js
+++ test/integration/v2/api.test.js
@@ -16,7 +16,7 @@ describe("GET /v2/polyfill.js", function() {
 			.expect(200)
 			.expect("Content-Type", "text/javascript; charset=utf-8")
 			.expect("cache-control", "public, s-maxage=31536000, max-age=604800, stale-while-revalidate=604800, stale-if-error=604800")
-			.expect("surrogate-key", "polyfill-service")
+			.expect("surrogate-key", /polyfill-service/)
 			.then(response => {
 				assert.isString(response.text);
 				assert.doesNotThrow(() => new vm.Script(response.text));
@@ -34,7 +34,7 @@ describe("GET /v2/polyfill.js?callback=AAA&callback=BBB", function() {
 			.expect(200)
 			.expect("Content-Type", "text/javascript; charset=utf-8")
 			.expect("cache-control", "public, s-maxage=31536000, max-age=604800, stale-while-revalidate=604800, stale-if-error=604800")
-			.expect("surrogate-key", "polyfill-service")
+			.expect("surrogate-key", /polyfill-service/)
 			.then(response => {
 				assert.isString(response.text);
 				assert.doesNotThrow(() => new vm.Script(response.text));
@@ -52,7 +52,7 @@ describe("GET /v2/polyfill.min.js", function() {
 			.expect(200)
 			.expect("Content-Type", "text/javascript; charset=utf-8")
 			.expect("cache-control", "public, s-maxage=31536000, max-age=604800, stale-while-revalidate=604800, stale-if-error=604800")
-			.expect("surrogate-key", "polyfill-service")
+			.expect("surrogate-key", /polyfill-service/)
 			.then(response => {
 				assert.isString(response.text);
 				assert.doesNotThrow(() => new vm.Script(response.text));
@@ -70,7 +70,7 @@ describe("GET /v2/polyfill.js?features=all&ua=non-existent-ua&unknown=polyfill&f
 			.expect(200)
 			.expect("Content-Type", "text/javascript; charset=utf-8")
 			.expect("cache-control", "public, s-maxage=31536000, max-age=604800, stale-while-revalidate=604800, stale-if-error=604800")
-			.expect("surrogate-key", "polyfill-service")
+			.expect("surrogate-key", /polyfill-service/)
 			.then(response => {
 				assert.isString(response.text);
 				// vm.Script will cause the event loop to become blocked whilst it parses the large response
@@ -89,7 +89,7 @@ describe("GET /v2/polyfill.min.js?features=all&ua=non-existent-ua&unknown=polyfi
 			.expect(200)
 			.expect("Content-Type", "text/javascript; charset=utf-8")
 			.expect("cache-control", "public, s-maxage=31536000, max-age=604800, stale-while-revalidate=604800, stale-if-error=604800")
-			.expect("surrogate-key", "polyfill-service")
+			.expect("surrogate-key", /polyfill-service/)
 			.then(response => {
 				assert.isString(response.text);
 				// vm.Script will cause the event loop to become blocked whilst it parses the large response
