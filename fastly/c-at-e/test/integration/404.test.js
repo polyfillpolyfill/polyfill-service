41e19cd3ab122f1ec3a6f4c0e53801401c140d33
Jake Champion <me@jakechampion.name>
2022-03-19 23:21:49 +0000

    make 404 tests match the tests in /test/integration/404.test.js

--- fastly/c-at-e/test/integration/404.test.js
+++ fastly/c-at-e/test/integration/404.test.js
@@ -10,39 +10,20 @@ describe("GET /404", function () {
     const response = await axios.get(`/404?use-compute-at-edge-backend=yes`);
     assert.equal(response.status, 404);
     assert.equal(response.headers["content-type"], "text/html; charset=utf-8");
-    assert.equal(
-      response.headers["cache-control"],
-      "max-age=30, public, s-maxage=31536000, stale-while-revalidate=604800, stale-if-error=604800"
-    );
   });
 
   it("responds with a 404 status", async () => {
     const response = await axios.get(`/pages/fixedData/?use-compute-at-edge-backend=yes`);
     assert.equal(response.status, 404);
-    assert.equal(response.headers["content-type"], "text/html; charset=utf-8");
-    assert.equal(
-      response.headers["cache-control"],
-      "max-age=30, public, s-maxage=31536000, stale-while-revalidate=604800, stale-if-error=604800"
-    );
   });
 
   it("responds with a 404 status", async () => {
     const response = await axios.get(`/https://polyfill.io/?use-compute-at-edge-backend=yes`);
     assert.equal(response.status, 404);
-    assert.equal(response.headers["content-type"], "text/html; charset=utf-8");
-    assert.equal(
-      response.headers["cache-control"],
-      "max-age=30, public, s-maxage=31536000, stale-while-revalidate=604800, stale-if-error=604800"
-    );
   });
 
   it("responds with a 404 status", async () => {
     const response = await axios.get(`/https://cdn.polyfill.io/?use-compute-at-edge-backend=yes`);
     assert.equal(response.status, 404);
-    assert.equal(response.headers["content-type"], "text/html; charset=utf-8");
-    assert.equal(
-      response.headers["cache-control"],
-      "max-age=30, public, s-maxage=31536000, stale-while-revalidate=604800, stale-if-error=604800"
-    );
   });
 });
