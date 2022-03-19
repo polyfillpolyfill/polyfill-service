7c95ccc03cb8996b5fb1100e8763767bec982647
Jake Champion <me@jakechampion.name>
2022-03-19 23:20:34 +0000

    correct test url

--- fastly/c-at-e/test/integration/home.test.js
+++ fastly/c-at-e/test/integration/home.test.js
@@ -7,7 +7,7 @@ import axios from "./helpers.js";
 
 describe("GET /", function() {
 	it("responds with a 301 status", async () => {
-		const response = await axios.get(`/&use-compute-at-edge-backend=yes`);
+		const response = await axios.get(`/?use-compute-at-edge-backend=yes`);
 		assert.equal(response.status, 301);
 		assert.equal(response.headers["location"], "/v3/");
 	});
