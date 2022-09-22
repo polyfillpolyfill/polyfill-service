08e5ca22edbd132437c08a9bb92979c1649c8539
Jake Champion <jchampion@fastly.com>
2022-09-22 12:18:57 +0100

    allow service-worker related globals to be used in the c@e linting rules

--- fastly/c-at-e/.eslintrc.cjs
+++ fastly/c-at-e/.eslintrc.cjs
@@ -1,14 +1,15 @@
 module.exports = {
 	"root": true,
-    "env": {
-        "es2022": true
-    },
-    "extends": "eslint:recommended",
-    "parserOptions": {
-        "ecmaVersion": 13,
-        "sourceType": "module"
-    },
-    "rules": {
-        "no-cond-assign": "off"
-    }
+	"env": {
+		"es2022": true,
+		"serviceworker": true
+	},
+	"extends": "eslint:recommended",
+	"parserOptions": {
+		"ecmaVersion": 13,
+		"sourceType": "module"
+	},
+	"rules": {
+		"no-cond-assign": "off"
+	}
 };
