b66e90c4be6bd92ccc2fc2baa076ec01d9216d6c
Jake Champion <jchampion@fastly.com>
2023-01-09 10:35:19 +0000

    Update fastly/terraform/dev_override.tf

--- fastly/terraform/dev_override.tf
+++ fastly/terraform/dev_override.tf
@@ -4,6 +4,52 @@ resource "fastly_service_compute" "app" {
   domain {
     name = "origami-polyfill-service-dev.edgecompute.app"
   }
+  
+  backend {
+    name                  = "v3_eu"
+    address               = "origami-polyfill-service-int.herokuapp.com"
+    port                  = 443
+    healthcheck           = "v3_eu_healthcheck"
+    ssl_cert_hostname     = "*.herokuapp.com"
+    auto_loadbalance      = false
+    connect_timeout       = 5000
+    first_byte_timeout    = 120000
+    between_bytes_timeout = 120000
+    error_threshold       = 0
+    override_host         = "origami-polyfill-service-int.herokuapp.com"
+  }
+
+  healthcheck {
+    name      = "v3_eu_healthcheck"
+    host      = "origami-polyfill-service-int.herokuapp.com"
+    path      = "/__gtg"
+    timeout   = 5000
+    threshold = 2
+    window    = 5
+  }
+
+  backend {
+    name                  = "v3_us"
+    address               = "origami-polyfill-service-int.herokuapp.com"
+    port                  = 443
+    healthcheck           = "v3_us_healthcheck"
+    ssl_cert_hostname     = "*.herokuapp.com"
+    auto_loadbalance      = false
+    connect_timeout       = 5000
+    first_byte_timeout    = 120000
+    between_bytes_timeout = 120000
+    error_threshold       = 0
+    override_host         = "origami-polyfill-service-int.herokuapp.com"
+  }
+
+  healthcheck {
+    name      = "v3_us_healthcheck"
+    host      = "origami-polyfill-service-int.herokuapp.com"
+    path      = "/__gtg"
+    timeout   = 5000
+    threshold = 2
+    window    = 5
+  }
 }
 
 resource "fastly_service_vcl" "app" {
