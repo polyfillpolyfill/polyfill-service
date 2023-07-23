5ed9acb751b7f687f691d7f545e6d61f640d9490
Jake Champion <jchampion@fastly.com>
2023-07-23 17:52:41 +0100

    .get already decodes so the second decode could now error

--- app/normalise-query-parameters.js
+++ app/normalise-query-parameters.js
@@ -7,54 +7,54 @@ export function normalise_querystring_parameters_for_polyfill_bundle(originalReq
 
     let features;
     if (features = currentQuerystring.get('features')) {
-		// # Parameter has already been set, use the already set value.
+        // # Parameter has already been set, use the already set value.
         try {
-            features = decodeURIComponent(features);
+            try { features = decodeURIComponent(features); } catch { /* empty */ }
             newQuerystring.set('features', features);
         } catch {
             // This is here because the VCL version of polyfill.io would silently ignore URI Errors
             newQuerystring.set('features', features);
         }
-	} else {
-		// # Parameter has not been set, use the default value.
+    } else {
+        // # Parameter has not been set, use the default value.
         newQuerystring.set('features', "default");
-	}
+    }
 
-	let excludes;
-	if (excludes = currentQuerystring.get('excludes')) {
-		try {
+    let excludes;
+    if (excludes = currentQuerystring.get('excludes')) {
+        try {
             // # Parameter has already been set, use the already set value.
-            excludes = decodeURIComponent(excludes);
+            try { excludes = decodeURIComponent(excludes); } catch { /* empty */ }
             newQuerystring.set('excludes', excludes);
         } catch {
             // This is here because the VCL version of polyfill.io would silently ignore URI Errors
             newQuerystring.set('excludes', excludes);
         }
-	} else {
-		newQuerystring.set('excludes', "");
-	}
+    } else {
+        newQuerystring.set('excludes', "");
+    }
 
     let unknown;
-	if (unknown = currentQuerystring.get('unknown')) {
+    if (unknown = currentQuerystring.get('unknown')) {
         newQuerystring.set('unknown', unknown);
-	} else {
+    } else {
         // # If unknown is not set, set to default value "polyfill"
-		newQuerystring.set('unknown', "polyfill");
-	}
+        newQuerystring.set('unknown', "polyfill");
+    }
 
     let flags;
-	if (flags = currentQuerystring.get('flags')) {
+    if (flags = currentQuerystring.get('flags')) {
         // # Parameter has already been set, use the already set value.
-        flags = decodeURIComponent(flags);
+        try { flags = decodeURIComponent(flags); } catch { /* empty */ }
         newQuerystring.set('flags', flags);
-	} else {
+    } else {
         // # If flags is not set, set to default value ""
-		newQuerystring.set('flags', "");
-	}
+        newQuerystring.set('flags', "");
+    }
 
-	// # If version is not set, set to default value
+    // # If version is not set, set to default value
     let version;
-	if (version = currentQuerystring.get('version')) {
+    if (version = currentQuerystring.get('version')) {
         switch (version) {
             case "3.111.0":
             case "3.110.1":
@@ -101,27 +101,27 @@ export function normalise_querystring_parameters_for_polyfill_bundle(originalReq
                 newQuerystring.set('version', "");
             }
         }
-	} else {
-		newQuerystring.set('version', "");
-	}
+    } else {
+        newQuerystring.set('version', "");
+    }
 
     let ua;
-	if (ua = currentQuerystring.get('ua')) {
-        newQuerystring.set("ua", decodeURIComponent(ua));
+    if (ua = currentQuerystring.get('ua')) {
+        try { newQuerystring.set("ua", decodeURIComponent(ua)); } catch { /* empty */ }
     } else {
         // # If ua is not set, normalise the User-Agent header based upon the version of the polyfill-library that has been requested.
         const useragent = originalRequest.headers.get("User-Agent");
         const normalisedUserAgent = version === "3.25.1" ? oldUserAgentNormaliser.UA(useragent) : latestUserAgentNormaliser.normalize(useragent);
-		newQuerystring.set("ua", normalisedUserAgent);
-	}
+        newQuerystring.set("ua", normalisedUserAgent);
+    }
 
     let callback;
-	if (callback = currentQuerystring.get('callback')) {
+    if (callback = currentQuerystring.get('callback')) {
         newQuerystring.set('callback', callback);
-	} else {
+    } else {
         // # If callback is not set, set to default value ""
-		newQuerystring.set('callback', "");
-	}
+        newQuerystring.set('callback', "");
+    }
 
-	return newQuerystring;
+    return newQuerystring;
 }
