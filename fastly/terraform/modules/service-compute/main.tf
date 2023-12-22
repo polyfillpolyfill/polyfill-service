19a1519dfffb570f7f273d511ccbcaae6a9347ff
Jake Champion <jchampion@fastly.com>
2023-12-22 23:56:16 +0000

    add https loggers

--- fastly/terraform/modules/service-compute/main.tf
+++ fastly/terraform/modules/service-compute/main.tf
@@ -14,6 +14,17 @@ resource "fastly_service_compute" "app" {
     port    = 80
   }
 
+  dynamic "logger" {
+    for_each = var.https_loggers
+    content {
+      name         = logger.value["name"]
+      url          = logger.value["url"]
+      message_type = logger.value["message_type"]
+      content_type = logger.value["content_type"]
+      method       = logger.value["method"]
+    }
+  }
+
   dynamic "domain" {
     for_each = var.compute_domains
     content {
--- fastly/terraform/modules/service-compute/variables.tf
+++ fastly/terraform/modules/service-compute/variables.tf
@@ -3,6 +3,16 @@ variable "compute_name" {
 }
 variable "compute_domains" {
   type = list(object({
-    name    = string
+    name = string
+  }))
+}
+
+variable "https_loggers" {
+  type = list(object({
+    name         = string
+    url          = string
+    message_type = string
+    content_type = string
+    method       = string
   }))
 }
